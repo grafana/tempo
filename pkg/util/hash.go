78dacb88c5060e0c6bd94d77623dd5f74bb6ed06
Joe Elliott <number101010@gmail.com>
2020-01-17 11:03:37 -0500

    Removed a bunch of stuff that was apparently not used

---
+++ pkg/util/hash.go
@@ -0,0 +1,17 @@
+package util
+
+import "hash/fnv"
+
+// TokenFor generates a token used for finding ingesters from ring
+func TokenFor(b []byte) uint32 {
+	h := fnv.New32()
+	_, _ = h.Write(b)
+	return h.Sum32()
+}
+
+// friggtodo:  better alg?  just add high order uint64 to low order uint64?
+func Fingerprint(b []byte) uint64 {
+	h := fnv.New64()
+	_, _ = h.Write(b)
+	return h.Sum64()
+}
