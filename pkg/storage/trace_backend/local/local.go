c41ac702be815e01593e86ce145d2a9798b03f93
Joe Elliott <number101010@gmail.com>
2020-01-21 12:02:10 -0500

    Added local backend impl and tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/storage/trace_backend/local/local.go
+++ pkg/storage/trace_backend/local/local.go
@@ -1,6 +1,12 @@
 package local
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+
 	"github.com/google/uuid"
 	"github.com/joe-elliott/frigg/pkg/storage/trace_backend"
 )
@@ -18,22 +24,130 @@ func New(cfg Config) (trace_backend.Reader, trace_backend.Writer) {
 }
 
 func (rw *readerWriter) Write(blockID uuid.UUID, tenantID string, bBloom []byte, bIndex []byte, tracesFilePath string) error {
+	err := os.MkdirAll(rw.rootPath(blockID, tenantID), os.ModePerm)
+	if err != nil {
+		return err
+	}
 
-	return nil
+	bloom := rw.bloomFileName(blockID, tenantID)
+	if fileExists(bloom) {
+		return fmt.Errorf("unexpectedly found bloom file at %s", bloom)
+	}
+	err = ioutil.WriteFile(bloom, bBloom, 0644)
+	if err != nil {
+		return err
+	}
+
+	index := rw.indexFileName(blockID, tenantID)
+	if fileExists(index) {
+		return fmt.Errorf("unexpectedly found index file at %s", index)
+	}
+	err = ioutil.WriteFile(index, bIndex, 0644)
+	if err != nil {
+		return err
+	}
+
+	traces := rw.tracesFileName(blockID, tenantID)
+	if fileExists(traces) {
+		return fmt.Errorf("unexpectedly found traces file at %s", index)
+	}
+	if !fileExists(tracesFilePath) {
+		return fmt.Errorf("traces file not found %s", tracesFilePath)
+	}
+
+	// copy traces file
+	src, err := os.Open(tracesFilePath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+	dst, err := os.Create(traces)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	return err
 }
 
 func (rw *readerWriter) Bloom(tenantID string, fn trace_backend.BloomIter) error {
+	path := path.Join(rw.cfg.path, tenantID)
+	folders, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range folders {
+		if !f.IsDir() {
+			continue
+		}
+
+		blockID, err := uuid.Parse(f.Name())
+		if err != nil {
+			return err
+		}
+
+		filename := rw.bloomFileName(blockID, tenantID)
+		bytes, err := ioutil.ReadFile(filename)
+
+		more, err := fn(bytes, blockID)
+
+		if err != nil {
+			return err
+		}
+
+		if !more {
+			break
+		}
+	}
+
 	return nil
 }
 
 func (rw *readerWriter) Index(blockID uuid.UUID, tenantID string) ([]byte, error) {
-	return nil, nil
+	filename := rw.indexFileName(blockID, tenantID)
+	return ioutil.ReadFile(filename)
 }
 
 func (rw *readerWriter) Trace(blockID uuid.UUID, tenantID string, start uint64, length uint32) ([]byte, error) {
-	return nil, nil
+	filename := rw.tracesFileName(blockID, tenantID)
+
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	b := make([]byte, length)
+	_, err = f.ReadAt(b, int64(start))
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
 }
 
 func (rw *readerWriter) bloomFileName(blockID uuid.UUID, tenantID string) string {
-	return ""
+	return path.Join(rw.rootPath(blockID, tenantID), "bloom")
+}
+
+func (rw *readerWriter) indexFileName(blockID uuid.UUID, tenantID string) string {
+	return path.Join(rw.rootPath(blockID, tenantID), "index")
+}
+
+func (rw *readerWriter) tracesFileName(blockID uuid.UUID, tenantID string) string {
+	return path.Join(rw.rootPath(blockID, tenantID), "traces")
+}
+
+func (rw *readerWriter) rootPath(blockID uuid.UUID, tenantID string) string {
+	return path.Join(rw.cfg.path, tenantID, blockID.String())
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
---
+++ pkg/storage/trace_backend/local/local_test.go
@@ -0,0 +1,63 @@
+package local
+
+import (
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadWrite(t *testing.T) {
+	tempDir, err := ioutil.TempDir("/tmp", "")
+	defer os.RemoveAll(tempDir)
+	assert.NoError(t, err, "unexpected error creating temp dir")
+
+	fakeTracesFile, err := ioutil.TempFile("/tmp", "")
+	defer os.Remove(fakeTracesFile.Name())
+	assert.NoError(t, err, "unexpected error creating temp file")
+
+	r, w := New(Config{
+		path: tempDir,
+	})
+
+	blockID := uuid.New()
+	tenantID := "fake"
+
+	fakeBloom := make([]byte, 20)
+	fakeIndex := make([]byte, 20)
+	fakeTraces := make([]byte, 200)
+
+	_, err = rand.Read(fakeBloom)
+	assert.NoError(t, err, "unexpected error creating fakeBloom")
+	_, err = rand.Read(fakeIndex)
+	assert.NoError(t, err, "unexpected error creating fakeIndex")
+	_, err = rand.Read(fakeTraces)
+	assert.NoError(t, err, "unexpected error creating fakeTraces")
+	_, err = fakeTracesFile.Write(fakeTraces)
+	assert.NoError(t, err, "unexpected error writing fakeTraces")
+
+	err = w.Write(blockID, tenantID, fakeBloom, fakeIndex, fakeTracesFile.Name())
+	assert.NoError(t, err, "unexpected error writing")
+
+	actualIndex, err := r.Index(blockID, tenantID)
+	assert.NoError(t, err, "unexpected error reading indexes")
+	assert.Equal(t, fakeIndex, actualIndex)
+
+	actualTrace, err := r.Trace(blockID, tenantID, 100, 20)
+	assert.NoError(t, err, "unexpected error reading traces")
+	assert.Equal(t, fakeTraces[100:120], actualTrace)
+
+	i := 0
+	err = r.Bloom(tenantID, func(actualBloomBytes []byte, actualBlockID uuid.UUID) (bool, error) {
+		assert.Equal(t, blockID, actualBlockID)
+		assert.Equal(t, fakeBloom, actualBloomBytes)
+		i++
+
+		return true, nil
+	})
+	assert.NoError(t, err, "unexpected error iterating bloom")
+	assert.Equal(t, 1, i, "should only be 1 bloom filter")
+}
