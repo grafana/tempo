06a1eb1ddf36cfe2bf302baf7332d6390e76fa46
Joe Elliott <number101010@gmail.com>
2020-01-31 13:02:41 -0500

    Use http for frigg-cli
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/frigg-cli/main.go
+++ cmd/frigg-cli/main.go
@@ -1,51 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"path"
-	"time"
+	"net/http"
 
-	"github.com/grafana/frigg/pkg/storage"
-	"github.com/grafana/frigg/pkg/storage/block"
-	"github.com/grafana/frigg/pkg/storage/trace_backend/local"
-	"github.com/grafana/frigg/pkg/util"
+	"github.com/grafana/frigg/pkg/friggpb"
 )
 
 var (
-	dir           string
-	modeDumpIndex bool
-	findTraceID   string
+	host        string
+	findTraceID string
 )
 
 func init() {
-	flag.StringVar(&dir, "dir", "", "dir to find chunks")
-	flag.BoolVar(&modeDumpIndex, "dump-index", false, "dumps the index at the path")
+	flag.StringVar(&host, "host", "localhost:3100", "frigg host to connect to")
 	flag.StringVar(&findTraceID, "find-trace", "", "finds a trace by id.  expected to be a hex value")
 }
 
 func main() {
 	flag.Parse()
 
-	if len(dir) == 0 {
-		fmt.Println("-dir is required")
+	if len(host) == 0 {
+		fmt.Println("-host is required")
 		return
 	}
 
-	if !modeDumpIndex && len(findTraceID) == 0 {
-		fmt.Println("One of -dump-index, -find-trace, ... is required")
+	if len(findTraceID) == 0 {
+		fmt.Println("One of -find-trace, ... is required")
 		return
 	}
 
 	var err error
-
-	if modeDumpIndex {
-		err = dumpIndex(dir)
-	}
-
 	if len(findTraceID) > 0 {
-		err = findTraceByID(dir, findTraceID)
+		err = findTraceByID(host, findTraceID)
 	}
 
 	if err != nil {
@@ -53,59 +42,18 @@ func main() {
 	}
 }
 
-func findTraceByID(dir string, id string) error {
-	byteID, err := util.HexStringToTraceID(id)
+func findTraceByID(host string, id string) error {
+	resp, err := http.Get("http://" + host + "/api/traces/" + id)
 	if err != nil {
 		return err
 	}
 
-	reader, _, err := storage.NewTraceStore(storage.TraceConfig{
-		Backend:                  "local",
-		BloomFilterFalsePositive: .01,
-		Local: local.Config{
-			Path: dir,
-		},
-	})
+	out := &friggpb.Trace{}
+	err = json.NewDecoder(resp.Body).Decode(out)
 	if err != nil {
 		return err
 	}
-
-	start := time.Now()
-	trace, metrics, err := reader.FindTrace("fake", byteID)
-	elapsed := time.Since(start)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("------trace--------")
-	if trace != nil {
-		fmt.Printf("%+v", trace)
-	} else {
-		fmt.Println("trace not found")
-	}
-	fmt.Println("------metrics--------")
-	fmt.Printf("%+v\n", metrics)
-	fmt.Printf("This is meaningless but: %s", elapsed)
-
-	return nil
-}
-
-func dumpIndex(dir string) error {
-
-	bytes, err := ioutil.ReadFile(path.Join(dir, "index"))
-	if err != nil {
-		return err
-	}
-
-	records, err := block.UnmarshalRecords(bytes)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("records: %d\n", len(records))
-	for i, r := range records {
-		fmt.Printf("%4d : %v %v %v\n", i, r.ID, r.Start, r.Length)
-	}
+	fmt.Printf("%+v", out)
 
 	return nil
 }
