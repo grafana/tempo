bad32bddd46114dd6ce31dfb4e6ea4c0bbc481b0
Joe Elliott <number101010@gmail.com>
2020-02-28 08:51:53 -0500

    Fixed proto to point to new location
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/friggpb/deps.go
+++ pkg/friggpb/deps.go
@@ -1,3 +1,3 @@
 package friggpb
 
-// import _ "github.com/open-telemetry/opentelemetry-proto/gen/go/collector/common/v1" // force vend to pull in the entire package and copy protos over
+import _ "github.com/open-telemetry/opentelemetry-proto/gen/go/trace/v1" // force vend to pull in the entire package and copy protos over
