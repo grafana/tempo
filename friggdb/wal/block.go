2e6c3ab896e78940b4d9607960bc54c8e57bc92e
Joe Elliott <number101010@gmail.com>
2020-03-02 10:14:14 -0500

    Removed readRecordBytes
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- friggdb/wal/block.go
+++ friggdb/wal/block.go
@@ -17,21 +17,6 @@ func (b *block) fullFilename() string {
 	return fmt.Sprintf("%s/%v:%v", b.filepath, b.meta.BlockID, b.meta.TenantID)
 }
 
-func (b *block) readRecordBytes(r *backend.Record) ([]byte, error) { // jpe?  belongs in backend? goes away when find is done
-	file, err := b.file()
-	if err != nil {
-		return nil, err
-	}
-
-	buff := make([]byte, r.Length)
-	_, err = file.ReadAt(buff, int64(r.Start))
-	if err != nil {
-		return nil, err
-	}
-
-	return buff, nil
-}
-
 func (b *block) file() (*os.File, error) {
 	if b.readFile == nil {
 		name := b.fullFilename()
