7d21cb38c03118e46c9e916b1aa7d624975eeeb6
Annanay <annanayagarwal@gmail.com>
2020-03-17 22:27:02 +0530

    Add basic (absolute) time window compactor
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

--- tempodb/compaction_block_selector.go
+++ tempodb/compaction_block_selector.go
@@ -47,3 +47,48 @@ func (sbs *simpleBlockSelector) BlocksToCompact() []*backend.BlockMeta {
 	// Could not find blocks suitable for compaction, break
 	return nil
 }
+
+/*************************** Time Window Block Selector **************************/
+
+type timeWindowBlockSelector struct {
+	cursor             int
+	slotStartTime      time.Time
+	blocklist          []*backend.BlockMeta
+	MaxCompactionRange time.Duration // Size of the time window - say 6 hours
+}
+
+var _ (CompactionBlockSelector) = (*timeWindowBlockSelector)(nil)
+
+func newTimeWindowBlockSelector(blocklist []*backend.BlockMeta, maxCompactionRange time.Duration) CompactionBlockSelector {
+	twbs := &timeWindowBlockSelector{
+		blocklist:          blocklist,
+		MaxCompactionRange: maxCompactionRange,
+	}
+
+	y, m, d := blocklist[0].StartTime.Date()
+	twbs.slotStartTime = time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+	return twbs
+}
+
+func (twbs *timeWindowBlockSelector) BlocksToCompact() []*backend.BlockMeta {
+	// Pick the same time window as the start block
+	startTime := twbs.blocklist[twbs.cursor].StartTime
+	for twbs.slotStartTime.Add(twbs.MaxCompactionRange).Sub(startTime).Seconds() < 0 {
+		twbs.slotStartTime.Add(twbs.MaxCompactionRange)
+	}
+
+	for twbs.cursor < len(twbs.blocklist)-inputBlocks+1 {
+		// Pick blocks in slotStartTime <> slotEndTime
+		slotEndTime := twbs.slotStartTime.Add(twbs.MaxCompactionRange)
+		cursorEnd := twbs.cursor + inputBlocks - 1
+		if twbs.blocklist[cursorEnd].StartTime.Sub(slotEndTime).Seconds() <= 0 {
+			startPos := twbs.cursor
+			twbs.cursor = startPos + inputBlocks
+			return twbs.blocklist[startPos : startPos+inputBlocks]
+		}
+		twbs.cursor++
+		twbs.slotStartTime.Add(twbs.MaxCompactionRange)
+	}
+
+	return nil
+}
