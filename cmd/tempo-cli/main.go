88e601e57d1acec19904b4a3430ef6bdfb5e44e4
Joe Elliott <number101010@gmail.com>
2020-03-25 10:06:40 -0400

    Added time window and sorted
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/tempo-cli/main.go
+++ cmd/tempo-cli/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 
@@ -15,13 +16,15 @@ import (
 )
 
 var (
-	gcsBucket string
-	tenantID  string
+	gcsBucket   string
+	tenantID    string
+	windowRange time.Duration
 )
 
 func init() {
 	flag.StringVar(&gcsBucket, "gcs-bucket", "", "bucket to scan")
 	flag.StringVar(&tenantID, "tenant-id", "", "tenant-id that contains the bucket")
+	flag.DurationVar(&windowRange, "window-range", 4*time.Hour, "block time window range for compaction")
 }
 
 func main() {
@@ -37,13 +40,13 @@ func main() {
 		return
 	}
 
-	err := dumpBucket(gcsBucket, tenantID)
+	err := dumpBucket(gcsBucket, tenantID, windowRange)
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
 }
 
-func dumpBucket(bucketName string, tenantID string) error {
+func dumpBucket(bucketName string, tenantID string, windowRange time.Duration) error {
 	r, _, c, err := gcs.New(&gcs.Config{
 		BucketName:      bucketName,
 		ChunkBufferSize: 10 * 1024 * 1024,
@@ -89,12 +92,13 @@ func dumpBucket(bucketName string, tenantID string) error {
 			}
 		}
 
-		objects, lvl, start, end := blockStats(meta, compactedMeta)
+		objects, lvl, window, start, end := blockStats(meta, compactedMeta, windowRange)
 		out = append(out, []string{
 			id.String(),
 			strconv.Itoa(int(lvl)),
 			strconv.Itoa(totalIDs),
 			strconv.Itoa(objects),
+			strconv.Itoa(int(window)),
 			strconv.Itoa(duplicateIDs),
 			start.Format(time.RFC3339),
 			end.Format(time.RFC3339),
@@ -102,21 +106,32 @@ func dumpBucket(bucketName string, tenantID string) error {
 		totalObjects += objects
 	}
 
+	sort.Slice(out, func(i, j int) bool {
+		lineI := out[i]
+		lineJ := out[j]
+
+		if lineI[4] == lineJ[4] {
+			return lineI[1] < lineJ[1]
+		}
+
+		return lineI[4] < lineJ[4]
+	})
+
 	w := tablewriter.NewWriter(os.Stdout)
-	w.SetHeader([]string{"id", "lvl", "idx", "count", "dupe", "start", "end"})
-	w.SetFooter([]string{"", "", "", strconv.Itoa(totalObjects), "", "", ""})
+	w.SetHeader([]string{"id", "lvl", "idx", "count", "window", "dupe", "start", "end"})
+	w.SetFooter([]string{"", "", "", strconv.Itoa(totalObjects), "", "", "", ""})
 	w.AppendBulk(out)
 	w.Render()
 
 	return nil
 }
 
-func blockStats(meta *backend.BlockMeta, compactedMeta *backend.CompactedBlockMeta) (int, uint8, time.Time, time.Time) {
+func blockStats(meta *backend.BlockMeta, compactedMeta *backend.CompactedBlockMeta, windowRange time.Duration) (int, uint8, int64, time.Time, time.Time) {
 	if meta != nil {
-		return meta.TotalObjects, meta.CompactionLevel, meta.StartTime, meta.EndTime
+		return meta.TotalObjects, meta.CompactionLevel, meta.StartTime.Unix() / int64(windowRange/time.Second), meta.StartTime, meta.EndTime
 	} else if compactedMeta != nil {
-		return compactedMeta.TotalObjects, compactedMeta.CompactionLevel, compactedMeta.StartTime, compactedMeta.EndTime
+		return compactedMeta.TotalObjects, compactedMeta.CompactionLevel, compactedMeta.StartTime.Unix() / int64(windowRange/time.Second), compactedMeta.StartTime, compactedMeta.EndTime
 	}
 
-	return -1, 0, time.Unix(0, 0), time.Unix(0, 0)
+	return -1, 0, -1, time.Unix(0, 0), time.Unix(0, 0)
 }
