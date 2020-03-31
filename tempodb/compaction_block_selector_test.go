641edab5691e7cde21915b0b3ade13c63f85bc6a
Joe Elliott <number101010@gmail.com>
2020-03-31 12:43:40 -0400

    Uncommented tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tempodb/compaction_block_selector_test.go
+++ tempodb/compaction_block_selector_test.go
@@ -17,62 +17,62 @@ func TestTimeWindowBlockSelector(t *testing.T) {
 		expected       []*backend.BlockMeta
 		expectedSecond []*backend.BlockMeta
 	}{
-		// {
-		// 	name:      "nil - nil",
-		// 	blocklist: nil,
-		// 	expected:  nil,
-		// },
-		// {
-		// 	name:      "empty - nil",
-		// 	blocklist: []*backend.BlockMeta{},
-		// 	expected:  nil,
-		// },
-		// {
-		// 	name: "two blocks returned",
-		// 	blocklist: []*backend.BlockMeta{
-		// 		&backend.BlockMeta{
-		// 			BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
-		// 		},
-		// 	},
-		// 	expected: []*backend.BlockMeta{
-		// 		&backend.BlockMeta{
-		// 			BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name: "three blocks choose smallest two",
-		// 	blocklist: []*backend.BlockMeta{
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-		// 			TotalObjects: 1,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 	},
-		// 	expected: []*backend.BlockMeta{
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 	},
-		// },
+		{
+			name:      "nil - nil",
+			blocklist: nil,
+			expected:  nil,
+		},
+		{
+			name:      "empty - nil",
+			blocklist: []*backend.BlockMeta{},
+			expected:  nil,
+		},
+		{
+			name: "two blocks returned",
+			blocklist: []*backend.BlockMeta{
+				&backend.BlockMeta{
+					BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+				},
+				&backend.BlockMeta{
+					BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+				},
+			},
+			expected: []*backend.BlockMeta{
+				&backend.BlockMeta{
+					BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+				},
+				&backend.BlockMeta{
+					BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+				},
+			},
+		},
+		{
+			name: "three blocks choose smallest two",
+			blocklist: []*backend.BlockMeta{
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+					TotalObjects: 0,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					TotalObjects: 1,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					TotalObjects: 0,
+				},
+			},
+			expected: []*backend.BlockMeta{
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+					TotalObjects: 0,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					TotalObjects: 0,
+				},
+			},
+		},
 		{
 			name: "three blocks across two windows",
 			blocklist: []*backend.BlockMeta{
@@ -104,47 +104,47 @@ func TestTimeWindowBlockSelector(t *testing.T) {
 				},
 			},
 		},
-		// {
-		// 	name: "two iterations of four blocks across two windows",
-		// 	blocklist: []*backend.BlockMeta{
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-		// 			TotalObjects: 1,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000003"),
-		// 			TotalObjects: 1,
-		// 		},
-		// 	},
-		// 	expected: []*backend.BlockMeta{
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
-		// 			TotalObjects: 0,
-		// 		},
-		// 	},
-		// 	expectedSecond: []*backend.BlockMeta{
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-		// 			TotalObjects: 1,
-		// 		},
-		// 		&backend.BlockMeta{
-		// 			BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000003"),
-		// 			TotalObjects: 1,
-		// 		},
-		// 	},
-		// },
+		{
+			name: "two iterations of four blocks across two windows",
+			blocklist: []*backend.BlockMeta{
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+					TotalObjects: 0,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					TotalObjects: 1,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					TotalObjects: 0,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000003"),
+					TotalObjects: 1,
+				},
+			},
+			expected: []*backend.BlockMeta{
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000002"),
+					TotalObjects: 0,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					TotalObjects: 0,
+				},
+			},
+			expectedSecond: []*backend.BlockMeta{
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					TotalObjects: 1,
+				},
+				&backend.BlockMeta{
+					BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000003"),
+					TotalObjects: 1,
+				},
+			},
+		},
 	}
 
 	for _, tt := range tests {
