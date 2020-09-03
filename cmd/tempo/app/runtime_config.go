49b31b4ef4aa17df76ef4d3cbe61be49a8bf3de1
Joe Elliott <number101010@gmail.com>
2020-09-03 10:59:51 -0400

    Added runtime_config
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ cmd/tempo/app/runtime_config.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"github.com/cortexproject/cortex/pkg/util/runtimeconfig"
+	"github.com/grafana/tempo/pkg/util/validation"
+)
+
+func tenantLimitsFromRuntimeConfig(c *runtimeconfig.Manager) validation.TenantLimits {
+	if c == nil {
+		return nil
+	}
+	return func(userID string) *validation.Limits {
+		cfg, ok := c.GetConfig().(*validation.OverridesConfig)
+		if !ok || cfg == nil {
+			return nil
+		}
+
+		return cfg.TenantLimits[userID]
+	}
+}
