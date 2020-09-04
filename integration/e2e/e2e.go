a68c3d901dfd0453d1239c6fe0ee00dde20268c4
Annanay <annanayagarwal@gmail.com>
2020-09-04 16:48:17 +0530

    Add comment to blank import
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

--- integration/e2e/e2e.go
+++ integration/e2e/e2e.go
@@ -1,5 +1,5 @@
 package e2e
 
 import (
-	_ "github.com/cortexproject/cortex/integration/e2e"
+	_ "github.com/cortexproject/cortex/integration/e2e" // blank import to test Makefile and vendor changes
 )
