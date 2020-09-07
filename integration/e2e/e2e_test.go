481c0d59bae3368dbd2470858130aec68655a7c4
Annanay <annanayagarwal@gmail.com>
2020-09-07 20:54:17 +0530

    Checkpoint, store connects
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

--- integration/e2e/e2e_test.go
+++ integration/e2e/e2e_test.go
@@ -1,12 +1,29 @@
 package e2e
 
 import (
-	"github.com/stretchr/testify/require"
-	"testing"
-
 	cortex_e2e "github.com/cortexproject/cortex/integration/e2e"
 	cortex_e2e_db "github.com/cortexproject/cortex/integration/e2e/db"
+	"github.com/stretchr/testify/require"
+	"path/filepath"
+	"testing"
 )
+
+var (
+	svc_name = "tempo"
+	image = "tempo:latest"
+)
+
+func NewTempoAllInOne() (*cortex_e2e.HTTPService, error) {
+	args := "-config.file="+filepath.Join(cortex_e2e.ContainerSharedDir, "config.yaml")
+
+	return cortex_e2e.NewHTTPService(
+		svc_name,
+		image,
+		cortex_e2e.NewCommandWithoutEntrypoint("/tempo", args),
+		cortex_e2e.NewHTTPReadinessProbe(3100, "/ready", 200, 505),
+		3100,
+	), nil
+}
 
 func TestIngest(t *testing.T) {
 	s, err := cortex_e2e.NewScenario("tempo_e2e")
@@ -17,7 +34,7 @@ func TestIngest(t *testing.T) {
 	require.NotNil(t, minio)
 	require.NoError(t, s.StartAndWaitReady(minio))
 
-	require.NoError(t, copyFileToSharedDir(s, "./config.yaml","config.yaml"))
+	require.NoError(t, copyFileToSharedDir(s, "./config.yaml", "config.yaml"))
 	tempo, err := NewTempoAllInOne()
 	require.NoError(t, err)
 	require.NoError(t, s.StartAndWaitReady(tempo))
