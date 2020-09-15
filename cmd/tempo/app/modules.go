ffeee65c3e941f02a4a6dac3c2661489983fe057
Joe Elliott <number101010@gmail.com>
2020-09-15 14:29:30 -0400

    added overrides dep
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/tempo/app/modules.go
+++ cmd/tempo/app/modules.go
@@ -200,7 +200,7 @@ func (t *App) setupModuleManager() error {
 		// MemberlistKV: nil,
 		Ring:        {Server, MemberlistKV},
 		Distributor: {Ring, Server, Overrides},
-		Ingester:    {Store, Server, MemberlistKV},
+		Ingester:    {Store, Server, Overrides, MemberlistKV},
 		Querier:     {Store, Ring},
 		Compactor:   {Store, Server, MemberlistKV},
 		All:         {Compactor, Querier, Ingester, Distributor},
