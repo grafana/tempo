a4f6132599aec3a5d3532513cb6829968c075590
Joe Elliott <joe.elliott@grafana.com>
2020-09-16 07:57:31 -0400

    Collection of Fixes To Get Tempo Redeployed (#163)
    * wups
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * added overrides dep
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * Set defaults for sharding ring
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/tempo/app/modules.go
+++ cmd/tempo/app/modules.go
@@ -170,11 +170,12 @@ func (t *App) initMemberlistKV() (services.Service, error) {
 	// todo: do we still need this?  does the package do this by default now?
 	t.cfg.MemberlistKV.NodeName = hostname + "-" + uuid.New().String()
 
+	t.memberlistKV = memberlist.NewKVInitService(&t.cfg.MemberlistKV)
+
 	t.cfg.Ingester.LifecyclerConfig.RingConfig.KVStore.MemberlistKV = t.memberlistKV.GetMemberlistKV
 	t.cfg.Distributor.DistributorRing.KVStore.MemberlistKV = t.memberlistKV.GetMemberlistKV
 	t.cfg.Compactor.ShardingRing.KVStore.MemberlistKV = t.memberlistKV.GetMemberlistKV
 
-	t.memberlistKV = memberlist.NewKVInitService(&t.cfg.MemberlistKV)
 	return t.memberlistKV, nil
 }
 
@@ -199,7 +200,7 @@ func (t *App) setupModuleManager() error {
 		// MemberlistKV: nil,
 		Ring:        {Server, MemberlistKV},
 		Distributor: {Ring, Server, Overrides},
-		Ingester:    {Store, Server, MemberlistKV},
+		Ingester:    {Store, Server, Overrides, MemberlistKV},
 		Querier:     {Store, Ring},
 		Compactor:   {Store, Server, MemberlistKV},
 		All:         {Compactor, Querier, Ingester, Distributor},
