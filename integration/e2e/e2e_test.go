f732874d1b1fa77f3458dc151b39be383b01dd31
Joe Elliott <joe.elliott@grafana.com>
2020-09-29 12:24:30 -0400

    Eventual consistency with replication factor of 2 (#190)
    * Added support for eventual consistency on repl factor = 2
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * Moved existing e2e test to all-in-one
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * moved service creation methods into util
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * Added microservices repl factor 2 tests
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * Test reduced ingesters
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * factored out query code
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * lint
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * Added more timing protection
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * renamed strategy
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * increased test timeout
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- integration/e2e/e2e_test.go
+++ integration/e2e/e2e_test.go
@@ -3,73 +3,21 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"math/rand"
-	"path/filepath"
 	"testing"
 	"time"
 
 	cortex_e2e "github.com/cortexproject/cortex/integration/e2e"
 	cortex_e2e_db "github.com/cortexproject/cortex/integration/e2e/db"
-	"github.com/golang/protobuf/jsonpb"
-	"github.com/grafana/tempo/pkg/tempopb"
-	jaeger_grpc "github.com/jaegertracing/jaeger/cmd/agent/app/reporter/grpc"
-	thrift "github.com/jaegertracing/jaeger/thrift-gen/jaeger"
-	"github.com/stretchr/testify/assert"
+	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
-	"google.golang.org/grpc"
 )
 
-var (
-	svcName = "tempo"
-	image   = "tempo:latest"
+const (
+	configAllInOne      = "./config-all-in-one.yaml"
+	configMicroservices = "./config-microservices.yaml"
 )
 
-func NewTempoAllInOne() (*cortex_e2e.HTTPService, error) {
-	args := "-config.file=" + filepath.Join(cortex_e2e.ContainerSharedDir, "config.yaml")
-
-	return cortex_e2e.NewHTTPService(
-		svcName,
-		image,
-		cortex_e2e.NewCommandWithoutEntrypoint("/tempo", args),
-		cortex_e2e.NewHTTPReadinessProbe(3100, "/ready", 200, 505),
-		3100,
-		14250,
-	), nil
-}
-
-func NewJaegerGRPCClient(endpoint string) (*jaeger_grpc.Reporter, error) {
-	// new jaeger grpc exporter
-	conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
-	}
-	logger, err := zap.NewDevelopment()
-	if err != nil {
-		return nil, err
-	}
-	return jaeger_grpc.NewReporter(conn, nil, logger), err
-}
-
-func MakeThriftBatch() *thrift.Batch {
-	var spans []*thrift.Span
-	spans = append(spans, &thrift.Span{
-		TraceIdLow:    rand.Int63(),
-		TraceIdHigh:   0,
-		SpanId:        rand.Int63(),
-		ParentSpanId:  0,
-		OperationName: "my operation",
-		References:    nil,
-		Flags:         0,
-		StartTime:     time.Now().Unix(),
-		Duration:      1,
-		Tags:          nil,
-		Logs:          nil,
-	})
-	return &thrift.Batch{Spans: spans}
-}
-
-func TestIngest(t *testing.T) {
+func TestAllInOne(t *testing.T) {
 	s, err := cortex_e2e.NewScenario("tempo_e2e")
 	require.NoError(t, err)
 	defer s.Close()
@@ -78,16 +26,15 @@ func TestIngest(t *testing.T) {
 	require.NotNil(t, minio)
 	require.NoError(t, s.StartAndWaitReady(minio))
 
-	require.NoError(t, copyFileToSharedDir(s, "./config.yaml", "config.yaml"))
-	tempo, err := NewTempoAllInOne()
-	require.NoError(t, err)
+	require.NoError(t, copyFileToSharedDir(s, configAllInOne, "config.yaml"))
+	tempo := newTempoAllInOne()
 	require.NoError(t, s.StartAndWaitReady(tempo))
 
 	// Get port for the otlp receiver endpoint
-	c, err := NewJaegerGRPCClient(tempo.Endpoint(14250))
+	c, err := newJaegerGRPCClient(tempo.Endpoint(14250))
 	require.NoError(t, err)
 	require.NotNil(t, c)
-	batch := MakeThriftBatch()
+	batch := makeThriftBatch()
 	require.NoError(t, c.EmitBatch(context.Background(), batch))
 
 	// test metrics
@@ -99,32 +46,118 @@ func TestIngest(t *testing.T) {
 	require.NoError(t, tempo.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_ingester_traces_created_total"))
 
 	// query an in-memory trace
-	res, err := cortex_e2e.GetRequest("http://" + tempo.Endpoint(3100) + "/api/traces/" + hexID)
-	require.NoError(t, err)
-	out := &tempopb.Trace{}
-	unmarshaller := &jsonpb.Unmarshaler{}
-	assert.NoError(t, unmarshaller.Unmarshal(res.Body, out))
-	assert.Len(t, out.Batches, 1)
-	assert.Equal(t, out.Batches[0].InstrumentationLibrarySpans[0].Spans[0].Name, "my operation")
-	defer res.Body.Close()
+	queryAndAssertTrace(t, "http://"+tempo.Endpoint(3100)+"/api/traces/"+hexID, "my operation", 1)
 
 	// flush trace to backend
-	res, err = cortex_e2e.GetRequest("http://" + tempo.Endpoint(3100) + "/flush")
+	res, err := cortex_e2e.GetRequest("http://" + tempo.Endpoint(3100) + "/flush")
 	require.NoError(t, err)
 	require.Equal(t, 204, res.StatusCode)
 
 	// sleep for one maintenance cycle
-	time.Sleep(2 * time.Second)
+	time.Sleep(5 * time.Second)
 
 	// test metrics
 	require.NoError(t, tempo.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_ingester_blocks_flushed_total"))
 	require.NoError(t, tempo.WaitSumMetrics(cortex_e2e.Equals(1), "tempodb_blocklist_length"))
 
 	// query trace - should fetch from backend
-	resp, err := cortex_e2e.GetRequest("http://" + tempo.Endpoint(3100) + "/api/traces/" + hexID)
+	queryAndAssertTrace(t, "http://"+tempo.Endpoint(3100)+"/api/traces/"+hexID, "my operation", 1)
+}
+
+func TestMicroservices(t *testing.T) {
+	s, err := cortex_e2e.NewScenario("tempo_e2e")
 	require.NoError(t, err)
-	assert.NoError(t, unmarshaller.Unmarshal(resp.Body, out))
-	assert.Len(t, out.Batches, 1)
-	assert.Equal(t, out.Batches[0].InstrumentationLibrarySpans[0].Spans[0].Name, "my operation")
-	defer resp.Body.Close()
+	defer s.Close()
+
+	minio := cortex_e2e_db.NewMinio(9000, "tempo")
+	require.NotNil(t, minio)
+	require.NoError(t, s.StartAndWaitReady(minio))
+
+	require.NoError(t, copyFileToSharedDir(s, configMicroservices, "config.yaml"))
+	tempoIngester1 := newTempoIngester(1)
+	require.NoError(t, s.StartAndWaitReady(tempoIngester1))
+
+	tempoIngester2 := newTempoIngester(2)
+	require.NoError(t, s.StartAndWaitReady(tempoIngester2))
+
+	tempoDistributor := newTempoDistributor()
+	require.NoError(t, s.StartAndWaitReady(tempoDistributor))
+
+	tempoQuerier := newTempoQuerier()
+	require.NoError(t, s.StartAndWaitReady(tempoQuerier))
+
+	// wait for 2 active ingesters
+	time.Sleep(1 * time.Second)
+	matchers := []*labels.Matcher{
+		{
+			Type:  labels.MatchEqual,
+			Name:  "name",
+			Value: "ingester",
+		},
+		{
+			Type:  labels.MatchEqual,
+			Name:  "state",
+			Value: "ACTIVE",
+		},
+	}
+	require.NoError(t, tempoDistributor.WaitSumMetricsWithOptions(cortex_e2e.Equals(2), []string{`cortex_ring_members`}, cortex_e2e.WithLabelMatchers(matchers...)))
+
+	// Get port for the otlp receiver endpoint
+	c, err := newJaegerGRPCClient(tempoDistributor.Endpoint(14250))
+	require.NoError(t, err)
+	require.NotNil(t, c)
+	batch := makeThriftBatch()
+
+	require.NoError(t, c.EmitBatch(context.Background(), batch))
+
+	// test metrics
+	require.NoError(t, tempoDistributor.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_distributor_spans_received_total"))
+
+	hexID := fmt.Sprintf("%016x%016x", batch.Spans[0].TraceIdHigh, batch.Spans[0].TraceIdLow)
+
+	// ensure trace is created in ingester (trace_idle_time has passed)
+	require.NoError(t, tempoIngester1.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_ingester_traces_created_total"))
+	require.NoError(t, tempoIngester2.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_ingester_traces_created_total"))
+
+	// query an in-memory trace
+	queryAndAssertTrace(t, "http://"+tempoQuerier.Endpoint(3100)+"/api/traces/"+hexID, "my operation", 1)
+
+	// flush trace to backend
+	res, err := cortex_e2e.GetRequest("http://" + tempoIngester1.Endpoint(3100) + "/flush")
+	require.NoError(t, err)
+	require.Equal(t, 204, res.StatusCode)
+
+	res, err = cortex_e2e.GetRequest("http://" + tempoIngester2.Endpoint(3100) + "/flush")
+	require.NoError(t, err)
+	require.Equal(t, 204, res.StatusCode)
+
+	// sleep for one maintenance cycle
+	time.Sleep(5 * time.Second)
+
+	// test metrics
+	require.NoError(t, tempoIngester1.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_ingester_blocks_flushed_total"))
+	require.NoError(t, tempoIngester1.WaitSumMetrics(cortex_e2e.Equals(2), "tempodb_blocklist_length"))
+	require.NoError(t, tempoIngester2.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_ingester_blocks_flushed_total"))
+	require.NoError(t, tempoIngester2.WaitSumMetrics(cortex_e2e.Equals(2), "tempodb_blocklist_length"))
+
+	// query trace - should fetch from backend
+	queryAndAssertTrace(t, "http://"+tempoQuerier.Endpoint(3100)+"/api/traces/"+hexID, "my operation", 1)
+
+	// stop an ingester and confirm we can still write and query
+	err = tempoIngester2.Stop()
+	require.NoError(t, err)
+
+	batch = makeThriftBatch()
+	require.NoError(t, c.EmitBatch(context.Background(), batch))
+	hexID = fmt.Sprintf("%016x%016x", batch.Spans[0].TraceIdHigh, batch.Spans[0].TraceIdLow)
+
+	// query an in-memory trace
+	queryAndAssertTrace(t, "http://"+tempoQuerier.Endpoint(3100)+"/api/traces/"+hexID, "my operation", 1)
+
+	// stop another ingester and confirm things fail
+	err = tempoIngester1.Stop()
+	require.NoError(t, err)
+
+	batch = makeThriftBatch()
+	require.Error(t, c.EmitBatch(context.Background(), batch))
 }
---
+++ pkg/ring/ring.go
@@ -0,0 +1,87 @@
+package ring
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/cortexproject/cortex/pkg/ring"
+	"github.com/cortexproject/cortex/pkg/ring/kv"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// New creates a new distributed consistent hash ring.  It shadows the cortex
+// ring.New method so we can use our own replication strategy for repl factor = 2
+func New(cfg ring.Config, name, key string, reg prometheus.Registerer) (*ring.Ring, error) {
+	if cfg.ReplicationFactor == 2 {
+		return newEventuallyConsistentRing(cfg, name, key, reg)
+	}
+
+	return ring.New(cfg, name, key, reg)
+}
+
+func newEventuallyConsistentRing(cfg ring.Config, name, key string, reg prometheus.Registerer) (*ring.Ring, error) {
+	codec := ring.GetCodec()
+	// Suffix all client names with "-ring" to denote this kv client is used by the ring
+	store, err := kv.NewClient(
+		cfg.KVStore,
+		codec,
+		kv.RegistererWithKVName(reg, name+"-ring"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return ring.NewWithStoreClientAndStrategy(cfg, name, key, store, &EventuallyConsistentStrategy{})
+}
+
+// EventuallyConsistentStrategy represents a repl strategy with a consistency of 1 on read and
+// write.  Note this is NOT strongly consistent!  It is _eventually_ consistent :)
+type EventuallyConsistentStrategy struct {
+}
+
+// Filter decides, given the set of ingesters eligible for a key,
+// which ingesters you will try and write to and how many failures you will
+// tolerate.
+// - Filters out dead ingesters so the one doesn't even try to write to them.
+// - Checks there is enough ingesters for an operation to succeed.
+// The ingesters argument may be overwritten.
+func (s *EventuallyConsistentStrategy) Filter(ingesters []ring.IngesterDesc, op ring.Operation, replicationFactor int, heartbeatTimeout time.Duration) ([]ring.IngesterDesc, int, error) {
+	minSuccess := 1
+
+	// Skip those that have not heartbeated in a while. NB these are still
+	// included in the calculation of minSuccess, so if too many failed ingesters
+	// will cause the whole write to fail.
+	for i := 0; i < len(ingesters); {
+		if ingesters[i].IsHealthy(op, heartbeatTimeout) {
+			i++
+		} else {
+			ingesters = append(ingesters[:i], ingesters[i+1:]...)
+		}
+	}
+
+	// This is just a shortcut - if there are not minSuccess available ingesters,
+	// after filtering out dead ones, don't even bother trying.
+	if len(ingesters) < minSuccess {
+		err := fmt.Errorf("at least %d live replicas required, could only find %d",
+			minSuccess, len(ingesters))
+		return nil, 0, err
+	}
+
+	return ingesters, len(ingesters) - minSuccess, nil
+}
+
+func (s *EventuallyConsistentStrategy) ShouldExtendReplicaSet(ingester ring.IngesterDesc, op ring.Operation) bool {
+	// We do not want to Write to Ingesters that are not ACTIVE, but we do want
+	// to write the extra replica somewhere.  So we increase the size of the set
+	// of replicas for the key. This means we have to also increase the
+	// size of the replica set for read, but we can read from Leaving ingesters,
+	// so don't skip it in this case.
+	// NB dead ingester will be filtered later by DefaultReplicationStrategy.Filter().
+	if op == ring.Write && ingester.State != ring.ACTIVE {
+		return true
+	} else if op == ring.Read && (ingester.State != ring.ACTIVE && ingester.State != ring.LEAVING) {
+		return true
+	}
+
+	return false
+}
