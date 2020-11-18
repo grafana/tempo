d53dc86903344bf17f130f4ca610ae0de5bbc2fa
Joe Elliott <number101010@gmail.com>
2020-11-18 10:57:39 -0500

    fixed double traceID in log messages
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tempodb/tempodb.go
+++ tempodb/tempodb.go
@@ -318,7 +318,7 @@ func (rw *readerWriter) Find(ctx context.Context, tenantID string, id encoding.I
 				break
 			}
 		}
-		level.Info(logger).Log("msg", "searching for trace in block", "traceID", hex.EncodeToString(id), "block", meta.BlockID, "found", foundObject != nil)
+		level.Info(logger).Log("msg", "searching for trace in block", "findTraceID", hex.EncodeToString(id), "block", meta.BlockID, "found", foundObject != nil)
 		span.LogFields(ot_log.String("msg", "searching for trace in block"), ot_log.String("traceID", hex.EncodeToString(id)), ot_log.String("block", meta.BlockID.String()), ot_log.Bool("found", foundObject != nil))
 		if foundObject != nil {
 			span.SetTag("object bytes", len(foundObject))
