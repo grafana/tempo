7cee618c0bbf4a384dc4a3b48636a5b778b2e9a8
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2020-12-10 18:37:24 +0100

    Reemove s3 region config flag
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- tempodb/backend/s3/config.go
+++ tempodb/backend/s3/config.go
@@ -3,7 +3,6 @@ package s3
 type Config struct {
 	Bucket    string `yaml:"bucket"`
 	Endpoint  string `yaml:"endpoint"`
-	Region    string `yaml:"region"`
 	AccessKey string `yaml:"access_key"`
 	SecretKey string `yaml:"secret_key"`
 	Insecure  bool   `yaml:"insecure"`
