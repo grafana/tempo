7da27af58bf6a3b58382e880adb9f4d856a0f640
Annanay <annanayagarwal@gmail.com>
2020-12-17 13:47:53 +0530

    Address comments, add querier.frontend-address flag
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

--- modules/frontend/querysharding_test.go
+++ modules/frontend/querysharding_test.go
@@ -34,7 +34,7 @@ func TestCreateBlockShards(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			bb := createBlockShards(tt.queryShards)
+			bb := createBlockBoundaries(tt.queryShards)
 			assert.Len(t, bb, len(tt.expected))
 
 			for i := 0; i < len(bb); i++ {
