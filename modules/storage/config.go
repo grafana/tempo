3a32677f33ffaa4b1ca30f776e41fd81ddf216cb
Martin Disibio <mdisibio@gmail.com>
2021-01-29 09:24:01 -0500

    Update tempo-cli to use same shared BoundedWaitGroup. Fix cli parameter name

--- modules/storage/config.go
+++ modules/storage/config.go
@@ -25,7 +25,7 @@ const DefaultBlocklistPoll = 5 * time.Minute
 func (cfg *Config) RegisterFlagsAndApplyDefaults(prefix string, f *flag.FlagSet) {
 	f.StringVar(&cfg.Trace.Backend, util.PrefixConfig(prefix, "trace.backend"), "", "Trace backend (s3, azure, gcs, local)")
 	f.DurationVar(&cfg.Trace.BlocklistPoll, util.PrefixConfig(prefix, "trace.maintenance-cycle"), DefaultBlocklistPoll, "Period at which to run the maintenance cycle.")
-	f.UintVar(&cfg.Trace.BlocklistPollConcurrency, util.PrefixConfig(prefix, "trace.maintenance-cycle"), tempodb.DefaultBlocklistPollConcurrency, "Number of concurrent requests to backend storage when performing blocklist polling.")
+	f.UintVar(&cfg.Trace.BlocklistPollConcurrency, util.PrefixConfig(prefix, "trace.blocklist-poll-concurrency"), tempodb.DefaultBlocklistPollConcurrency, "Number of concurrent requests to backend storage when performing blocklist polling.")
 
 	cfg.Trace.WAL = &wal.Config{}
 	f.StringVar(&cfg.Trace.WAL.Filepath, util.PrefixConfig(prefix, "trace.wal.path"), "/var/tempo/wal", "Path at which store WAL blocks.")
