fe5612d394ba7a50b2aec63bf981ff6b3bb0ac3f
Annanay <annanayagarwal@gmail.com>
2021-04-20 15:48:56 +0530

    Add tests on limits format
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

---
+++ modules/overrides/limits_test.go
@@ -0,0 +1,52 @@
+package overrides
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+)
+
+// Copied from Cortex
+func TestLimitsTagsYamlMatchJson(t *testing.T) {
+	limits := reflect.TypeOf(Limits{})
+	n := limits.NumField()
+	var mismatch []string
+
+	for i := 0; i < n; i++ {
+		field := limits.Field(i)
+
+		// Note that we aren't requiring YAML and JSON tags to match, just that
+		// they either both exist or both don't exist.
+		hasYAMLTag := field.Tag.Get("yaml") != ""
+		hasJSONTag := field.Tag.Get("json") != ""
+
+		if hasYAMLTag != hasJSONTag {
+			mismatch = append(mismatch, field.Name)
+		}
+	}
+
+	assert.Empty(t, mismatch, "expected no mismatched JSON and YAML tags")
+}
+
+// Copied from Cortex and modified
+func TestLimitsStringDurationYamlMatchJson(t *testing.T) {
+	inputYAML := `
+ingestion_rate_strategy: global
+ingestion_rate_limit_bytes: 100_000
+`
+	inputJSON := `{"ingestion_rate_strategy": "global", "ingestion_rate_limit_bytes": 100000}`
+
+	limitsYAML := Limits{}
+	err := yaml.Unmarshal([]byte(inputYAML), &limitsYAML)
+	require.NoError(t, err, "expected to be able to unmarshal from YAML")
+
+	limitsJSON := Limits{}
+	err = json.Unmarshal([]byte(inputJSON), &limitsJSON)
+	require.NoError(t, err, "expected to be able to unmarshal from JSON")
+
+	assert.Equal(t, limitsYAML, limitsJSON)
+}
