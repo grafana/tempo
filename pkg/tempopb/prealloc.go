c822a14ea84511b61357c3b93057e78f970da23e
Annanay <annanayagarwal@gmail.com>
2021-04-26 20:46:13 +0530

    Preallocate byte slices from prom pool
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

---
+++ pkg/tempopb/prealloc.go
@@ -0,0 +1,45 @@
+package tempopb
+
+import (
+	"github.com/prometheus/prometheus/pkg/pool"
+)
+
+var (
+	// buckets: [1KiB, 2KiB, 4KiB, 8KiB, 16KiB]
+	bytePool = pool.New(1_000, 16_000, 2, func(size int) interface{} { return make([]byte, 0, size) })
+)
+
+// PreallocRequest is a (repeated bytes requests) which preallocs slices on Unmarshal.
+type PreallocRequest struct {
+	Request []byte
+}
+
+// Unmarshal implements proto.Message.
+func (r *PreallocRequest) Unmarshal(dAtA []byte) error {
+	r.Request = bytePool.Get(len(dAtA)).([]byte)
+	r.Request = r.Request[:len(dAtA)]
+	copy(r.Request, dAtA)
+	return nil
+}
+
+// MarshalTo implements proto.Marshaller.
+func (r *PreallocRequest) MarshalTo(dAtA []byte) (int, error) {
+	copy(dAtA[:], r.Request[:])
+	return 0, nil  // returned int is not used
+}
+
+// Size implements proto.Sizer.
+func (r *PreallocRequest) Size() (n int) {
+	if r == nil {
+		return 0
+	}
+	return len(r.Request)
+}
+
+// ReuseRequest puts the byte slice back into bytePool for reuse.
+func ReuseRequest(req *PushBytesRequest) {
+	for i, _ := range req.Requests {
+		// We want to preserve the underlying allocated memory, [:0] helps us retains the cap() of the slice
+		bytePool.Put(req.Requests[i].Request[:0])
+	}
+}
