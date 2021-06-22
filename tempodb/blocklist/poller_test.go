5c820fb535bb9844801689435aefed1aafc03031
Joe Elliott <number101010@gmail.com>
2021-06-22 14:29:15 -0400

    Added poller and tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ tempodb/blocklist/poller_test.go
@@ -0,0 +1,237 @@
+package blocklist
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/grafana/tempo/tempodb/backend"
+	"github.com/grafana/tempo/tempodb/backend/util"
+	"github.com/stretchr/testify/assert"
+)
+
+var testPollConcurrency = uint(10)
+
+func TestDo(t *testing.T) {
+	tests := []struct {
+		name                  string
+		list                  PerTenant
+		compactedList         PerTenantCompacted
+		expectedList          PerTenant
+		expectedCompactedList PerTenantCompacted
+		expectsError          bool
+	}{
+		{
+			name:                  "nothing!",
+			expectedList:          PerTenant{},
+			expectedCompactedList: PerTenantCompacted{},
+		},
+		{
+			name: "err",
+			list: PerTenant{
+				"test": []*backend.BlockMeta{
+					{
+						BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					},
+				},
+			},
+			expectsError: true,
+		},
+		{
+			name: "block meta",
+			list: PerTenant{
+				"test": []*backend.BlockMeta{
+					{
+						BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					},
+				},
+			},
+			expectedList: PerTenant{
+				"test": []*backend.BlockMeta{
+					{
+						BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					},
+				},
+			},
+			expectedCompactedList: PerTenantCompacted{
+				"test": []*backend.CompactedBlockMeta{},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			c := newMockCompactor(tc.compactedList, tc.expectsError)
+			r := newMockReader(tc.list, tc.compactedList, tc.expectsError)
+
+			poller := NewPoller(testPollConcurrency, r, c)
+			actualList, actualCompactedList, err := poller.Do()
+
+			assert.Equal(t, tc.expectedList, actualList)
+			assert.Equal(t, tc.expectedCompactedList, actualCompactedList)
+			if tc.expectsError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestPollBlock(t *testing.T) {
+	tests := []struct {
+		name                  string
+		list                  PerTenant
+		compactedList         PerTenantCompacted
+		pollTenantID          string
+		pollBlockID           uuid.UUID
+		expectedMeta          *backend.BlockMeta
+		expectedCompactedMeta *backend.CompactedBlockMeta
+		expectsError          bool
+	}{
+		{
+			name:         "block and tenant don't exist",
+			pollTenantID: "test",
+			pollBlockID:  uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+		},
+		{
+			name:         "block exists",
+			pollTenantID: "test",
+			pollBlockID:  uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+			list: PerTenant{
+				"test": []*backend.BlockMeta{
+					{
+						BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+					},
+				},
+			},
+			expectedMeta: &backend.BlockMeta{
+				BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+			},
+		},
+		{
+			name:         "compactedblock exists",
+			pollTenantID: "test",
+			pollBlockID:  uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+			compactedList: PerTenantCompacted{
+				"test": []*backend.CompactedBlockMeta{
+					{
+						BlockMeta: backend.BlockMeta{
+							BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+						},
+					},
+				},
+			},
+			expectedCompactedMeta: &backend.CompactedBlockMeta{
+				BlockMeta: backend.BlockMeta{
+					BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+				},
+			},
+		},
+		{
+			name:         "errors",
+			pollTenantID: "test",
+			pollBlockID:  uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+			compactedList: PerTenantCompacted{
+				"test": []*backend.CompactedBlockMeta{
+					{
+						BlockMeta: backend.BlockMeta{
+							BlockID: uuid.MustParse("00000000-0000-0000-0000-000000000001"),
+						},
+					},
+				},
+			},
+			expectsError: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			c := newMockCompactor(tc.compactedList, tc.expectsError)
+			r := newMockReader(tc.list, nil, tc.expectsError)
+
+			poller := NewPoller(testPollConcurrency, r, c)
+			actualMeta, actualCompactedMeta, err := poller.pollBlock(context.Background(), tc.pollTenantID, tc.pollBlockID)
+
+			assert.Equal(t, tc.expectedMeta, actualMeta)
+			assert.Equal(t, tc.expectedCompactedMeta, actualCompactedMeta)
+			if tc.expectsError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func newMockCompactor(list PerTenantCompacted, expectsError bool) backend.Compactor {
+	return &util.MockCompactor{
+		BlockMetaFn: func(blockID uuid.UUID, tenantID string) (*backend.CompactedBlockMeta, error) {
+			if expectsError {
+				return nil, errors.New("!!")
+			}
+
+			l, ok := list[tenantID]
+			if !ok {
+				return nil, backend.ErrMetaDoesNotExist
+			}
+
+			for _, m := range l {
+				if m.BlockID == blockID {
+					return m, nil
+				}
+			}
+
+			return nil, backend.ErrMetaDoesNotExist
+		},
+	}
+}
+
+func newMockReader(list PerTenant, compactedList PerTenantCompacted, expectsError bool) backend.Reader {
+	tenants := []string{}
+	for t := range list {
+		tenants = append(tenants, t)
+	}
+	for t := range compactedList {
+		tenants = append(tenants, t)
+	}
+
+	return &util.MockReader{
+		T: tenants,
+		BlockFn: func(ctx context.Context, tenantID string) ([]uuid.UUID, error) {
+			if expectsError {
+				return nil, errors.New("!!")
+			}
+			blocks := list[tenantID]
+			uuids := []uuid.UUID{}
+			for _, b := range blocks {
+				uuids = append(uuids, b.BlockID)
+			}
+			compactedBlocks := compactedList[tenantID]
+			for _, b := range compactedBlocks {
+				uuids = append(uuids, b.BlockID)
+			}
+
+			return uuids, nil
+		},
+		BlockMetaFn: func(ctx context.Context, blockID uuid.UUID, tenantID string) (*backend.BlockMeta, error) {
+			if expectsError {
+				return nil, errors.New("!!")
+			}
+
+			l, ok := list[tenantID]
+			if !ok {
+				return nil, backend.ErrMetaDoesNotExist
+			}
+
+			for _, m := range l {
+				if m.BlockID == blockID {
+					return m, nil
+				}
+			}
+
+			return nil, backend.ErrMetaDoesNotExist
+		},
+	}
+}
