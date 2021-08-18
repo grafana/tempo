6e2e59efedfd13d04e842072774518a3c127c2b0
Zach Leslie <zach.leslie@grafana.com>
2021-08-18 15:12:50 -0600

    Include test for vulture helper

---
+++ cmd/tempo-vulture/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/grafana/tempo/pkg/tempopb"
+	v1 "github.com/grafana/tempo/pkg/tempopb/trace/v1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHasMissingSpans(t *testing.T) {
+	cases := []struct {
+		trace   *tempopb.Trace
+		expeted bool
+	}{
+		{
+			&tempopb.Trace{
+				Batches: []*v1.ResourceSpans{
+					&v1.ResourceSpans{
+						InstrumentationLibrarySpans: []*v1.InstrumentationLibrarySpans{
+							{
+								Spans: []*v1.Span{
+									{
+										ParentSpanId: []byte("01234"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			true,
+		},
+		{
+			&tempopb.Trace{
+				Batches: []*v1.ResourceSpans{
+					&v1.ResourceSpans{
+						InstrumentationLibrarySpans: []*v1.InstrumentationLibrarySpans{
+							{
+								Spans: []*v1.Span{
+									{
+										SpanId: []byte("01234"),
+									},
+									{
+										ParentSpanId: []byte("01234"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
+	}
+
+	for _, tc := range cases {
+		require.Equal(t, tc.expeted, hasMissingSpans(tc.trace))
+	}
+}
