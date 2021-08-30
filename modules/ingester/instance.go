2a40702e9b123afab3c411b4839840a12d008956
Martin Disibio <mdisibio@gmail.com>
2021-08-30 09:36:37 -0400

    Default search to snappy encoding
    Signed-off-by: Martin Disibio <mdisibio@gmail.com>

--- modules/ingester/instance.go
+++ modules/ingester/instance.go
@@ -269,7 +269,7 @@ func (i *instance) CompleteBlock(blockID uuid.UUID) error {
 
 	var newSearch search.SearchBlock
 	if oldSearch != nil {
-		err = search.NewBackendSearchBlock(oldSearch.b, i.local, backendBlock.BlockMeta().BlockID, backendBlock.BlockMeta().TenantID, backend.EncNone, 0)
+		err = search.NewBackendSearchBlock(oldSearch.b, i.local, backendBlock.BlockMeta().BlockID, backendBlock.BlockMeta().TenantID, backend.EncSnappy, 0)
 		if err != nil {
 			return err
 		}
