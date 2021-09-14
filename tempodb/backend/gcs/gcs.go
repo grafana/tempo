b68b0380d87239114b245bcec94c2cb9bfb1aa92
Joe Elliott <number101010@gmail.com>
2021-09-14 10:33:26 -0400

    disable compression
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tempodb/backend/gcs/gcs.go
+++ tempodb/backend/gcs/gcs.go
@@ -215,6 +215,7 @@ func (rw *readerWriter) readRange(ctx context.Context, name string, offset int64
 func createBucket(ctx context.Context, cfg *Config, hedge bool) (*storage.BucketHandle, error) {
 	// start with default transport
 	customTransport := http.DefaultTransport.(*http.Transport).Clone()
+	customTransport.DisableCompression = true
 
 	// add google auth
 	transportOptions := []option.ClientOption{
