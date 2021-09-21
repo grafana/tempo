e498f3f6f346b8acf391c2ef4e33646170d1c7fb
Joe Elliott <number101010@gmail.com>
2021-09-21 10:19:46 -0400

    Added label scan
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/tempo-cli/cmd-list-block.go
+++ cmd/tempo-cli/cmd-list-block.go
@@ -7,14 +7,29 @@ import (
 	"io"
 	"os"
 	"os/signal"
+	"sort"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/dustin/go-humanize"
 	"github.com/google/uuid"
+	"github.com/grafana/tempo/pkg/model"
+	"github.com/grafana/tempo/pkg/tempopb"
+	v1 "github.com/grafana/tempo/pkg/tempopb/common/v1"
 	tempodb_backend "github.com/grafana/tempo/tempodb/backend"
 	"github.com/grafana/tempo/tempodb/encoding"
 )
+
+type valueStats struct {
+	count int
+}
+type values struct {
+	all   map[string]valueStats
+	key   string
+	count int
+}
+type kvPairs map[string]values
 
 type listBlockCmd struct {
 	backendOptions
@@ -85,6 +100,7 @@ func dumpBlock(r tempodb_backend.Reader, c tempodb_backend.Compactor, tenantID s
 		maxObjSize := 0
 		minObjSize := 0
 
+		allKVP := kvPairs{}
 		printStats := func() {
 			fmt.Println()
 			fmt.Println("Scanning results:")
@@ -92,6 +108,8 @@ func dumpBlock(r tempodb_backend.Reader, c tempodb_backend.Compactor, tenantID s
 			fmt.Println("Duplicates      : ", dupe)
 			fmt.Println("Smallest object : ", humanize.Bytes(uint64(minObjSize)))
 			fmt.Println("Largest object  : ", humanize.Bytes(uint64(maxObjSize)))
+			fmt.Println("")
+			printKVPairs(allKVP)
 		}
 
 		// Print stats on ctrl+c
@@ -127,6 +145,19 @@ func dumpBlock(r tempodb_backend.Reader, c tempodb_backend.Compactor, tenantID s
 			}
 
 			copy(prevID, objID)
+
+			trace, err := model.Unmarshal(obj, meta.DataEncoding)
+			if err != nil {
+				return err
+			}
+			kvp := extractKVPairs(trace)
+			for k, vs := range kvp {
+				addKey(allKVP, k, 1)
+				for v := range vs.all {
+					addVal(allKVP, k, v, 1)
+				}
+			}
+
 			i++
 			if i%100000 == 0 {
 				fmt.Println("Record: ", i)
@@ -137,4 +168,105 @@ func dumpBlock(r tempodb_backend.Reader, c tempodb_backend.Compactor, tenantID s
 	}
 
 	return nil
+}
+
+// helper methods for calculating label stats
+func printKVPairs(kvp kvPairs) {
+	allValues := make([]values, 0, len(kvp))
+	for _, vs := range kvp {
+		allValues = append(allValues, vs)
+	}
+	sort.Slice(allValues, func(i, j int) bool {
+		return relativeValue(allValues[i]) > relativeValue(allValues[j])
+	})
+	for _, vs := range allValues {
+		fmt.Println("key:", vs.key, "count:", vs.count, "len:", len(vs.all), "value:", relativeValue(vs))
+		for a, c := range vs.all {
+			fmt.Printf(" %s:\t%.2f\n", a, float64(c.count)/float64(vs.count))
+		}
+	}
+}
+
+// attempts to calculate the "value" that storing a given label would provide by. currently (number of times appeared)^2 / cardinality
+//  this is not researched and could definitely be improved
+func relativeValue(v values) float64 {
+	return (float64(v.count) * float64(v.count)) / float64(len(v.all))
+}
+func extractKVPairs(t *tempopb.Trace) kvPairs {
+	kvp := kvPairs{}
+	for _, b := range t.Batches {
+		spanCount := 0
+		for _, ils := range b.InstrumentationLibrarySpans {
+			for _, s := range ils.Spans {
+				spanCount++
+				for _, a := range s.Attributes {
+					val, yay := stringVal(a.Value)
+					if !yay {
+						continue
+					}
+					addKey(kvp, a.Key, 1)
+					addVal(kvp, a.Key, val, 1)
+				}
+			}
+		}
+		for _, a := range b.Resource.Attributes {
+			val, yay := stringVal(a.Value)
+			if !yay {
+				continue
+			}
+			addKey(kvp, a.Key, spanCount)
+			addVal(kvp, a.Key, val, spanCount)
+		}
+	}
+	return kvp
+}
+func addKey(kvp kvPairs, key string, count int) {
+	v, ok := kvp[key]
+	if !ok {
+		v = values{
+			all: map[string]valueStats{},
+			key: key,
+		}
+	}
+	v.count += count
+	kvp[key] = v
+}
+func addVal(kvp kvPairs, key string, val string, count int) {
+	v := kvp[key]
+	stats, ok := v.all[val]
+	if !ok {
+		stats = valueStats{
+			count: 0,
+		}
+	}
+	stats.count += count
+	v.all[val] = stats
+	kvp[key] = v
+}
+func stringVal(v *v1.AnyValue) (string, bool) {
+	if sVal, ok := v.Value.(*v1.AnyValue_StringValue); ok {
+		return sVal.StringValue, true
+	}
+	if nVal, ok := v.Value.(*v1.AnyValue_IntValue); ok {
+		return strconv.FormatInt(nVal.IntValue, 10), true
+	}
+	if dVal, ok := v.Value.(*v1.AnyValue_DoubleValue); ok {
+		return fmt.Sprintf("%f", dVal.DoubleValue), true
+		// strconv.FormatFloat()
+	}
+	if bVal, ok := v.Value.(*v1.AnyValue_BoolValue); ok {
+		if bVal.BoolValue {
+			return "true", true
+		} else {
+			return "false", true
+		}
+	}
+	// todo? add support for these?
+	// if kVal, ok := v.Value.(*v1.AnyValue_KvlistValue); ok {
+	//  return fmt.Sprintf("kvval %v", kVal.KvlistValue) // better way?
+	// }
+	// if aVal, ok := v.Value.(*v1.AnyValue_ArrayValue); ok {
+	//  return fmt.Sprintf("arrayval %v", aVal.ArrayValue) // better way?
+	// }
+	return "", false
 }
