f1a05b990f9d9bdc38b54df44e1d8aebe6b27b96
Joe Elliott <number101010@gmail.com>
2021-09-21 10:49:30 -0400

    docs
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ tempodb/encoding/iterator_prefetch_test.go
@@ -0,0 +1,102 @@
+package encoding
+
+import (
+	"context"
+	"errors"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/grafana/tempo/tempodb/encoding/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrefetchIterates(t *testing.T) {
+	tests := []struct {
+		ids  []common.ID
+		objs [][]byte
+		err  error
+	}{
+		{
+			ids: []common.ID{
+				{0x01},
+				{0x02},
+				{0x03},
+			},
+			objs: [][]byte{
+				{0x05},
+				{0x06},
+				{0x06},
+			},
+			err: nil,
+		},
+		{
+			ids: []common.ID{
+				{0x01},
+				{0x02},
+				{0x03},
+			},
+			objs: [][]byte{
+				{0x05},
+				{0x06},
+				{0x06},
+			},
+			err: errors.New("!!"),
+		},
+	}
+
+	ctx := context.Background()
+	for _, tc := range tests {
+		iter := &testIterator{}
+		for i := 0; i < len(tc.ids); i++ {
+			iter.Add(tc.ids[i], tc.objs[i], tc.err)
+		}
+		prefetchIter := NewPrefetchIterator(ctx, iter, 10)
+
+		count := 0
+		for {
+			id, obj, err := prefetchIter.Next(context.TODO())
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				assert.Equal(t, tc.err, err)
+				break
+			}
+			assert.Equal(t, tc.ids[count], id)
+			assert.Equal(t, tc.objs[count], obj)
+			count++
+		}
+
+		if tc.err == nil {
+			assert.Equal(t, len(tc.ids), count)
+		}
+	}
+}
+
+func TestPrefetchPrefetches(t *testing.T) {
+	ctx := context.Background()
+
+	iter := &testIterator{}
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+
+	_ = NewPrefetchIterator(ctx, iter, 10)
+	time.Sleep(100 * time.Millisecond)
+	assert.Equal(t, 3, iter.i) // prefetch all 3
+
+	iter = &testIterator{}
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+	iter.Add([]byte{0x01}, []byte{0x01}, nil)
+
+	prefetchIter := NewPrefetchIterator(ctx, iter, 3)
+	time.Sleep(100 * time.Millisecond)
+	assert.Equal(t, 4, iter.i) // prefetch only the buffer. this happens to be 1 more than the passed buffer. maybe one day we will "correct" that
+	_, _, _ = prefetchIter.Next(ctx)
+	time.Sleep(100 * time.Millisecond)
+	assert.Equal(t, 5, iter.i) // get all
+}
