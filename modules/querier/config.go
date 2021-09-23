38a2aefd31ae4dc82105b2063fdf96f959cc3ca3
Koenraad Verheyden <koenraad.verheyden@grafana.com>
2021-09-23 19:23:09 +0200

    Split up QueryTimeout for trace and search queries

--- modules/querier/config.go
+++ modules/querier/config.go
@@ -11,7 +11,8 @@ import (
 
 // Config for a querier.
 type Config struct {
-	QueryTimeout         time.Duration        `yaml:"query_timeout"`
+	TraceIDQueryTimeout  time.Duration        `yaml:"query_timeout"`
+	SearchQueryTimeout   time.Duration        `yaml:"search_query_timeout"`
 	ExtraQueryDelay      time.Duration        `yaml:"extra_query_delay,omitempty"`
 	MaxConcurrentQueries int                  `yaml:"max_concurrent_queries"`
 	Worker               cortex_worker.Config `yaml:"frontend_worker"`
@@ -19,7 +20,8 @@ type Config struct {
 
 // RegisterFlagsAndApplyDefaults register flags.
 func (cfg *Config) RegisterFlagsAndApplyDefaults(prefix string, f *flag.FlagSet) {
-	cfg.QueryTimeout = 10 * time.Second
+	cfg.TraceIDQueryTimeout = 10 * time.Second
+	cfg.SearchQueryTimeout = 30 * time.Second
 	cfg.ExtraQueryDelay = 0
 	cfg.MaxConcurrentQueries = 5
 	cfg.Worker = cortex_worker.Config{
