6ddf5fffd481f00cb911dfe10f9ab8df48e5c818
Joe Elliott <number101010@gmail.com>
2021-10-18 10:39:16 -0400

    grammar
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/api/http_test.go
+++ pkg/api/http_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// For licensing reasons they strings exist in two packages. This test exists to make sure they don't
+// For licensing reasons these strings exist in two packages. This test exists to make sure they don't
 // drift.
 func TestEquality(t *testing.T) {
 	assert.Equal(t, HeaderAccept, tempo.AcceptHeaderKey)
