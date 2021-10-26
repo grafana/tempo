5f2d55351f9cec69db8874e5f94b9a56d4fbac9f
Koenraad Verheyden <koenraad.verheyden@grafana.com>
2021-10-26 17:37:43 +0200

    Fix compilation errors

--- tempodb/search/streaming_search_block.go
+++ tempodb/search/streaming_search_block.go
@@ -90,23 +90,24 @@ func (s *StreamingSearchBlock) Append(ctx context.Context, id common.ID, searchD
 }
 
 func (s *StreamingSearchBlock) Tags(ctx context.Context, tags map[string]struct{}) error {
-	for k := range s.header.Tags {
+	s.header.Tags.Range(func(k, v string) {
 		if _, ok := tags[k]; !ok {
 			tags[k] = struct{}{}
 		}
-	}
+	})
 	return nil
 }
 
 func (s *StreamingSearchBlock) TagValues(ctx context.Context, tagName string, tagValues map[string]struct{}) error {
-	if values, ok := s.header.Tags[tagName]; ok {
-		for _, v := range values {
-			// TODO use binary search?
+	// TODO optimize to grab all values of tagName
+	s.header.Tags.Range(func(k, v string) {
+		if tagName == k {
 			if _, ok := tagValues[v]; !ok {
 				tagValues[v] = struct{}{}
 			}
+
 		}
-	}
+	})
 	return nil
 }
 
