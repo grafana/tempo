ab3b5b4d17e9712029178a8e91b9628837d9ee96
Martin Disibio <mdisibio@gmail.com>
2021-10-27 14:16:29 -0400

    fix key/value typo

--- modules/ingester/instance_search.go
+++ modules/ingester/instance_search.go
@@ -189,7 +189,7 @@ func (i *instance) SearchTags(ctx context.Context) (*tempopb.SearchTagsResponse,
 	err := i.visitSearchEntriesLiveTraces(ctx, func(entry *tempofb.SearchEntry) {
 		for i, ii := 0, entry.TagsLength(); i < ii; i++ {
 			entry.Tags(kv, i)
-			key := string(kv.Value(i))
+			key := string(kv.Key())
 			// check the tag is already set, this is more performant with repetitive values
 			if _, ok := tags[key]; !ok {
 				tags[key] = struct{}{}
