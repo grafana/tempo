0a43f70d9886727e447b0f9614e7365e5e55f957
Joe Elliott <number101010@gmail.com>
2021-11-19 16:41:27 -0500

    tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/api/http_test.go
+++ pkg/api/http_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/grafana/tempo/cmd/tempo-query/tempo"
 	"github.com/grafana/tempo/pkg/tempopb"
+	"github.com/grafana/tempo/tempodb/backend"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -304,5 +305,45 @@ func TestParseBackendSearchQuerier(t *testing.T) {
 		assert.Equal(t, tc.expectedStartPage, actualStartPage)
 		assert.Equal(t, tc.expectedTotalPages, actualTotalPages)
 		assert.Equal(t, tc.expectedBlockID, actualBlockID)
+	}
+}
+
+// todo(search): improve this test. it is currently thin b/c i expect it to change
+func TestParseBackendSearchServerless(t *testing.T) {
+	tests := []struct {
+		url                   string
+		expectedError         string
+		expectedEncoding      backend.Encoding
+		expectedDataEncoding  string
+		expectedIndexPageSize uint32
+		expectedTotalRecords  uint32
+		expectedTenant        string
+		expectedVersion       string
+	}{
+		{
+			url:                   "/?encoding=none&dataEncoding=v2&indexPageSize=2&totalRecords=3&tenant=yay&version=v3",
+			expectedEncoding:      backend.EncNone,
+			expectedDataEncoding:  "v2",
+			expectedIndexPageSize: 2,
+			expectedTotalRecords:  3,
+			expectedTenant:        "yay",
+			expectedVersion:       "v3",
+		},
+	}
+
+	for _, tc := range tests {
+		r := httptest.NewRequest("GET", tc.url, nil)
+		encoding, dataEncoding, indexPageSize, totalRecords, tenant, version, err := ParseBackendSearchServerless(r)
+
+		if len(tc.expectedError) != 0 {
+			assert.EqualError(t, err, tc.expectedError)
+			continue
+		}
+		assert.Equal(t, tc.expectedEncoding, encoding)
+		assert.Equal(t, tc.expectedDataEncoding, dataEncoding)
+		assert.Equal(t, tc.expectedIndexPageSize, indexPageSize)
+		assert.Equal(t, tc.expectedTotalRecords, totalRecords)
+		assert.Equal(t, tc.expectedTenant, tenant)
+		assert.Equal(t, tc.expectedVersion, version)
 	}
 }
