0aafb87bf4f6b318f74eac22c0181b265c2b1749
Joe Elliott <number101010@gmail.com>
2022-01-06 16:20:14 -0500

    added stuff
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ pkg/model/v0/decoder.go
@@ -0,0 +1,61 @@
+package v0
+
+import (
+	"fmt"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/grafana/tempo/pkg/model/trace"
+	"github.com/grafana/tempo/pkg/tempopb"
+)
+
+const Encoding = ""
+
+type enc struct {
+}
+
+var staticEncoding = &enc{}
+
+func NewEncoding() *enc {
+	return staticEncoding
+}
+
+func (d *enc) Unmarshal(obj []byte) (*tempopb.Trace, error) {
+	trace := &tempopb.Trace{}
+	err := proto.Unmarshal(obj, trace)
+	if err != nil {
+		return nil, err
+	}
+	return trace, err
+}
+
+func (d *enc) Matches(id []byte, obj []byte, req *tempopb.SearchRequest) (*tempopb.TraceSearchMetadata, error) {
+	t, err := d.Unmarshal(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	return trace.MatchesProto(id, t, req)
+}
+
+func (d *enc) Combine(objs ...[]byte) ([]byte, error) {
+	var combinedTrace *tempopb.Trace
+	for _, obj := range objs {
+		t, err := d.Unmarshal(obj)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshaling trace: %w", err)
+		}
+
+		combinedTrace, _ = trace.CombineTraceProtos(combinedTrace, t)
+	}
+
+	combinedBytes, err := d.Marshal(combinedTrace)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling combinedBytes: %w", err)
+	}
+
+	return combinedBytes, nil
+}
+
+func (d *enc) Marshal(t *tempopb.Trace) ([]byte, error) {
+	return proto.Marshal(t)
+}
