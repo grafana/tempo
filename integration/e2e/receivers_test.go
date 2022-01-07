a7b2c99076f8eb692d7015988b91cd4d7c55013e
Annanay <annanayagarwal@gmail.com>
2022-01-07 23:31:56 +0530

    Add otlp receiver test
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

--- integration/e2e/receivers_test.go
+++ integration/e2e/receivers_test.go
@@ -7,11 +7,6 @@ import (
 	"testing"
 
 	cortex_e2e "github.com/cortexproject/cortex/integration/e2e"
-	util "github.com/grafana/tempo/integration"
-	"github.com/grafana/tempo/pkg/tempopb"
-	v1_trace "github.com/grafana/tempo/pkg/tempopb/trace/v1"
-	tempoUtil "github.com/grafana/tempo/pkg/util"
-	"github.com/grafana/tempo/pkg/util/test"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/jaegerexporter"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -21,11 +16,17 @@ import (
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/configgrpc"
 	"go.opentelemetry.io/collector/config/configtls"
-	"go.opentelemetry.io/collector/exporter/exporterhelper"
+	"go.opentelemetry.io/collector/exporter/otlpexporter"
 	"go.opentelemetry.io/collector/model/otlp"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
+
+	util "github.com/grafana/tempo/integration"
+	"github.com/grafana/tempo/pkg/tempopb"
+	v1_trace "github.com/grafana/tempo/pkg/tempopb/trace/v1"
+	tempoUtil "github.com/grafana/tempo/pkg/util"
+	"github.com/grafana/tempo/pkg/util/test"
 )
 
 func TestReceivers(t *testing.T) {
@@ -47,12 +48,25 @@ func TestReceivers(t *testing.T) {
 						Insecure: true,
 					},
 				}
-				jaegerCfg.RetrySettings = exporterhelper.RetrySettings{
-					Enabled: false,
-				}
 				return jaegerCfg
 			},
 			14250,
+		},
+		{
+			"otlp gRPC",
+			otlpexporter.NewFactory(),
+			func(factory component.ExporterFactory, endpoint string) config.Exporter {
+				exporterCfg := factory.CreateDefaultConfig()
+				otlpCfg := exporterCfg.(*otlpexporter.Config)
+				otlpCfg.GRPCClientSettings = configgrpc.GRPCClientSettings{
+					Endpoint: endpoint,
+					TLSSetting: configtls.TLSClientSetting{
+						Insecure: true,
+					},
+				}
+				return otlpCfg
+			},
+			4317,
 		},
 	}
 
@@ -66,7 +80,7 @@ func TestReceivers(t *testing.T) {
 			tempo := util.NewTempoAllInOne()
 			require.NoError(t, s.StartAndWaitReady(tempo))
 
-			// Create exporter
+			// create exporter
 			exporter, err := tc.factory.CreateTracesExporter(
 				context.Background(),
 				component.ExporterCreateSettings{
@@ -84,14 +98,15 @@ func TestReceivers(t *testing.T) {
 			err = exporter.Start(context.Background(), componenttest.NewNopHost())
 			require.NoError(t, err)
 
+			// make request
 			traceID := make([]byte, 16)
 			_, err = rand.Read(traceID)
 			require.NoError(t, err)
-
 			req := test.MakeRequest(20, traceID)
 			b, err := req.Marshal()
 			require.NoError(t, err)
 
+			// unmarshal into otlp proto
 			traces, err := otlp.NewProtobufTracesUnmarshaler().UnmarshalTraces(b)
 			require.NoError(t, err)
 			require.NotNil(t, traces)
@@ -100,10 +115,21 @@ func TestReceivers(t *testing.T) {
 			ctx, err = user.InjectIntoGRPCRequest(ctx)
 			require.NoError(t, err)
 
+			// send traces to tempo
 			err = exporter.ConsumeTraces(ctx, traces)
 			require.NoError(t, err)
 
-			client := tempoUtil.NewClient("http://"+tempo.Endpoint(3200), "")
+			// shutdown to ensure traces are flushed
+			require.NoError(t, exporter.Shutdown(context.Background()))
+
+			// test metrics
+			require.NoError(t, tempo.WaitSumMetrics(cortex_e2e.Equals(20), "tempo_distributor_spans_received_total"))
+
+			// ensure trace is created in ingester (trace_idle_time has passed)
+			require.NoError(t, tempo.WaitSumMetrics(cortex_e2e.Equals(1), "tempo_ingester_traces_created_total"))
+
+			// query for the trace
+			client := tempoUtil.NewClient("http://"+tempo.Endpoint(3200), tempoUtil.FakeTenantID)
 			trace, err := client.QueryTrace(tempoUtil.TraceIDToHexString(traceID))
 			require.NoError(t, err)
 
@@ -113,5 +139,4 @@ func TestReceivers(t *testing.T) {
 			assert.True(t, equalTraces(&tempopb.Trace{Batches: []*v1_trace.ResourceSpans{req.Batch}}, trace))
 		})
 	}
-
 }
