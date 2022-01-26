55361d6d82981349151a1cf4ff92369b1a8f563f
Joe Elliott <number101010@gmail.com>
2022-01-26 10:31:57 -0500

    added basic lambda structure
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ cmd/tempo-serverless/lambda/go.mod
@@ -0,0 +1,23 @@
+module github.com/grafana/tempo/cmd/tempo-serverless/lambda
+
+go 1.16
+
+replace github.com/grafana/tempo => ../../../
+
+// additional Cortex or upstream required replaces
+replace (
+	github.com/bradfitz/gomemcache => github.com/themihai/gomemcache v0.0.0-20180902122335-24332e2d58ab
+	github.com/gocql/gocql => github.com/grafana/gocql v0.0.0-20200605141915-ba5dc39ece85
+	k8s.io/api => k8s.io/api v0.20.4
+	k8s.io/client-go => k8s.io/client-go v0.20.4
+)
+
+// Pin to the latest release of grpc-go with GenerateAndRegisterManualResolver
+// This function is used by jeagertracing/jaeger, but we can't update jaeger
+// without updating the open-telemetry/collector as well
+replace google.golang.org/grpc => google.golang.org/grpc v1.38.0
+
+require (
+	github.com/aws/aws-lambda-go v1.28.0
+	github.com/grafana/tempo v0.0.0-00010101000000-000000000000
+)
--- cmd/tempo-serverless/lambda/main.go
+++ cmd/tempo-serverless/lambda/main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	serverless "github.com/grafana/tempo/cmd/tempo-serverless"
+	"github.com/grafana/tempo/pkg/tempopb"
 )
 
-type MyEvent struct {
-	Name string `json:"What is your name?"`
-	Age  int    `json:"How old are you?"`
-}
+func HandleLambdaEvent(event events.ALBTargetGroupRequest) (*tempopb.SearchResponse, error) {
+	// jpe do some magic to create an http.Requeset{}
 
-type MyResponse struct {
-	Message string `json:"Answer:"`
-}
+	resp, httpErr := serverless.Handler(&http.Request{})
+	if httpErr != nil {
+		// jpe how do i return a specific status code?
+		return nil, httpErr.Err
+	}
 
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+	return resp, nil
 }
 
 func main() {
