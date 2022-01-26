c7cbb63909275a7b6d794e8d3d966735ef986a8f
Zach Leslie <zach.leslie@grafana.com>
2022-01-26 19:48:31 +0000

    Copy `cortex/pkg/frontend` package dependency into Tempo

--- cmd/tempo/app/modules.go
+++ cmd/tempo/app/modules.go
@@ -6,7 +6,6 @@ import (
 	"path"
 
 	"github.com/cortexproject/cortex/pkg/cortex"
-	cortex_frontend "github.com/cortexproject/cortex/pkg/frontend"
 	cortex_frontend_v1pb "github.com/cortexproject/cortex/pkg/frontend/v1/frontendv1pb"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/kv/codec"
@@ -183,7 +182,7 @@ func (t *App) initQuerier() (services.Service, error) {
 func (t *App) initQueryFrontend() (services.Service, error) {
 	// cortexTripper is a bridge between http and httpgrpc. it does the job of passing data to the cortex
 	// frontend code
-	cortexTripper, v1, _, err := cortex_frontend.InitFrontend(t.cfg.Frontend.Config, frontend.CortexNoQuerierLimits{}, 0, log.Logger, prometheus.DefaultRegisterer)
+	cortexTripper, v1, _, err := frontend.InitFrontend(t.cfg.Frontend.Config, frontend.CortexNoQuerierLimits{}, 0, log.Logger, prometheus.DefaultRegisterer)
 	if err != nil {
 		return nil, err
 	}
