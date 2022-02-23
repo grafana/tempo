2a3e51ffb3a05f1c8bd4146a0a48814a57391afc
Joe Elliott <number101010@gmail.com>
2022-02-23 15:18:32 -0500

    improve logging
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/tempo-serverless/handler.go
+++ cmd/tempo-serverless/handler.go
@@ -47,43 +47,28 @@ type HTTPError struct {
 func Handler(r *http.Request) (*tempopb.SearchResponse, *HTTPError) {
 	searchReq, err := api.ParseSearchBlockRequest(r)
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusBadRequest,
-		}
+		return nil, httpError(err, http.StatusBadRequest)
 	}
 
 	// load config, fields are set through env vars TEMPO_
 	reader, cfg, err := loadBackend()
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusInternalServerError,
-		}
+		return nil, httpError(err, http.StatusInternalServerError)
 	}
 
 	tenant, _, err := user.ExtractOrgIDFromHTTPRequest(r)
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusBadRequest,
-		}
+		return nil, httpError(err, http.StatusBadRequest)
 	}
 
 	blockID, err := uuid.Parse(searchReq.BlockID)
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusBadRequest,
-		}
+		return nil, httpError(err, http.StatusBadRequest)
 	}
 
 	enc, err := backend.ParseEncoding(searchReq.Encoding)
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusBadRequest,
-		}
+		return nil, httpError(err, http.StatusBadRequest)
 	}
 
 	// /giphy so meta
@@ -99,20 +84,14 @@ func Handler(r *http.Request) (*tempopb.SearchResponse, *HTTPError) {
 
 	block, err := encoding.NewBackendBlock(meta, reader)
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusInternalServerError,
-		}
+		return nil, httpError(err, http.StatusInternalServerError)
 	}
 
 	// tempodb exposes an IterateObjects() method to basically perform the below loop. currently we are purposefully
 	// not using that so that the serverless function doesn't have to instantiate a full tempodb instance.
 	iter, err := block.PartialIterator(cfg.Search.ChunkSizeBytes, int(searchReq.StartPage), int(searchReq.PagesToSearch))
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusInternalServerError,
-		}
+		return nil, httpError(err, http.StatusInternalServerError)
 	}
 	iter = encoding.NewPrefetchIterator(r.Context(), iter, cfg.Search.PrefetchTraceCount)
 
@@ -122,10 +101,7 @@ func Handler(r *http.Request) (*tempopb.SearchResponse, *HTTPError) {
 
 	decoder, err := model.NewObjectDecoder(searchReq.DataEncoding)
 	if err != nil {
-		return nil, &HTTPError{
-			Err:    err,
-			Status: http.StatusInternalServerError,
-		}
+		return nil, httpError(err, http.StatusInternalServerError)
 	}
 
 	for {
@@ -134,10 +110,7 @@ func Handler(r *http.Request) (*tempopb.SearchResponse, *HTTPError) {
 			break
 		}
 		if err != nil {
-			return nil, &HTTPError{
-				Err:    err,
-				Status: http.StatusInternalServerError,
-			}
+			return nil, httpError(err, http.StatusInternalServerError)
 		}
 
 		resp.Metrics.InspectedTraces++
@@ -145,10 +118,7 @@ func Handler(r *http.Request) (*tempopb.SearchResponse, *HTTPError) {
 
 		metadata, err := decoder.Matches(id, obj, searchReq.SearchReq)
 		if err != nil {
-			return nil, &HTTPError{
-				Err:    err,
-				Status: http.StatusInternalServerError,
-			}
+			return nil, httpError(err, http.StatusInternalServerError)
 		}
 		if metadata == nil {
 			continue
@@ -268,5 +238,12 @@ func stringToFlagExt() mapstructure.DecodeHookFunc {
 		return flagext.Secret{
 			Value: data.(string),
 		}, nil
+	}
+}
+
+func httpError(err error, status int) *HTTPError {
+	return &HTTPError{
+		Err:    fmt.Errorf("serverless handler: %w", err),
+		Status: status,
 	}
 }
