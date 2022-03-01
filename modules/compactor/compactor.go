80dbcc5188db04af363ddefb0ef111db6426023e
Joe Elliott <number101010@gmail.com>
2022-03-01 14:48:04 -0500

    drop spans during compaction
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- modules/compactor/compactor.go
+++ modules/compactor/compactor.go
@@ -29,6 +29,8 @@ const (
 	ringNumTokens = 512
 
 	compactorRingKey = "compactor"
+
+	reasonCompactorDiscardedSpans = "trace_too_large_compaction"
 )
 
 var (
@@ -211,9 +213,26 @@ func (c *Compactor) Owns(hash string) bool {
 	return rs.Instances[0].Addr == ringAddr
 }
 
-// Combine implements common.ObjectCombiner
-func (c *Compactor) Combine(dataEncoding string, objs ...[]byte) ([]byte, bool, error) {
-	return model.ObjectCombiner.Combine(dataEncoding, objs...)
+// Combine implements tempodb.CompactorSharder
+func (c *Compactor) Combine(dataEncoding string, tenantID string, objs ...[]byte) ([]byte, bool, error) {
+	if len(objs) <= 0 {
+		return nil, false, errors.New("no objects provided")
+	}
+
+	maxBytes := c.overrides.MaxBytesPerTrace(tenantID)
+	combinedObj, wasCombined, err := model.ObjectCombiner.Combine(dataEncoding, objs...)
+	if err != nil {
+		return nil, false, err
+	}
+
+	if len(combinedObj) > maxBytes {
+		spansDiscarded := countSpans(dataEncoding, objs[1:]...)
+		overrides.RecordDiscardedSpans(spansDiscarded, reasonCompactorDiscardedSpans, tenantID)
+
+		return objs[0], wasCombined, nil
+	}
+
+	return combinedObj, wasCombined, nil
 }
 
 // BlockRetentionForTenant implements CompactorOverrides
@@ -258,4 +277,28 @@ func (c *Compactor) OnRingInstanceStopping(lifecycler *ring.BasicLifecycler) {}
 // OnRingInstanceHeartbeat is called while the instance is updating its heartbeat
 // in the ring.
 func (c *Compactor) OnRingInstanceHeartbeat(lifecycler *ring.BasicLifecycler, ringDesc *ring.Desc, instanceDesc *ring.InstanceDesc) {
+}
+
+//
+func countSpans(dataEncoding string, objs ...[]byte) int {
+	decoder, err := model.NewObjectDecoder(dataEncoding)
+	if err != nil {
+		return 0
+	}
+	spans := 0
+
+	for _, o := range objs {
+		t, err := decoder.PrepareForRead(o)
+		if err != nil {
+			continue
+		}
+
+		for _, b := range t.Batches {
+			for _, ilm := range b.InstrumentationLibrarySpans {
+				spans += len(ilm.Spans)
+			}
+		}
+	}
+
+	return spans
 }
