4aab1c170dfafeea0dfc62cb4f58a27d718534f7
Zach Leslie <zach.leslie@grafana.com>
2022-03-02 22:03:34 +0000

    Avoid extending the block meta time window if a span is too old

--- tempodb/backend/block_meta.go
+++ tempodb/backend/block_meta.go
@@ -14,21 +14,22 @@ type CompactedBlockMeta struct {
 }
 
 type BlockMeta struct {
-	Version         string    `json:"format"`          // Version indicates the block format version. This includes specifics of how the indexes and data is stored
-	BlockID         uuid.UUID `json:"blockID"`         // Unique block id
-	MinID           []byte    `json:"minID"`           // Minimum object id stored in this block
-	MaxID           []byte    `json:"maxID"`           // Maximum object id stored in this block
-	TenantID        string    `json:"tenantID"`        // ID of tehant to which this block belongs
-	StartTime       time.Time `json:"startTime"`       // Roughly matches when the first obj was written to this block. Used to determine block age for different purposes (cacheing, etc)
-	EndTime         time.Time `json:"endTime"`         // Currently mostly meaningless but roughly matches to the time the last obj was written to this block
-	TotalObjects    int       `json:"totalObjects"`    // Total objects in this block
-	Size            uint64    `json:"size"`            // Total size in bytes of the data object
-	CompactionLevel uint8     `json:"compactionLevel"` // Kind of the number of times this block has been compacted
-	Encoding        Encoding  `json:"encoding"`        // Encoding/compression format
-	IndexPageSize   uint32    `json:"indexPageSize"`   // Size of each index page in bytes
-	TotalRecords    uint32    `json:"totalRecords"`    // Total Records stored in the index file
-	DataEncoding    string    `json:"dataEncoding"`    // DataEncoding is a string provided externally, but tracked by tempodb that indicates the way the bytes are encoded
-	BloomShardCount uint16    `json:"bloomShards"`     // Number of bloom filter shards
+	Version         string        `json:"format"`             // Version indicates the block format version. This includes specifics of how the indexes and data is stored
+	BlockID         uuid.UUID     `json:"blockID"`            // Unique block id
+	MinID           []byte        `json:"minID"`              // Minimum object id stored in this block
+	MaxID           []byte        `json:"maxID"`              // Maximum object id stored in this block
+	TenantID        string        `json:"tenantID"`           // ID of tehant to which this block belongs
+	StartTime       time.Time     `json:"startTime"`          // Roughly matches when the first obj was written to this block. Used to determine block age for different purposes (cacheing, etc)
+	EndTime         time.Time     `json:"endTime"`            // Currently mostly meaningless but roughly matches to the time the last obj was written to this block
+	TotalObjects    int           `json:"totalObjects"`       // Total objects in this block
+	Size            uint64        `json:"size"`               // Total size in bytes of the data object
+	CompactionLevel uint8         `json:"compactionLevel"`    // Kind of the number of times this block has been compacted
+	Encoding        Encoding      `json:"encoding"`           // Encoding/compression format
+	IndexPageSize   uint32        `json:"indexPageSize"`      // Size of each index page in bytes
+	TotalRecords    uint32        `json:"totalRecords"`       // Total Records stored in the index file
+	DataEncoding    string        `json:"dataEncoding"`       // DataEncoding is a string provided externally, but tracked by tempodb that indicates the way the bytes are encoded
+	BloomShardCount uint16        `json:"bloomShards"`        // Number of bloom filter shards
+	MaxSpanIndexAge time.Duration `json:"max_span_index_age"` // Maximum span index age that will adjust the StartTime of the BlockMeta
 }
 
 func NewBlockMeta(tenantID string, blockID uuid.UUID, version string, encoding Encoding, dataEncoding string) *BlockMeta {
@@ -51,10 +52,16 @@ func (b *BlockMeta) ObjectAdded(id []byte, start uint32, end uint32) {
 	startTime := time.Unix(int64(start), 0)
 	endTime := time.Unix(int64(end), 0)
 
-	if b.StartTime.IsZero() || startTime.Before(b.StartTime) {
+	// We want to adjust the block meta time window only when the object being
+	// added is not older than the MaxSpanIndexAge.  This will mean search
+	// results will not be able to search for this trace.
+	maxSpanAgeTime := time.Now().Add(-b.MaxSpanIndexAge)
+	spanExtendsBlockTime := startTime.After(maxSpanAgeTime)
+
+	if b.StartTime.IsZero() || (startTime.Before(b.StartTime) && spanExtendsBlockTime) {
 		b.StartTime = startTime
 	}
-	if b.EndTime.IsZero() || endTime.After(b.EndTime) {
+	if b.EndTime.IsZero() || (endTime.After(b.EndTime) && spanExtendsBlockTime) {
 		b.EndTime = endTime
 	}
 
