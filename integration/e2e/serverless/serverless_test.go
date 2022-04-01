4c85f965185dc6a74de669f432db7f16969175f6
Zach Leslie <zach.leslie@grafana.com>
2022-04-01 16:58:39 +0000

    Update serverless build for lambda docker

--- integration/e2e/serverless/serverless_test.go
+++ integration/e2e/serverless/serverless_test.go
@@ -141,7 +141,7 @@ func newTempoServerlessGCF() *e2e.HTTPService {
 func newTempoServerlessLambda() *e2e.HTTPService {
 	s := e2e.NewHTTPService(
 		"serverless",
-		"tempo-serverless-lambda-proxy", // created by build-lambda-docker-local-proxy make target
+		"tempo-serverless-lambda", // created by build-docker-lambda make target
 		nil,
 		nil,
 		8080,
