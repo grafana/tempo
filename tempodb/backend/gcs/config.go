4b297636949f238571789c3dca04ea9d1b045e50
Zach Leslie <zach.leslie@grafana.com>
2022-04-11 19:09:25 +0000

    Sort struct fields

--- tempodb/backend/gcs/config.go
+++ tempodb/backend/gcs/config.go
@@ -7,13 +7,11 @@ import (
 )
 
 type Config struct {
-	BucketName string `yaml:"bucket_name"`
-	Endpoint   string `yaml:"endpoint"`
-
-	ChunkBufferSize   int `yaml:"chunk_buffer_size"`
-	HedgeRequestsUpTo int `yaml:"hedge_requests_up_to"`
-
-	Insecure        bool                 `yaml:"insecure"`
-	HedgeRequestsAt time.Duration        `yaml:"hedge_requests_at"`
-	ObjAttrs        *storage.ObjectAttrs `yaml:"obj_attrs"`
+	BucketName        string               `yaml:"bucket_name"`
+	Endpoint          string               `yaml:"endpoint"`
+	ChunkBufferSize   int                  `yaml:"chunk_buffer_size"`
+	HedgeRequestsUpTo int                  `yaml:"hedge_requests_up_to"`
+	Insecure          bool                 `yaml:"insecure"`
+	HedgeRequestsAt   time.Duration        `yaml:"hedge_requests_at"`
+	ObjAttrs          *storage.ObjectAttrs `yaml:"obj_attrs"`
 }
