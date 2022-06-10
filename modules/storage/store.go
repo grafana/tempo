ebc6b17324ea38b719747a6216f99106c35ef981
Zach Leslie <zach.leslie@grafana.com>
2022-06-10 15:38:26 +0000

    Add store configuration usage metrics

--- modules/storage/store.go
+++ modules/storage/store.go
@@ -6,7 +6,17 @@ import (
 	"github.com/go-kit/log"
 	"github.com/grafana/dskit/services"
 
+	"github.com/grafana/tempo/pkg/usagestats"
 	"github.com/grafana/tempo/tempodb"
+)
+
+var (
+	cacheStats               = usagestats.NewString("storage_cache")
+	backendStats             = usagestats.NewString("storage_backend")
+	walEncodingStats         = usagestats.NewString("storage_wal_encoding")
+	walSearchEncodingStats   = usagestats.NewString("storage_wal_search_encoding")
+	blockEncodingStats       = usagestats.NewString("storage_block_encoding")
+	blockSearchEncodingStats = usagestats.NewString("storage_block_search_encoding")
 )
 
 // Store wraps the tempodb storage layer
@@ -30,6 +40,14 @@ type store struct {
 
 // NewStore creates a new Tempo Store using configuration supplied.
 func NewStore(cfg Config, logger log.Logger) (Store, error) {
+
+	cacheStats.Set(cfg.Trace.Cache)
+	backendStats.Set(cfg.Trace.Backend)
+	walEncodingStats.Set(cfg.Trace.WAL.Encoding.String())
+	walSearchEncodingStats.Set(cfg.Trace.WAL.SearchEncoding.String())
+	blockEncodingStats.Set(cfg.Trace.Block.Encoding.String())
+	blockSearchEncodingStats.Set(cfg.Trace.Block.SearchEncoding.String())
+
 	r, w, c, err := tempodb.New(&cfg.Trace, logger)
 	if err != nil {
 		return nil, err
