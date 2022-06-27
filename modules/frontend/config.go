bc9f792cb83948507f38927a435c269ef43e1c4a
Zach Leslie <zach.leslie@grafana.com>
2022-06-27 20:40:35 +0000

    Include frontend version stats

--- modules/frontend/config.go
+++ modules/frontend/config.go
@@ -13,7 +13,13 @@ import (
 	"github.com/grafana/tempo/modules/frontend/transport"
 	v1 "github.com/grafana/tempo/modules/frontend/v1"
 	v2 "github.com/grafana/tempo/modules/frontend/v2"
+	"github.com/grafana/tempo/pkg/usagestats"
 	"github.com/grafana/tempo/pkg/util"
+)
+
+var (
+	workerConcurrencyStats = usagestats.NewInt("frontend_worker_concurrency")
+	versionStats           = usagestats.NewString("frontend_version")
 )
 
 type Config struct {
@@ -85,6 +91,9 @@ func InitFrontend(cfg CombinedFrontendConfig, limits v1.Limits, grpcListenPort i
 		return rt, nil, nil, err
 
 	case cfg.FrontendV2.SchedulerAddress != "":
+		versionStats.Set("v2")
+		workerConcurrencyStats.Set(int64(cfg.FrontendV2.WorkerConcurrency))
+
 		// If query-scheduler address is configured, use Frontend.
 		if cfg.FrontendV2.Addr == "" {
 			addr, err := util.GetFirstAddressOf(cfg.FrontendV2.InfNames)
@@ -103,6 +112,7 @@ func InitFrontend(cfg CombinedFrontendConfig, limits v1.Limits, grpcListenPort i
 		return transport.AdaptGrpcRoundTripperToHTTPRoundTripper(fr), nil, fr, err
 
 	default:
+		versionStats.Set("v1")
 		// No scheduler = use original frontend.
 		fr, err := v1.New(cfg.FrontendV1, limits, log, reg)
 		if err != nil {
