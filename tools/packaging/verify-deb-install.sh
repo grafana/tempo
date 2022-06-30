07e9ef2f1c25a63ac6061307a3a8453b81b5e417
Julien Duchesne <julien.duchesne@grafana.com>
2022-06-30 09:56:57 -0400

    Add .deb and .rpm packages (#1533)
    This is done through goreleaser's nfpm tool
    The package files are then tested using systemd docker images. Tempo is installed onto debian and centos and we check that it's running and available

---
+++ tools/packaging/verify-deb-install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -euxo pipefail
+
+docker ps
+image="$(docker ps --filter ancestor=jrei/systemd-debian:12 --latest --format "{{.ID}}")"
+echo "Running on container: ${image}"
+
+dir="."
+if [ -n "${CI}" ]; then
+    dir="/drone/src"
+fi
+echo "Running on directory: ${dir}"
+
+cat <<EOF | docker exec --interactive "${image}" sh
+    set -x
+
+    # Install tempo and check it's running
+    dpkg -i ${dir}/dist/tempo*_amd64.deb
+    [ "\$(systemctl is-active tempo)" = "active" ] || (echo "tempo is inactive" && exit 1)
+
+    # Wait for tempo to be ready. The script is cat-ed because it is passed to docker exec
+    apt update && apt install -y curl
+    $(cat ${dir}/tools/packaging/wait-for-ready.sh)
+EOF
