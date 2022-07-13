9f6aded050ec0f05618a6b461c54faa3b44b0fc2
Annanay <annanayagarwal@gmail.com>
2022-07-13 17:01:08 +0530

    Add missing file
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

---
+++ tempodb/encoding/vparquet/readers.go
@@ -0,0 +1,98 @@
+package vparquet
+
+import (
+	"context"
+	"encoding/binary"
+	"io"
+
+	"github.com/google/uuid"
+
+	"github.com/grafana/tempo/tempodb/backend"
+)
+
+type BackendReaderAt struct {
+	ctx      context.Context
+	r        backend.Reader
+	name     string
+	blockID  uuid.UUID
+	tenantID string
+
+	TotalBytesRead uint64
+}
+
+var _ io.ReaderAt = (*BackendReaderAt)(nil)
+
+func NewBackendReaderAt(ctx context.Context, r backend.Reader, name string, blockID uuid.UUID, tenantID string) *BackendReaderAt {
+	return &BackendReaderAt{ctx, r, name, blockID, tenantID, 0}
+}
+
+func (b *BackendReaderAt) ReadAt(p []byte, off int64) (int, error) {
+	b.TotalBytesRead += uint64(len(p))
+	err := b.r.ReadRange(b.ctx, b.name, b.blockID, b.tenantID, uint64(off), p, false)
+	return len(p), err
+}
+
+func (b *BackendReaderAt) ReadAtWithCache(p []byte, off int64) (int, error) {
+	err := b.r.ReadRange(b.ctx, b.name, b.blockID, b.tenantID, uint64(off), p, true)
+	return len(p), err
+}
+
+// Set of functions called by the parquet-go library in OpenFile() to set offset and lengths of metadata objects
+type ParquetMetadataSections interface {
+	SetFooterSection(offset, length int64)
+	SetColumnIndexSection(offset, length int64)
+	SetOffsetIndexSection(offset, length int64)
+}
+
+type parquetOptimizedReaderAt struct {
+	r          io.ReaderAt
+	br         *BackendReaderAt
+	readerSize int64
+	footerSize uint32
+
+	// storing offsets and length of objects we want to cache
+	cachedObjects map[int64]int64
+}
+
+var _ io.ReaderAt = (*parquetOptimizedReaderAt)(nil)
+var _ ParquetMetadataSections = (*parquetOptimizedReaderAt)(nil)
+
+func NewParquetOptimizedReaderAt(br io.ReaderAt, rr *BackendReaderAt, size int64, footerSize uint32) *parquetOptimizedReaderAt {
+	return &parquetOptimizedReaderAt{br, rr, size, footerSize, map[int64]int64{}}
+}
+
+// called by parquet-go to set offset and length of footer section
+func (r *parquetOptimizedReaderAt) SetFooterSection(offset, length int64) {
+	r.cachedObjects[offset] = length
+}
+
+// called by parquet-go to set offset and length of column indexes
+func (r *parquetOptimizedReaderAt) SetColumnIndexSection(offset, length int64) {
+	r.cachedObjects[offset] = length
+}
+
+// called by parquet-go to set offset and length of offset index section
+func (r *parquetOptimizedReaderAt) SetOffsetIndexSection(offset, length int64) {
+	r.cachedObjects[offset] = length
+}
+
+func (r *parquetOptimizedReaderAt) ReadAt(p []byte, off int64) (int, error) {
+	if len(p) == 4 && off == 0 {
+		// Magic header
+		return copy(p, []byte("PAR1")), nil
+	}
+
+	if len(p) == 8 && off == r.readerSize-8 && r.footerSize > 0 /* not present in previous block metas */ {
+		// Magic footer
+		binary.LittleEndian.PutUint32(p, r.footerSize)
+		copy(p[4:8], []byte("PAR1"))
+		return 8, nil
+	}
+
+	// check if the offset and length is stored as a special object
+	if r.cachedObjects[off] == int64(len(p)) {
+		r.br.ReadAtWithCache(p, off)
+	}
+
+	return r.r.ReadAt(p, off)
+}
