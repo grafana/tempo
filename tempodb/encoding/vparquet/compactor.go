8765035ed1a56463a045319b1fc6d07637b85084
Joe Elliott <number101010@gmail.com>
2022-08-29 15:36:00 -0400

    merge fix
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tempodb/encoding/vparquet/compactor.go
+++ tempodb/encoding/vparquet/compactor.go
@@ -160,7 +160,7 @@ func (c *Compactor) Compact(ctx context.Context, l log.Logger, r backend.Reader,
 		}
 
 		// Flush existing block data if the next trace can't fit
-		if currentBlock.CurrentBufferedValues() > 0 && currentBlock.EstimatedBufferedBytes()+estimateProtoSize(lowestObject) > c.opts.BlockConfig.RowGroupSizeBytes {
+		if currentBlock.EstimatedBufferedBytes() > 0 && currentBlock.EstimatedBufferedBytes()+estimateProtoSize(lowestObject) > c.opts.BlockConfig.RowGroupSizeBytes {
 			runtime.GC()
 			err = c.appendBlock(ctx, currentBlock, l)
 			if err != nil {
