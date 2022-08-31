be7137f6e68d188e357630247a96c8c524853fd4
Joe Elliott <number101010@gmail.com>
2022-08-31 15:24:18 -0400

    Revert "snappy removal only"
    This reverts commit 7fd047a85a1631047cb9e7e9af0a7047995c0f17.

--- tempodb/encoding/vparquet/schema.go
+++ tempodb/encoding/vparquet/schema.go
@@ -77,7 +77,7 @@ type Span struct {
 	// friendly like trace ID, and []byte is half the size of string.
 	ID                     []byte      `parquet:","`
 	Name                   string      `parquet:",snappy,dict"`
-	Kind                   int         `parquet:",delta"`
+	Kind                   int         `parquet:",dict"`
 	ParentSpanID           []byte      `parquet:","`
 	TraceState             string      `parquet:",snappy"`
 	StartUnixNanos         uint64      `parquet:",delta"`
@@ -129,12 +129,12 @@ type ResourceSpans struct {
 
 type Trace struct {
 	// TraceID is a byte slice as it helps maintain the sort order of traces within a parquet file
-	TraceID       []byte          `parquet:""`
+	TraceID       []byte          `parquet:",delta"`
 	ResourceSpans []ResourceSpans `parquet:"rs"`
 
 	// TraceIDText is for better useability on downstream systems i.e: something other than Tempo is reading these files.
 	// It will not be used as the primary traceID field within Tempo and is only helpful for debugging purposes.
-	TraceIDText string `parquet:",snappy"`
+	TraceIDText string `parquet:",delta"`
 
 	// Trace-level attributes for searching
 	StartTimeUnixNano uint64 `parquet:",delta"`
