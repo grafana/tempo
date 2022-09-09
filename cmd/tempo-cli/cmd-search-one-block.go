e9598be4c7cb8225d6d7ea41e8ca2374f3597329
Joe Elliott <number101010@gmail.com>
2022-09-09 14:58:55 -0400

    gerbegey gerbege
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/tempo-cli/cmd-search-one-block.go
+++ cmd/tempo-cli/cmd-search-one-block.go
@@ -3,14 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
+	"math/rand"
+	"sync"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/google/uuid"
 	"github.com/grafana/tempo/pkg/tempopb"
-	"github.com/grafana/tempo/pkg/util/test"
 	"github.com/grafana/tempo/tempodb"
-	"github.com/grafana/tempo/tempodb/backend/instrumentation"
+	"github.com/grafana/tempo/tempodb/backend"
 	"github.com/grafana/tempo/tempodb/encoding"
 	"github.com/grafana/tempo/tempodb/encoding/common"
 	"github.com/pkg/errors"
@@ -31,22 +31,37 @@ func (cmd *searchOneBlockCmd) Run(opts *globalOptions) error {
 		return err
 	}
 
-	blockID, err := uuid.Parse(cmd.BlockID)
-	if err != nil {
-		return err
-	}
+	// blockID, err := uuid.Parse(cmd.BlockID)
+	// if err != nil {
+	// 	return err
+	// }
 
 	searchReq := &tempopb.SearchRequest{
-		Tags:  map[string]string{cmd.Name: cmd.Value},
-		Limit: limit,
+		Tags:          map[string]string{cmd.Name: cmd.Value},
+		MinDurationMs: 1000,
+		Limit:         limit,
 	}
 
+	// find requested block
+	wg := sync.WaitGroup{}
+	for i := 0; i < 20; i++ {
+		wg.Add(2)
+		go find(r, "a6cb3099-8763-4f43-bff2-01a3a5d420c9", searchReq, &wg)
+		go find(r, "62153e5b-6c4e-442d-a05d-852e190cfc26", searchReq, &wg)
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+func find(r backend.Reader, blockID string, searchReq *tempopb.SearchRequest, wg *sync.WaitGroup) {
+	defer wg.Done()
 	ctx := context.Background()
 
-	// find requested block
-	meta, err := r.BlockMeta(ctx, blockID, cmd.TenantID)
+	meta, err := r.BlockMeta(ctx, uuid.MustParse(blockID), "1")
 	if err != nil {
-		return errors.Wrap(err, "failed to find block meta")
+		panic(errors.Wrap(err, "failed to find block meta"))
 	}
 
 	fmt.Println("Searching:")
@@ -54,28 +69,20 @@ func (cmd *searchOneBlockCmd) Run(opts *globalOptions) error {
 
 	searchOpts := common.SearchOptions{}
 	tempodb.SearchConfig{}.ApplyToOptions(&searchOpts)
+	searchOpts.PrefetchTraceCount = 1000
+	searchOpts.ReadBufferSize = 1024 * 1024
+	searchOpts.StartPage = 5
+	searchOpts.TotalPages = rand.Intn(10) + 1
 
-	start := time.Now()
 	block, err := encoding.OpenBlock(meta, r)
 	if err != nil {
-		return errors.Wrap(err, "failed to open block")
+		panic(errors.Wrap(err, "failed to open block"))
 	}
 
 	resp, err := block.Search(ctx, searchReq, searchOpts)
 	if err != nil {
-		return errors.Wrap(err, "error searching block")
+		panic(errors.Wrap(err, "error searching block"))
 	}
 
-	fmt.Println("Duration:", time.Since(start))
-	fmt.Println("Response:")
-	spew.Dump(resp)
-
-	vals, err := test.GetHistogramValue(instrumentation.HistogramVec(), "GET", "206")
-	if err != nil {
-		fmt.Println("Error retrieving histogram: ", err)
-		return nil
-	}
-	spew.Dump(vals)
-
-	return nil
+	fmt.Println(resp)
 }
