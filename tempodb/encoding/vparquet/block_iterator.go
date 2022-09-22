19f1e73cf3d731dcea7f40ab5e51880117b9f821
Joe Elliott <number101010@gmail.com>
2022-09-22 09:46:50 -0400

    don't load things you don't need to
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tempodb/encoding/vparquet/block_iterator.go
+++ tempodb/encoding/vparquet/block_iterator.go
@@ -19,7 +19,7 @@ func (b *backendBlock) open(ctx context.Context) (*parquet.File, *parquet.Reader
 	// 128 MB memory buffering
 	br := tempo_io.NewBufferedReaderAt(rr, int64(b.meta.Size), 2*1024*1024, 64)
 
-	pf, err := parquet.OpenFile(br, int64(b.meta.Size))
+	pf, err := parquet.OpenFile(br, int64(b.meta.Size), parquet.SkipBloomFilters(true), parquet.SkipPageIndex(true))
 	if err != nil {
 		return nil, nil, err
 	}
