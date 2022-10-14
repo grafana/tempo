23d1f2c7c87afd915792fec476c2444bc0d68010
Lukasz Gut <40406905+Blinkuu@users.noreply.github.com>
2022-10-14 13:37:28 +0200

    Refactor queueManager into generic queue.Queue (#1796)
    * Refactor queueManager into generic queue.Queue
    
    * Update CHANGELOG.md
    
    * Create shared processFun method and a helper function for queue creation
    
    * Remove returning error from ProcessFunc and update chan *request[T] to chan T.
    
    * Fix unit tests
    
    * Replace go.uber.org/atomic with sync/atomic
    
    * Remove redundant accessor
    
    * Fix processFunc definition
    
    * Add breaking change to CHANGELOG.md
    
    * Define metrics on the package level to overcome Prometheus DefaultRegistry limitations
    
    * Fix tests in queue_test.go
    
    * Deprecate generatorForwarder metrics in favor of queue.Queue metrics

--- modules/distributor/forwarder_test.go
+++ modules/distributor/forwarder_test.go
@@ -3,21 +3,19 @@ package distributor
 import (
 	"context"
 	"flag"
-	"os"
-	"path/filepath"
 	"sync"
 	"testing"
 	"time"
 
-	"github.com/grafana/dskit/services"
+	"github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/grafana/tempo/modules/overrides"
 	v1 "github.com/grafana/tempo/pkg/tempopb/trace/v1"
 	"github.com/grafana/tempo/pkg/util"
 	"github.com/grafana/tempo/pkg/util/test"
-	"github.com/prometheus/common/model"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v2"
 )
 
 const tenantID = "tenant-id"
@@ -37,13 +35,18 @@ func TestForwarder(t *testing.T) {
 	require.NoError(t, err)
 
 	wg := sync.WaitGroup{}
-	f := newForwarder(func(ctx context.Context, userID string, k []uint32, traces []*rebatchedTrace) error {
-		assert.Equal(t, tenantID, userID)
-		assert.Equal(t, keys, k)
-		assert.Equal(t, rebatchedTraces, traces)
-		wg.Done()
-		return nil
-	}, o)
+	f := newGeneratorForwarder(
+		log.NewNopLogger(),
+		prometheus.NewPedanticRegistry(),
+		func(ctx context.Context, userID string, k []uint32, traces []*rebatchedTrace) error {
+			assert.Equal(t, tenantID, userID)
+			assert.Equal(t, keys, k)
+			assert.Equal(t, rebatchedTraces, traces)
+			wg.Done()
+			return nil
+		},
+		o,
+	)
 	require.NoError(t, f.start(context.Background()))
 	defer func() {
 		require.NoError(t, f.stop(nil))
@@ -56,52 +59,6 @@ func TestForwarder(t *testing.T) {
 	wg.Add(1)
 	f.SendTraces(context.Background(), tenantID, keys, rebatchedTraces)
 	wg.Wait()
-
-	assert.Equal(t, 0, len(f.queueManagers[tenantID].reqChan))
-}
-
-func TestForwarder_QueueSizeConfig(t *testing.T) {
-	// this test sends more traces than the configured queue size
-	// and asserts that the queue size is equal or less than the configured size
-
-	queueSize := 10
-	oCfg := overrides.Limits{
-		MetricsGeneratorForwarderQueueSize: queueSize,
-		MetricsGeneratorForwarderWorkers:   1,
-	}
-	oCfg.RegisterFlags(&flag.FlagSet{})
-
-	id, err := util.HexStringToTraceID("1234567890abcdef")
-	require.NoError(t, err)
-
-	b := test.MakeBatch(10, id)
-	keys, rebatchedTraces, err := requestsByTraceID([]*v1.ResourceSpans{b}, tenantID, 10)
-	require.NoError(t, err)
-
-	o, err := overrides.NewOverrides(oCfg)
-	require.NoError(t, err)
-
-	shutdownCh := make(chan struct{})
-
-	f := newForwarder(func(ctx context.Context, userID string, k []uint32, traces []*rebatchedTrace) error {
-		<-shutdownCh
-		return nil
-	}, o)
-
-	require.NoError(t, f.start(context.Background()))
-	defer func() {
-		close(shutdownCh)
-		require.NoError(t, f.stop(nil))
-		assert.Equal(t, 0, len(f.queueManagers[tenantID].reqChan))
-	}()
-
-	// sending more traces than queue size
-	for i := 0; i < queueSize+2; i++ {
-		f.SendTraces(context.Background(), tenantID, keys, rebatchedTraces)
-	}
-
-	// queue length is less or equal to the configured queue size
-	assert.LessOrEqual(t, len(f.queueManagers[tenantID].reqChan), queueSize)
 }
 
 func TestForwarder_shutdown(t *testing.T) {
@@ -120,14 +77,19 @@ func TestForwarder_shutdown(t *testing.T) {
 	require.NoError(t, err)
 
 	signalCh := make(chan struct{})
-	f := newForwarder(func(ctx context.Context, userID string, k []uint32, traces []*rebatchedTrace) error {
-		<-signalCh
+	f := newGeneratorForwarder(
+		log.NewNopLogger(),
+		prometheus.NewPedanticRegistry(),
+		func(ctx context.Context, userID string, k []uint32, traces []*rebatchedTrace) error {
+			<-signalCh
 
-		assert.Equal(t, tenantID, userID)
-		assert.Equal(t, keys, k)
-		assert.Equal(t, rebatchedTraces, traces)
-		return nil
-	}, o)
+			assert.Equal(t, tenantID, userID)
+			assert.Equal(t, keys, k)
+			assert.Equal(t, rebatchedTraces, traces)
+			return nil
+		},
+		o,
+	)
 
 	require.NoError(t, f.start(context.Background()))
 	defer func() {
@@ -137,117 +99,9 @@ func TestForwarder_shutdown(t *testing.T) {
 			close(signalCh)
 		}()
 		require.NoError(t, f.stop(nil))
-		f.mutex.Lock()
-		assert.Equal(t, 0, len(f.queueManagers[tenantID].reqChan))
-		f.mutex.Unlock()
 	}()
 
 	for i := 0; i < 100; i++ {
 		f.SendTraces(context.Background(), tenantID, keys, rebatchedTraces)
 	}
-}
-
-func TestForwarder_overrides(t *testing.T) {
-	overridesReloadInterval := 100 * time.Millisecond
-	limits := overrides.Limits{}
-	overridesFile := filepath.Join(t.TempDir(), "overrides.yaml")
-
-	buff, err := yaml.Marshal(map[string]map[string]*overrides.Limits{
-		"overrides": {
-			tenantID: {
-				MetricsGeneratorForwarderQueueSize: 10,
-				MetricsGeneratorForwarderWorkers:   1,
-			},
-		},
-	})
-	require.NoError(t, err)
-
-	err = os.WriteFile(overridesFile, buff, os.ModePerm)
-	require.NoError(t, err)
-
-	limits.PerTenantOverrideConfig = overridesFile
-	limits.PerTenantOverridePeriod = model.Duration(overridesReloadInterval)
-
-	id, err := util.HexStringToTraceID("1234567890abcdef")
-	require.NoError(t, err)
-
-	b := test.MakeBatch(10, id)
-	keys, rebatchedTraces, err := requestsByTraceID([]*v1.ResourceSpans{b}, tenantID, 10)
-	require.NoError(t, err)
-
-	o, err := overrides.NewOverrides(limits)
-	require.NoError(t, err)
-	require.NoError(t, services.StartAndAwaitRunning(context.TODO(), o))
-
-	signalCh := make(chan struct{})
-	wg := sync.WaitGroup{}
-	f := newForwarder(func(ctx context.Context, userID string, k []uint32, traces []*rebatchedTrace) error {
-		wg.Done()
-		<-signalCh
-		return nil
-	}, o)
-	f.overridesInterval = overridesReloadInterval
-
-	require.NoError(t, f.start(context.Background()))
-	defer func() {
-		close(signalCh)
-		require.NoError(t, f.stop(nil))
-		f.mutex.Lock()
-		assert.Equal(t, 0, len(f.queueManagers[tenantID].reqChan))
-		f.mutex.Unlock()
-	}()
-
-	wg.Add(1)
-	f.SendTraces(context.Background(), tenantID, keys, rebatchedTraces)
-	wg.Wait()
-
-	// 10 pushes are buffered, 10 are discarded
-	for i := 0; i < 20; i++ {
-		f.SendTraces(context.Background(), tenantID, keys, rebatchedTraces)
-	}
-
-	// queue is full with 10 items
-	f.mutex.Lock()
-	assert.Equal(t, 10, len(f.queueManagers[tenantID].reqChan))
-	f.mutex.Unlock()
-	wg.Add(10)
-
-	buff, err = yaml.Marshal(map[string]map[string]*overrides.Limits{
-		"overrides": {
-			tenantID: {
-				MetricsGeneratorForwarderQueueSize: 20,
-				MetricsGeneratorForwarderWorkers:   2,
-			},
-		},
-	})
-	require.NoError(t, err)
-
-	err = os.WriteFile(overridesFile, buff, os.ModePerm)
-	require.NoError(t, err)
-
-	// Wait triple the reload interval to ensure overrides are updated (overrides interval + forwarder interval)
-	time.Sleep(3 * overridesReloadInterval)
-
-	// Allow for pending requests to be processed so queueManager can be closed and a new one created
-	for i := 0; i < 11; i++ {
-		signalCh <- struct{}{}
-	}
-	wg.Wait()
-
-	wg.Add(2)
-	for i := 0; i < 2; i++ {
-		f.SendTraces(context.Background(), tenantID, keys, rebatchedTraces)
-	}
-	wg.Wait()
-
-	// 20 pushes are buffered, 10 are discarded
-	for i := 0; i < 30; i++ {
-		f.SendTraces(context.Background(), tenantID, keys, rebatchedTraces)
-	}
-
-	// queue is full with 20 items
-	f.mutex.Lock()
-	assert.Equal(t, 20, len(f.queueManagers[tenantID].reqChan))
-	f.mutex.Unlock()
-	wg.Add(20)
 }
---
+++ modules/distributor/queue/queue.go
@@ -0,0 +1,195 @@
+package queue
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/weaveworks/common/user"
+)
+
+var (
+	pushesTotalMetrics = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "tempo",
+		Subsystem: "distributor",
+		Name:      "queue_pushes_total",
+		Help:      "Total number of successful requests queued up for a tenant to the generatorForwarder",
+	}, []string{"name", "tenant"})
+	pushesFailuresTotalMetric = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "tempo",
+		Subsystem: "distributor",
+		Name:      "queue_pushes_failures_total",
+		Help:      "Total number of failed pushes to the queue for a tenant to the generatorForwarder",
+	}, []string{"name", "tenant"})
+	lengthMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "tempo",
+		Subsystem: "distributor",
+		Name:      "queue_length",
+		Help:      "Number of queued requests for a tenant",
+	}, []string{"name", "tenant"})
+)
+
+type ProcessFunc[T any] func(ctx context.Context, data T)
+
+// Queue represents a single tenant's queue.
+type Queue[T any] struct {
+	// wg is used to wait for the workers to drain the queue while stopping
+	wg sync.WaitGroup
+
+	logger log.Logger
+
+	name           string
+	tenantID       string
+	workerCount    int
+	size           int
+	reqChan        chan T
+	fn             ProcessFunc[T]
+	workersCloseCh chan struct{}
+
+	pushesTotalMetrics         *prometheus.CounterVec
+	pushesFailuresTotalMetrics *prometheus.CounterVec
+	lengthMetric               *prometheus.GaugeVec
+
+	readOnly *atomic.Bool
+}
+
+func New[T any](cfg Config, logger log.Logger, _ prometheus.Registerer, fn ProcessFunc[T]) *Queue[T] {
+	return &Queue[T]{
+		logger:                     logger,
+		name:                       cfg.Name,
+		tenantID:                   cfg.TenantID,
+		workerCount:                cfg.WorkerCount,
+		size:                       cfg.Size,
+		reqChan:                    make(chan T, cfg.Size),
+		fn:                         fn,
+		workersCloseCh:             make(chan struct{}),
+		pushesTotalMetrics:         pushesTotalMetrics,
+		pushesFailuresTotalMetrics: pushesFailuresTotalMetric,
+		lengthMetric:               lengthMetric,
+		readOnly:                   &atomic.Bool{},
+	}
+}
+
+// Push pushes data onto a queue.
+// If the queue is full, the data is dropped
+func (m *Queue[T]) Push(ctx context.Context, data T) error {
+	if m.readOnly.Load() {
+		return fmt.Errorf("queue is read-only")
+	}
+
+	m.pushesTotalMetrics.WithLabelValues(m.name, m.tenantID).Inc()
+
+	select {
+	case <-ctx.Done():
+		m.pushesFailuresTotalMetrics.WithLabelValues(m.name, m.tenantID).Inc()
+		return fmt.Errorf("failed to push data to queue for tenant=%s and queue_name=%s: %w", m.tenantID, m.name, ctx.Err())
+	default:
+	}
+
+	select {
+	case m.reqChan <- data:
+		m.lengthMetric.WithLabelValues(m.name, m.tenantID).Inc()
+		return nil
+	default:
+	}
+
+	// Fail fast if the queue is full
+	m.pushesFailuresTotalMetrics.WithLabelValues(m.name, m.tenantID).Inc()
+
+	return fmt.Errorf("failed to push data to queue for tenant=%s and queue_name=%s: queue is full", m.tenantID, m.name)
+}
+
+func (m *Queue[T]) StartWorkers() {
+	for i := 0; i < m.workerCount; i++ {
+		m.wg.Add(1)
+
+		go m.worker()
+	}
+}
+
+// TenantID returns the tenant id.
+func (m *Queue[T]) TenantID() string {
+	return m.tenantID
+}
+
+// Size returns the size of the queue.
+func (m *Queue[T]) Size() int {
+	return m.size
+}
+
+// WorkerCount returns the number of expected workers.
+func (m *Queue[T]) WorkerCount() int {
+	return m.workerCount
+}
+
+// ShouldUpdate returns true if the queue size or worker count (alive or total) has changed
+func (m *Queue[T]) ShouldUpdate(size, workerCount int) bool {
+	return m.size != size || m.workerCount != workerCount
+}
+
+func (m *Queue[T]) Shutdown(ctx context.Context) error {
+	// Call to stopWorkers only once
+	if m.readOnly.CompareAndSwap(false, true) {
+		return m.stopWorkers(ctx)
+	}
+
+	return nil
+}
+
+func (m *Queue[T]) worker() {
+	defer m.wg.Done()
+
+	for {
+		select {
+		case req := <-m.reqChan:
+			m.lengthMetric.WithLabelValues(m.name, m.tenantID).Dec()
+			m.forwardRequest(req)
+		case <-m.workersCloseCh:
+			// Forces to always trying to pull from the queue before exiting
+			// This is important during shutdown to ensure that the queue is drained
+			select {
+			case req, ok := <-m.reqChan:
+				if !ok { //closed
+					return
+				}
+
+				m.lengthMetric.WithLabelValues(m.name, m.tenantID).Dec()
+				m.forwardRequest(req)
+			default:
+				return
+			}
+		}
+	}
+}
+
+func (m *Queue[T]) forwardRequest(req T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	ctx = user.InjectOrgID(ctx, m.tenantID)
+
+	m.fn(ctx, req)
+}
+
+func (m *Queue[T]) stopWorkers(ctx context.Context) error {
+	// Close workersCloseCh and wait for all workers to return
+	close(m.workersCloseCh)
+
+	doneCh := make(chan struct{})
+	go func() {
+		m.wg.Wait()
+		close(doneCh)
+	}()
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("failed to stop tenant %s Queue: %w", m.tenantID, ctx.Err())
+	case <-doneCh:
+		return nil
+	}
+}
