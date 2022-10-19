dce6caf71c0af96d6b26da9b3042336a37a2b6dc
Koenraad Verheyden <koenraad.verheyden@grafana.com>
2022-10-19 16:28:17 +0200

    linting

--- pkg/traceql/engine_test.go
+++ pkg/traceql/engine_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/tempo/pkg/tempopb"
-	"github.com/grafana/tempo/pkg/tempopb/common/v1"
+	v1 "github.com/grafana/tempo/pkg/tempopb/common/v1"
 	"github.com/grafana/tempo/pkg/util"
 )
 
