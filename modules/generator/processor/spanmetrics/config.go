f21da452477289c7b9c78aff013fe158cc98d80f
A. Stoewer <adrian@stoewer.me>
2022-12-14 15:32:17 +1000

    Add intrinsic dimenstions to spanmetrics config

--- modules/generator/processor/spanmetrics/config.go
+++ modules/generator/processor/spanmetrics/config.go
@@ -13,11 +13,27 @@ const (
 type Config struct {
 	// Buckets for latency histogram in seconds.
 	HistogramBuckets []float64 `yaml:"histogram_buckets"`
-	// Additional dimensions (labels) to be added to the metric,
-	// along with the default ones (service, span_name, span_kind and span_status).
+	// Intrinsic dimensions (labels) added to the metric, that are generated from fixed span
+	// data. The dimensions service, span_name, span_kind, and status_code are enabled by
+	// default, whereas the dimension status_message must be enabled explicitly.
+	IntrinsicDimensions IntrinsicDimensions `yaml:"intrinsic_dimensions"`
+	// Additional dimensions (labels) to be added to the metric. The dimensions are generated
+	// from span attributes and are created along with the intrinsic dimensions.
 	Dimensions []string `yaml:"dimensions"`
+}
+
+type IntrinsicDimensions struct {
+	Service       bool `yaml:"service"`
+	SpanName      bool `yaml:"span_name"`
+	SpanKind      bool `yaml:"span_kind"`
+	StatusCode    bool `yaml:"status_code"`
+	StatusMessage bool `yaml:"status_message,omitempty"`
 }
 
 func (cfg *Config) RegisterFlagsAndApplyDefaults(prefix string, f *flag.FlagSet) {
 	cfg.HistogramBuckets = prometheus.ExponentialBuckets(0.002, 2, 14)
+	cfg.IntrinsicDimensions.Service = true
+	cfg.IntrinsicDimensions.SpanName = true
+	cfg.IntrinsicDimensions.SpanKind = true
+	cfg.IntrinsicDimensions.StatusCode = true
 }
