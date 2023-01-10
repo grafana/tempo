a06158ca72c82d4fcc9c990737e17de2670e70ff
Joe Elliott <number101010@gmail.com>
2023-01-10 14:16:24 -0500

    async read mode
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tempodb/encoding/vparquet/block_search.go
+++ tempodb/encoding/vparquet/block_search.go
@@ -56,6 +56,7 @@ func (b *backendBlock) openForSearch(ctx context.Context, opts common.SearchOpti
 	o := []parquet.FileOption{
 		parquet.SkipBloomFilters(true),
 		parquet.SkipPageIndex(true),
+		parquet.FileReadMode(parquet.ReadModeAsync),
 	}
 
 	// backend reader
