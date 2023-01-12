a65709fdb87d61d17979a950293c1bc7e5d96e3a
Martin Disibio <mdisibio@gmail.com>
2023-01-12 14:37:06 -0500

    Add tests

--- pkg/traceql/parse_test.go
+++ pkg/traceql/parse_test.go
@@ -893,9 +893,20 @@ func TestIntrinsics(t *testing.T) {
 }
 
 func TestParseIdentifier(t *testing.T) {
-	a, err := ParseIdentifier("span.foo")
-	require.NoError(t, err)
-	require.Equal(t, NewScopedAttribute(AttributeScopeSpan, false, "foo"), a)
+	testCases := map[string]Attribute{
+		"name":             NewIntrinsic(IntrinsicName),
+		"status":           NewIntrinsic(IntrinsicStatus),
+		".name":            NewAttribute("name"),
+		".status":          NewAttribute("status"),
+		".foo.bar":         NewAttribute("foo.bar"),
+		"resource.foo.bar": NewScopedAttribute(AttributeScopeResource, false, "foo.bar"),
+		"span.foo.bar":     NewScopedAttribute(AttributeScopeSpan, false, "foo.bar"),
+	}
+	for i, expected := range testCases {
+		actual, err := ParseIdentifier(i)
+		require.NoError(t, err, i)
+		require.Equal(t, expected, actual, i)
+	}
 }
 
 func TestEmptyQuery(t *testing.T) {
