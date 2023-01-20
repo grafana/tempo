f795a01c8e5f7ecd7de952f13b31aa7daacc209e
Martin Disibio <mdisibio@gmail.com>
2023-01-20 09:50:57 -0500

    Fix iterator error and context handling. Fix LeftJoinIterator performance issue

--- pkg/parquetquery/iters_test.go
+++ pkg/parquetquery/iters_test.go
@@ -78,7 +78,7 @@ func TestColumnIterator(t *testing.T) {
 	require.Nil(t, res)
 }
 
-func TestColumnIteratorContextCanceled(t *testing.T) {
+func TestColumnIteratorExitEarly(t *testing.T) {
 	type T struct{ A int }
 
 	rows := []T{}
@@ -89,24 +89,73 @@ func TestColumnIteratorContextCanceled(t *testing.T) {
 
 	pf := createFileWith(t, rows)
 	idx, _ := GetColumnIndexByPath(pf, "A")
+	readSize := 1000
 
-	ctx, cancel := context.WithCancel(context.TODO())
-	cancel()
-
-	iter := NewColumnIterator(ctx, pf.RowGroups(), idx, "", 1000, nil, "A")
-	defer iter.Close()
-
-	// Verify iterator exits early
-	received := 0
-	for {
-		res, err := iter.Next()
-		require.NoError(t, err)
-		if res == nil {
-			break
+	readIter := func(iter Iterator) (int, error) {
+		received := 0
+		for {
+			res, err := iter.Next()
+			if err != nil {
+				return received, err
+			}
+			if res == nil {
+				break
+			}
+			received++
 		}
-		received++
+		return received, nil
 	}
-	require.Less(t, received, count)
+
+	t.Run("cancelledEarly", func(t *testing.T) {
+		// Cancel before iterating
+		ctx, cancel := context.WithCancel(context.TODO())
+		cancel()
+		iter := NewColumnIterator(ctx, pf.RowGroups(), idx, "", readSize, nil, "A")
+		count, err := readIter(iter)
+		require.ErrorContains(t, err, "context canceled")
+		require.Equal(t, 0, count)
+	})
+
+	t.Run("cancelledPartial", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.TODO())
+		iter := NewColumnIterator(ctx, pf.RowGroups(), idx, "", readSize, nil, "A")
+
+		// Read some results
+		_, err := iter.Next()
+		require.NoError(t, err)
+
+		// Then cancel
+		cancel()
+
+		// Read again = context cancelled
+		_, err = readIter(iter)
+		require.ErrorContains(t, err, "context canceled")
+	})
+
+	t.Run("closedEarly", func(t *testing.T) {
+		// Close before iterating
+		iter := NewColumnIterator(context.TODO(), pf.RowGroups(), idx, "", readSize, nil, "A")
+		iter.Close()
+		count, err := readIter(iter)
+		require.NoError(t, err)
+		require.Equal(t, 0, count)
+	})
+
+	t.Run("closedPartial", func(t *testing.T) {
+		iter := NewColumnIterator(context.TODO(), pf.RowGroups(), idx, "", readSize, nil, "A")
+
+		// Read some results
+		_, err := iter.Next()
+		require.NoError(t, err)
+
+		// Then close
+		iter.Close()
+
+		// Read again = should close early
+		res2, err := readIter(iter)
+		require.NoError(t, err)
+		require.Less(t, readSize+res2, count)
+	})
 }
 
 func BenchmarkColumnIterator(b *testing.B) {
