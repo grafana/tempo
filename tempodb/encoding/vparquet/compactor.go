8c7d9b1a4599c96fb8661a62e9b3013dbcee51e4
A. Stoewer <adrian@stoewer.me>
2023-03-16 08:42:53 +1000

    [vParquet2] Annotate repeated groups as nested list types
    This improves the compatibility with other tooling using the parquet
    format such as parquet-mr/parquet-cli

--- tempodb/encoding/vparquet/compactor.go
+++ tempodb/encoding/vparquet/compactor.go
@@ -324,7 +324,7 @@ func countSpans(schema *parquet.Schema, row parquet.Row) (traceID string, spans 
 		return "", 0
 	}
 
-	spanID, found := schema.Lookup("rs", "ss", "Spans", "SpanID")
+	spanID, found := schema.Lookup("rs", "list", "element", "ss", "list", "element", "Spans", "list", "element", "SpanID")
 	if !found {
 		return "", 0
 	}
