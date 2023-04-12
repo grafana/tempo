faa54d7a66d6c8129a6087aeba7eaccbe7958328
Zach Leslie <zach.leslie@grafana.com>
2023-04-12 21:11:50 +0000

    Include sharedconfig pacakge
    Signed-off-by: Zach Leslie <zach.leslie@grafana.com>

---
+++ pkg/sharedconfig/metrics_generator.go
@@ -0,0 +1,23 @@
+package sharedconfig
+
+type FilterPolicy struct {
+	Include *PolicyMatch `yaml:"include"`
+	Exclude *PolicyMatch `yaml:"exclude"`
+}
+
+type MatchType string
+
+const (
+	Strict MatchType = "strict"
+	Regex  MatchType = "regex"
+)
+
+type PolicyMatch struct {
+	MatchType  MatchType              `yaml:"match_type"`
+	Attributes []MatchPolicyAttribute `yaml:"attributes"`
+}
+
+type MatchPolicyAttribute struct {
+	Key   string      `yaml:"key"`
+	Value interface{} `yaml:"value"`
+}
