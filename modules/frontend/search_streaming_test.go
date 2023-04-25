b916f757d9f42f58840dfdabd2aacf2e6b9e2308
Joe Elliott <number101010@gmail.com>
2023-04-25 11:20:51 -0400

    tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- modules/frontend/search_streaming_test.go
+++ modules/frontend/search_streaming_test.go
@@ -1,82 +1,201 @@
 package frontend
 
-import "testing"
+import (
+	"context"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
 
-func TestStreamingSearchHandler(t *testing.T) {}
+	"github.com/go-kit/log"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/google/uuid"
+	"github.com/grafana/tempo/modules/overrides"
+	"github.com/grafana/tempo/pkg/tempopb"
+	"github.com/grafana/tempo/tempodb/backend"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"github.com/weaveworks/common/user"
+	"go.uber.org/atomic"
+	"google.golang.org/grpc/metadata"
+)
 
-// var fakeGRPCAuthStreamMiddleware = func(srv interface{}, ss grpc.ServerStream, _ *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-// 	ctx := user.InjectOrgID(ss.Context(), util.FakeTenantID)
-// 	return handler(srv, serverStream{
-// 		ctx:          ctx,
-// 		ServerStream: ss,
-// 	})
-// }
+type mockStreamingServer struct {
+	lastResponse atomic.Pointer[*tempopb.SearchResponse]
+	responses    atomic.Int32
+	ctx          context.Context
+}
 
-// type serverStream struct {
-// 	ctx context.Context
-// 	grpc.ServerStream
-// }
+func (m *mockStreamingServer) Send(r *tempopb.SearchResponse) error {
+	m.lastResponse.Store(&r)
+	m.responses.Inc()
+	return nil
+}
+func (m *mockStreamingServer) Context() context.Context     { return m.ctx }
+func (m *mockStreamingServer) SendHeader(metadata.MD) error { return nil }
+func (m *mockStreamingServer) SetHeader(metadata.MD) error  { return nil }
+func (m *mockStreamingServer) SendMsg(interface{}) error    { return nil }
+func (m *mockStreamingServer) RecvMsg(interface{}) error    { return nil }
+func (m *mockStreamingServer) SetTrailer(metadata.MD)       {}
 
-// func (ss serverStream) Context() context.Context {
-// 	return ss.ctx
-// }
+func newMockStreamingServer() *mockStreamingServer {
+	return &mockStreamingServer{
+		ctx: user.InjectOrgID(context.Background(), "fake-tenant"),
+	}
+}
 
-// func TestFrontendStreamingSearch(t *testing.T) {
-// 	o, err := overrides.NewOverrides(overrides.Limits{})
-// 	require.NoError(t, err)
+func TestStreamingSearchHandlerSucceeds(t *testing.T) {
+	traceResp := []*tempopb.TraceSearchMetadata{
+		{
+			TraceID:         "1234",
+			RootServiceName: "root",
+		},
+	}
 
-// 	next := &mockNextTripperware{}
-// 	f, err := New(Config{
-// 		TraceByID: TraceByIDConfig{
-// 			QueryShards: minQueryShards,
-// 			SLO:         testSLOcfg,
-// 		},
-// 		Search: SearchConfig{
-// 			Sharder: SearchSharderConfig{
-// 				ConcurrentRequests:    defaultConcurrentRequests,
-// 				TargetBytesPerRequest: defaultTargetBytesPerRequest,
-// 			},
-// 			SLO: testSLOcfg,
-// 		},
-// 	}, next, o, nil, log.NewNopLogger(), "", nil)
-// 	require.NoError(t, err)
+	next := RoundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		response := &tempopb.SearchResponse{
+			Traces:  traceResp,
+			Metrics: &tempopb.SearchMetrics{},
+		}
+		resString, err := (&jsonpb.Marshaler{}).MarshalToString(response)
+		require.NoError(t, err)
 
-// 	srv, err := server.New(server.Config{
-// 		GRPCListenPort:           9090,
-// 		GRPCServerMaxSendMsgSize: 100 * 1024 * 1024,
-// 		GPRCServerMaxRecvMsgSize: 100 * 1024 * 1024,
-// 		GRPCStreamMiddleware: []grpc.StreamServerInterceptor{
-// 			fakeGRPCAuthStreamMiddleware,
-// 		},
-// 	})
-// 	require.NoError(t, err)
+		return &http.Response{
+			Body:       io.NopCloser(strings.NewReader(resString)),
+			StatusCode: 200,
+		}, nil
+	})
 
-// 	tempopb.RegisterStreamingQuerierServer(srv.GRPC, f)
+	srv := newMockStreamingServer()
+	handler := testHandler(t, next)
+	err := handler(&tempopb.SearchRequest{
+		Start: 1000,
+		End:   1500,
+		Query: "{}",
+	}, srv)
+	require.NoError(t, err)
+	// confirm final result is expected
+	require.Equal(t,
+		*srv.lastResponse.Load(),
+		&tempopb.SearchResponse{
+			Traces: traceResp,
+			Metrics: &tempopb.SearchMetrics{
+				InspectedBlocks: 1,
+				CompletedJobs:   2,
+				TotalJobs:       2,
+				TotalBlockBytes: 209715200,
+			},
+		},
+	)
+}
 
-// 	// start grpc server
-// 	go func() {
-// 		err := srv.Run()
-// 		require.NoError(t, err)
-// 	}()
+func TestStreamingSearchHandlerStreams(t *testing.T) {
+	next := RoundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		time.Sleep(1 * time.Second) // forces the streaming responses to work
 
-// 	// create a fake grpc client
-// 	conn, err := grpc.DialContext(context.Background(), "localhost:9090", grpc.WithInsecure(), grpc.WithBlock())
-// 	require.NoError(t, err)
-// 	defer conn.Close()
+		response := &tempopb.SearchResponse{Metrics: &tempopb.SearchMetrics{}}
+		resString, err := (&jsonpb.Marshaler{}).MarshalToString(response)
+		require.NoError(t, err)
 
-// 	client := tempopb.NewStreamingQuerierClient(conn)
-// 	c, err := client.Search(context.Background(), &tempopb.SearchRequest{
-// 		Start: 0,
-// 		End:   10,
-// 		Query: "{}",
-// 	})
-// 	require.NoError(t, err)
+		return &http.Response{
+			Body:       io.NopCloser(strings.NewReader(resString)),
+			StatusCode: 200,
+		}, nil
+	})
 
-// 	for {
-// 		_, err := c.Recv()
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		require.NoError(t, err)
-// 	}
-// }
+	srv := newMockStreamingServer()
+	handler := testHandler(t, next)
+	err := handler(&tempopb.SearchRequest{
+		Start: 1000,
+		End:   1500,
+		Query: "{}",
+	}, srv)
+	require.NoError(t, err)
+	require.GreaterOrEqual(t, srv.responses.Load(), int32(3)) // confirm that our server got 3 or more sends
+}
+
+func TestStreamingSearchHandlerCancels(t *testing.T) {
+	next := RoundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		time.Sleep(24 * time.Hour) // will break any test time limits
+		return nil, nil
+	})
+
+	var cancel context.CancelFunc
+	srv := newMockStreamingServer()
+	srv.ctx, cancel = context.WithCancel(srv.ctx)
+
+	go func() {
+		time.Sleep(500 * time.Millisecond)
+		cancel()
+	}()
+
+	handler := testHandler(t, next)
+	err := handler(&tempopb.SearchRequest{
+		Start: 1000,
+		End:   1500,
+		Query: "{}",
+	}, srv)
+	require.Equal(t, context.Canceled, err)
+}
+
+func TestStreamingSearchHandlerFailsDueToStatusCode(t *testing.T) {
+	next := RoundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		return &http.Response{
+			Body:       io.NopCloser(strings.NewReader("error!")),
+			StatusCode: 500,
+		}, nil
+	})
+
+	srv := newMockStreamingServer()
+	handler := testHandler(t, next)
+	err := handler(&tempopb.SearchRequest{
+		Start: 1000,
+		End:   1500,
+		Query: "{}",
+	}, srv)
+	require.Error(t, err)
+}
+
+func TestStreamingSearchHandlerFailsDueToError(t *testing.T) {
+	next := RoundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		return nil, errors.New("error!")
+	})
+
+	srv := newMockStreamingServer()
+	handler := testHandler(t, next)
+	err := handler(&tempopb.SearchRequest{
+		Start: 1000,
+		End:   1500,
+		Query: "{}",
+	}, srv)
+	require.Error(t, err)
+}
+
+func testHandler(t *testing.T, next http.RoundTripper) streamingSearchHandler {
+	t.Helper()
+
+	o, err := overrides.NewOverrides(overrides.Limits{})
+	require.NoError(t, err)
+
+	handler := newSearchStreamingHandler(Config{
+		Search: SearchConfig{
+			Sharder: SearchSharderConfig{
+				ConcurrentRequests:    1, // 1 concurrent request to force order
+				TargetBytesPerRequest: defaultTargetBytesPerRequest,
+			},
+		},
+	}, o, next, &mockReader{
+		metas: []*backend.BlockMeta{ // one block with 2 records that are each the target bytes per request will force 2 sub queries
+			{
+				StartTime:    time.Unix(1100, 0),
+				EndTime:      time.Unix(1200, 0),
+				Size:         defaultTargetBytesPerRequest * 2,
+				TotalRecords: 2,
+				BlockID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+			},
+		},
+	}, "", log.NewNopLogger())
+
+	return handler
+}
