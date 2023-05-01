480a1b54ed1e91a3c02fd3cfaec2cde6cfb168cd
Joe Elliott <number101010@gmail.com>
2023-05-01 13:24:08 -0400

    fix race (#2415)
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- modules/ingester/instance_search.go
+++ modules/ingester/instance_search.go
@@ -439,11 +439,10 @@ func (i *instance) SearchTagValuesV2(ctx context.Context, req *tempopb.SearchTag
 			return nil // Early exit if any error has occurred
 		}
 
-		if maxBlocks > 0 && inspectedBlocks.Load() >= maxBlocks {
+		if maxBlocks > 0 && inspectedBlocks.Inc() > maxBlocks {
 			return nil
 		}
 
-		inspectedBlocks.Inc()
 		fetcher := traceql.NewSpansetFetcherWrapper(func(ctx context.Context, req traceql.FetchSpansRequest) (traceql.FetchSpansResponse, error) {
 			return s.Fetch(ctx, req, common.DefaultSearchOptions())
 		})
