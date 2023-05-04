eab5c68d7d7ef839609ddf3276a86caaaf8a2a05
A. Stoewer <adrian@stoewer.me>
2023-05-04 10:43:37 +1000

    Add function to assign nested set model bounds to traces

---
+++ tempodb/encoding/vparquet2/nested_set_model.go
@@ -0,0 +1,106 @@
+package vparquet2
+
+import "github.com/grafana/tempo/pkg/util"
+
+// TODO should root spans and children be sorted before traversal to make the outcome more predictable
+func assignNestedSetModelBounds(trace *Trace) {
+	var (
+		assignmentNeeded bool
+		rootSpans        []*Span
+		spanChildren     = map[uint64][]*Span{}
+	)
+
+	// find root spans and map span IDs to child spans
+	for _, rs := range trace.ResourceSpans {
+		for _, ss := range rs.ScopeSpans {
+			for i, s := range ss.Spans {
+				if s.NestedSetLeft == 0 || s.NestedSetRight == 0 {
+					assignmentNeeded = true
+				}
+
+				if len(s.ParentSpanID) == 0 {
+					rootSpans = append(rootSpans, &ss.Spans[i])
+				} else {
+					parentID := util.SpanIDToUint64(s.ParentSpanID)
+					spanChildren[parentID] = append(spanChildren[parentID], &ss.Spans[i])
+				}
+			}
+		}
+	}
+
+	if len(rootSpans) == 0 || !assignmentNeeded {
+		return
+	}
+
+	// traverse the tree
+	var (
+		ancestors      stack[wrappedSpan]
+		nestedSetBound int32 = 1
+	)
+
+	for _, root := range rootSpans {
+		root.NestedSetLeft = nestedSetBound
+		nestedSetBound++
+
+		ancestors.reset()
+		ancestors.push(&wrappedSpan{span: root, id: util.SpanIDToUint64(root.SpanID)})
+
+		for !ancestors.isEmpty() {
+			parent := ancestors.peek()
+			children := spanChildren[parent.id]
+
+			if parent.nextChild < len(children) {
+				child := children[parent.nextChild]
+				child.ParentID = parent.span.NestedSetLeft
+				parent.nextChild++
+
+				child.NestedSetLeft = nestedSetBound
+				nestedSetBound++
+
+				ancestors.push(&wrappedSpan{span: child, id: util.SpanIDToUint64(child.SpanID)})
+			} else {
+				parent.span.NestedSetRight = nestedSetBound
+				nestedSetBound++
+
+				ancestors.pop()
+			}
+		}
+	}
+}
+
+type wrappedSpan struct {
+	span      *Span
+	id        uint64
+	nextChild int
+}
+
+type stack[T any] []*T
+
+func (ss *stack[T]) push(element *T) {
+	*ss = append(*ss, element)
+}
+
+func (ss *stack[T]) peek() *T {
+	if len(*ss) == 0 {
+		return nil
+	}
+	return (*ss)[len(*ss)-1]
+}
+
+func (ss *stack[T]) pop() *T {
+	if len(*ss) == 0 {
+		return nil
+	}
+	i := len(*ss) - 1
+	s := (*ss)[i]
+	*ss = (*ss)[:i]
+	return s
+}
+
+func (ss *stack[T]) isEmpty() bool {
+	return len(*ss) == 0
+}
+
+func (ss *stack[T]) reset() {
+	*ss = (*ss)[:0]
+}
---
+++ tempodb/encoding/vparquet2/nested_set_model_test.go
@@ -0,0 +1,254 @@
+package vparquet2
+
+import (
+	"github.com/grafana/tempo/pkg/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAssignNestedSetModelBounds(t *testing.T) {
+	tests := []struct {
+		name     string
+		trace    [][]Span
+		expected [][]Span
+	}{
+		{
+			name: "single span",
+			trace: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa")},
+				},
+			},
+			expected: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa"), NestedSetLeft: 1, NestedSetRight: 2},
+				},
+			},
+		},
+		{
+			name: "linear trace",
+			trace: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb")},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa")},
+				},
+			},
+			expected: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa"), NestedSetLeft: 1, NestedSetRight: 6},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa"), NestedSetLeft: 2, NestedSetRight: 5, ParentID: 1},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 3, NestedSetRight: 4, ParentID: 2},
+				},
+			},
+		},
+		{
+			name: "branched trace",
+			trace: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb")},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb")},
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("dddddddd")},
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("dddddddd")},
+				},
+			},
+			expected: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa"), NestedSetLeft: 1, NestedSetRight: 12},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa"), NestedSetLeft: 2, NestedSetRight: 11, ParentID: 1},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 3, NestedSetRight: 4, ParentID: 2},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 5, NestedSetRight: 10, ParentID: 2},
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("dddddddd"), NestedSetLeft: 6, NestedSetRight: 7, ParentID: 5},
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("dddddddd"), NestedSetLeft: 8, NestedSetRight: 9, ParentID: 5},
+				},
+			},
+		},
+		{
+			name: "multiple scope spans",
+			trace: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb")},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb")},
+				},
+				{
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("dddddddd")},
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("dddddddd")},
+				},
+			},
+			expected: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa"), NestedSetLeft: 1, NestedSetRight: 12},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa"), NestedSetLeft: 2, NestedSetRight: 11, ParentID: 1},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 3, NestedSetRight: 4, ParentID: 2},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 5, NestedSetRight: 10, ParentID: 2},
+				},
+				{
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("dddddddd"), NestedSetLeft: 6, NestedSetRight: 7, ParentID: 5},
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("dddddddd"), NestedSetLeft: 8, NestedSetRight: 9, ParentID: 5},
+				},
+			},
+		},
+		{
+			name: "multiple roots",
+			trace: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb")},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb")},
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("dddddddd")},
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("dddddddd")},
+
+					{SpanID: []byte("gggggggg")},
+					{SpanID: []byte("iiiiiiii"), ParentSpanID: []byte("hhhhhhhh")},
+					{SpanID: []byte("hhhhhhhh"), ParentSpanID: []byte("gggggggg")},
+				},
+			},
+			expected: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa"), NestedSetLeft: 1, NestedSetRight: 12},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa"), NestedSetLeft: 2, NestedSetRight: 11, ParentID: 1},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 3, NestedSetRight: 4, ParentID: 2},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 5, NestedSetRight: 10, ParentID: 2},
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("dddddddd"), NestedSetLeft: 6, NestedSetRight: 7, ParentID: 5},
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("dddddddd"), NestedSetLeft: 8, NestedSetRight: 9, ParentID: 5},
+
+					{SpanID: []byte("gggggggg"), NestedSetLeft: 13, NestedSetRight: 18},
+					{SpanID: []byte("hhhhhhhh"), ParentSpanID: []byte("gggggggg"), NestedSetLeft: 14, NestedSetRight: 17, ParentID: 13},
+					{SpanID: []byte("iiiiiiii"), ParentSpanID: []byte("hhhhhhhh"), NestedSetLeft: 15, NestedSetRight: 16, ParentID: 14},
+				},
+			},
+		},
+		{
+			name: "interrupted",
+			trace: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa")},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb")},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb")},
+
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("xxxxxxxx")}, // <- interrupted
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("eeeeeeee")},
+				},
+			},
+			expected: [][]Span{
+				{
+					{SpanID: []byte("aaaaaaaa"), NestedSetLeft: 1, NestedSetRight: 8},
+					{SpanID: []byte("bbbbbbbb"), ParentSpanID: []byte("aaaaaaaa"), NestedSetLeft: 2, NestedSetRight: 7, ParentID: 1},
+					{SpanID: []byte("cccccccc"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 3, NestedSetRight: 4, ParentID: 2},
+					{SpanID: []byte("dddddddd"), ParentSpanID: []byte("bbbbbbbb"), NestedSetLeft: 5, NestedSetRight: 6, ParentID: 2},
+
+					{SpanID: []byte("eeeeeeee"), ParentSpanID: []byte("xxxxxxxx")}, // <- interrupted
+					{SpanID: []byte("ffffffff"), ParentSpanID: []byte("eeeeeeee")},
+				},
+			},
+		},
+	}
+
+	makeTrace := func(traceSpans [][]Span) *Trace {
+		var resourceSpans ResourceSpans
+		for _, spans := range traceSpans {
+			var scopeSpans ScopeSpans
+			for _, s := range spans {
+				scopeSpans.Spans = append(scopeSpans.Spans, s)
+			}
+			resourceSpans.ScopeSpans = append(resourceSpans.ScopeSpans, scopeSpans)
+		}
+		return &Trace{ResourceSpans: []ResourceSpans{resourceSpans}}
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			trace := makeTrace(tt.trace)
+			expected := makeTrace(tt.expected)
+			assignNestedSetModelBounds(trace)
+			assertEqualNestedSetModelBounds(t, trace, expected)
+		})
+	}
+}
+
+func assertEqualNestedSetModelBounds(t testing.TB, actual, expected *Trace) {
+	t.Helper()
+
+	actualSpans := map[uint64]*Span{}
+	actualCount := 0
+	for _, rs := range actual.ResourceSpans {
+		for _, ss := range rs.ScopeSpans {
+			for i, s := range ss.Spans {
+				actualSpans[util.SpanIDToUint64(s.SpanID)] = &ss.Spans[i]
+				actualCount++
+			}
+		}
+	}
+
+	expectedCount := 0
+	for _, rs := range expected.ResourceSpans {
+		for _, ss := range rs.ScopeSpans {
+			for _, exp := range ss.Spans {
+				expectedCount++
+				act, ok := actualSpans[util.SpanIDToUint64(exp.SpanID)]
+				require.Truef(t, ok, "span '%v' expected but was missing", string(exp.SpanID))
+				assert.Equalf(t, exp.NestedSetLeft, act.NestedSetLeft, "span '%v' NestedSetLeft is expected %d but was %d", string(exp.SpanID), exp.NestedSetLeft, act.NestedSetLeft)
+				assert.Equalf(t, exp.NestedSetRight, act.NestedSetRight, "span '%v' NestedSetRight is expected %d but was %d", string(exp.SpanID), exp.NestedSetRight, act.NestedSetRight)
+				assert.Equalf(t, exp.ParentID, act.ParentID, "span '%v' ParentID is expected %d but was %d", string(exp.SpanID), exp.ParentID, act.ParentID)
+				assert.Equalf(t, exp.ParentSpanID, act.ParentSpanID, "span '%v' ParentSpanID is expected %d but was %d", string(exp.SpanID), string(exp.ParentSpanID), string(act.ParentSpanID))
+			}
+		}
+	}
+
+	assert.Equalf(t, expectedCount, actualCount, "expected %d spans but found %d instead", expectedCount, actualCount)
+}
+
+func TestStack(t *testing.T) {
+	type testVal struct {
+		val int
+	}
+
+	var (
+		testStack stack[testVal]
+		val       *testVal
+	)
+
+	assert.Len(t, testStack, 0)
+	assert.True(t, testStack.isEmpty(), "testStack expected to be empty")
+
+	val = testStack.peek()
+	assert.Nil(t, val, "testStack.peek() expected to be nil")
+	val = testStack.pop()
+	assert.Nil(t, val, "testStack.pop() expected to be nil")
+
+	testStack.push(&testVal{1})
+	val = testStack.peek()
+	require.NotNil(t, val)
+	assert.Equal(t, &testVal{1}, val)
+
+	testStack.push(&testVal{2})
+	val = testStack.peek()
+	require.NotNil(t, val)
+	assert.Equal(t, &testVal{2}, val)
+
+	val = testStack.pop()
+	require.NotNil(t, val)
+	assert.Equal(t, &testVal{2}, val)
+
+	val = testStack.pop()
+	require.NotNil(t, val)
+	assert.Equal(t, &testVal{1}, val)
+
+	val = testStack.pop()
+	assert.True(t, testStack.isEmpty(), "testStack expected to be empty")
+	assert.Nil(t, val, "testStack.peek() expected to be nil")
+	assert.Nil(t, val, "testStack.pop() expected to be nil")
+
+	testStack.push(&testVal{1})
+	testStack.reset()
+	assert.True(t, testStack.isEmpty(), "testStack expected to be empty")
+	assert.Len(t, testStack, 0)
+	assert.Greater(t, cap(testStack), 0)
+}
