45bd9ff02bbf1f8819ee15b0d1fd7faebc1803b0
Zach Leslie <zach.leslie@grafana.com>
2023-05-04 16:46:09 +0000

    Include make fragment for installing tools add dependent target
    Signed-off-by: Zach Leslie <zach.leslie@grafana.com>

---
+++ build/tools.mk
@@ -0,0 +1,32 @@
+#
+# Makefile fragment for installing tools
+#
+
+GO               ?= go
+GOFMT            ?= gofmt
+VENDOR_CMD       ?= ${GO} mod tidy
+GO_MOD_OUTDATED  ?= go-mod-outdated
+
+# Go file to track tool deps with go modules
+TOOL_DIR     ?= tools
+TOOL_CONFIG  ?= $(TOOL_DIR)/tools.go
+
+GOTOOLS ?= $(shell cd $(TOOL_DIR) && go list -f '{{ .Imports }}' -tags tools |tr -d '[]')
+
+tools:
+	@echo "=== [ tools            ]: Installing tools required by the project..."
+	@cd $(TOOL_DIR) && $(GO) install $(GOTOOLS)
+	@cd $(TOOL_DIR) && $(VENDOR_CMD)
+
+tools-outdated:
+	@echo "=== [ tools-outdated   ]: Finding outdated tool deps with $(GO_MOD_OUTDATED)..."
+	@cd $(TOOL_DIR) && $(GO) list -u -m -json all | $(GO_MOD_OUTDATED) -direct -update
+
+tools-update:
+	@echo "=== [ tools-update     ]: Updating tools required by the project..."
+	@cd $(TOOL_DIR) && for x in $(GOTOOLS); do \
+		$(GO) get -u $$x; \
+	done
+	@cd $(TOOL_DIR) && $(VENDOR_CMD)
+
+.PHONY: tools tools-update tools-outdated
