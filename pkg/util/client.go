2a49cdf1429d1b7b5e8953ee7629b28ef65ed6d0
Jennie Pham <jenniepham0221@gmail.com>
2023-05-14 22:35:35 -0500

    add range to traceql search for vulture

--- pkg/util/client.go
+++ pkg/util/client.go
@@ -141,9 +141,7 @@ func (c *Client) Search(tags string) (*tempopb.SearchResponse, error) {
 // epoch timestamps in seconds.
 func (c *Client) SearchWithRange(tags string, start int64, end int64) (*tempopb.SearchResponse, error) {
 	m := &tempopb.SearchResponse{}
-	query := c.BaseURL+"/api/search?tags="+url.QueryEscape(tags)+"&start="+strconv.FormatInt(start, 10)+"&end="+strconv.FormatInt(end, 10)
-	fmt.Println(query)
-	_, err := c.getFor(query, m)
+	_, err := c.getFor(c.BaseURL+"/api/search?tags="+url.QueryEscape(tags)+"&start="+strconv.FormatInt(start, 10)+"&end="+strconv.FormatInt(end, 10), m)
 	if err != nil {
 		return nil, err
 	}
@@ -166,9 +164,17 @@ func (c *Client) QueryTrace(id string) (*tempopb.Trace, error) {
 
 func (c *Client) SearchTraceQL(query string) (*tempopb.SearchResponse, error) {
 	m := &tempopb.SearchResponse{}
-	queryURL := c.BaseURL+"/api/search?q="+url.QueryEscape(query)
-	fmt.Println(queryURL)
-	_, err := c.getFor(queryURL, m)
+	_, err := c.getFor(c.BaseURL+"/api/search?q="+url.QueryEscape(query), m)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (c *Client) SearchTraceQLWithRange(query string, start int64, end int64) (*tempopb.SearchResponse, error) {
+	m := &tempopb.SearchResponse{}
+	_, err := c.getFor(c.BaseURL+"/api/search?q="+url.QueryEscape(query)+"&start="+strconv.FormatInt(start, 10)+"&end="+strconv.FormatInt(end, 10), m)
 	if err != nil {
 		return nil, err
 	}
