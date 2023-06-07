ad6532fd3aceee6b9a2073f91a7966f9a57c0ad3
Manish <10056406+mghildiy@users.noreply.github.com>
2023-06-07 15:25:06 +0530

    FIX-699: Add capability to completely flush on shutdown (#2538)

--- modules/ingester/config.go
+++ modules/ingester/config.go
@@ -25,6 +25,7 @@ type Config struct {
 	MaxBlockBytes        uint64        `yaml:"max_block_bytes"`
 	CompleteBlockTimeout time.Duration `yaml:"complete_block_timeout"`
 	OverrideRingKey      string        `yaml:"override_ring_key"`
+	FlushAllOnShutdown   bool          `yaml:"flush_all_on_shutdown"`
 
 	AutocompleteFilteringEnabled bool `yaml:"-"`
 }
@@ -40,6 +41,7 @@ func (cfg *Config) RegisterFlagsAndApplyDefaults(prefix string, f *flag.FlagSet)
 	cfg.ConcurrentFlushes = 4
 	cfg.FlushCheckPeriod = 10 * time.Second
 	cfg.FlushOpTimeout = 5 * time.Minute
+	cfg.FlushAllOnShutdown = false
 
 	f.DurationVar(&cfg.MaxTraceIdle, prefix+".trace-idle-period", 10*time.Second, "Duration after which to consider a trace complete if no spans have been received")
 	f.DurationVar(&cfg.MaxBlockDuration, prefix+".max-block-duration", 30*time.Minute, "Maximum duration which the head block can be appended to before cutting it.")
