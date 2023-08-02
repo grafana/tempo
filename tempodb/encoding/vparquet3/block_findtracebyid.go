7dea128c6222cf9872e2fd62fc9d169be3fc1582
Martin Disibio <mdisibio@gmail.com>
2023-08-02 14:30:42 -0400

    the opposite of move fast and break things

--- tempodb/encoding/vparquet3/block_findtracebyid.go
+++ tempodb/encoding/vparquet3/block_findtracebyid.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
@@ -13,6 +14,7 @@ import (
 	"github.com/grafana/tempo/pkg/parquetquery"
 	pq "github.com/grafana/tempo/pkg/parquetquery"
 	"github.com/grafana/tempo/pkg/tempopb"
+	"github.com/grafana/tempo/pkg/util"
 	"github.com/grafana/tempo/tempodb/backend"
 	"github.com/grafana/tempo/tempodb/encoding/common"
 )
@@ -51,8 +53,8 @@ func (b *backendBlock) checkBloom(ctx context.Context, id common.ID) (found bool
 	return filter.Test(id), nil
 }
 
-func (b *backendBlock) checkIndex(ctx context.Context, id common.ID) (int, error) {
-	span, derivedCtx := opentracing.StartSpanFromContext(ctx, "parquet.backendBlock.checkIndex",
+func (b *backendBlock) checkIndex(ctx context.Context, id common.ID) (bool, int, error) {
+	span, derivedCtx := opentracing.StartSpanFromContext(ctx, "parquet3.backendBlock.checkIndex",
 		opentracing.Tags{
 			"blockID":  b.meta.BlockID,
 			"tenantID": b.meta.TenantID,
@@ -60,16 +62,25 @@ func (b *backendBlock) checkIndex(ctx context.Context, id common.ID) (int, error
 	defer span.Finish()
 
 	indexBytes, err := b.r.Read(derivedCtx, common.NameIndex, b.meta.BlockID, b.meta.TenantID, true)
+	if err == backend.ErrDoesNotExist {
+		return true, -1, nil
+	}
 	if err != nil {
-		return -1, fmt.Errorf("error retrieving index (%s, %s): %w", b.meta.TenantID, b.meta.BlockID, err)
+		return false, -1, fmt.Errorf("error retrieving index (%s, %s): %w", b.meta.TenantID, b.meta.BlockID, err)
 	}
 
 	index, err := unmarshalIndex(indexBytes)
 	if err != nil {
-		return -1, fmt.Errorf("error parsing index (%s, %s): %w", b.meta.TenantID, b.meta.BlockID, err)
+		return false, -1, fmt.Errorf("error parsing index (%s, %s): %w", b.meta.TenantID, b.meta.BlockID, err)
 	}
 
-	return index.Find(id), nil
+	rowGroup := index.Find(id)
+	if rowGroup == -1 {
+		// Ruled out by index
+		return false, -1, nil
+	}
+
+	return true, rowGroup, nil
 }
 
 func (b *backendBlock) FindTraceByID(ctx context.Context, traceID common.ID, opts common.SearchOptions) (_ *tempopb.Trace, err error) {
@@ -89,11 +100,11 @@ func (b *backendBlock) FindTraceByID(ctx context.Context, traceID common.ID, opt
 		return nil, nil
 	}
 
-	rowGroup, err := b.checkIndex(ctx, traceID)
+	ok, rowGroup, err := b.checkIndex(ctx, traceID)
 	if err != nil {
 		return nil, err
 	}
-	if rowGroup < 0 {
+	if !ok {
 		return nil, nil
 	}
 
@@ -113,6 +124,88 @@ func findTraceByID(ctx context.Context, traceID common.ID, meta *backend.BlockMe
 	colIndex, _ := pq.GetColumnIndexByPath(pf, TraceIDColumnName)
 	if colIndex == -1 {
 		return nil, fmt.Errorf("unable to get index for column: %s", TraceIDColumnName)
+	}
+
+	// If no index then fallback to binary searching the rowgroups.
+	if rowGroup == -1 {
+		var (
+			numRowGroups = len(pf.RowGroups())
+			buf          = make(parquet.Row, 1)
+			err          error
+		)
+
+		// Cache of row group bounds
+		rowGroupMins := make([]common.ID, numRowGroups+1)
+		// todo: restore using meta min/max id once it works
+		//    https://github.com/grafana/tempo/issues/1903
+		rowGroupMins[0] = bytes.Repeat([]byte{0}, 16)
+		rowGroupMins[numRowGroups] = bytes.Repeat([]byte{255}, 16) // This is actually inclusive and the logic is special for the last row group below
+
+		// Gets the minimum trace ID within the row group. Since the column is sorted
+		// ascending we just read the first value from the first page.
+		getRowGroupMin := func(rgIdx int) (common.ID, error) {
+			min := rowGroupMins[rgIdx]
+			if len(min) > 0 {
+				// Already loaded
+				return min, nil
+			}
+
+			pages := pf.RowGroups()[rgIdx].ColumnChunks()[colIndex].Pages()
+			defer pages.Close()
+
+			page, err := pages.ReadPage()
+			if err != nil {
+				return nil, err
+			}
+
+			c, err := page.Values().ReadValues(buf)
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+			if c < 1 {
+				return nil, fmt.Errorf("failed to read value from page: traceID: %s blockID:%v rowGroupIdx:%d", util.TraceIDToHexString(traceID), meta.BlockID, rgIdx)
+			}
+
+			min = buf[0].ByteArray()
+			rowGroupMins[rgIdx] = min
+			return min, nil
+		}
+
+		rowGroup, err = binarySearch(numRowGroups, func(rgIdx int) (int, error) {
+			min, err := getRowGroupMin(rgIdx)
+			if err != nil {
+				return 0, err
+			}
+
+			if check := bytes.Compare(traceID, min); check <= 0 {
+				// Trace is before or in this group
+				return check, nil
+			}
+
+			max, err := getRowGroupMin(rgIdx + 1)
+			if err != nil {
+				return 0, err
+			}
+
+			// This is actually the min of the next group, so check is exclusive not inclusive like min
+			// Except for the last group, it is inclusive
+			check := bytes.Compare(traceID, max)
+			if check > 0 || (check == 0 && rgIdx < (numRowGroups-1)) {
+				// Trace is after this group
+				return 1, nil
+			}
+
+			// Must be in this group
+			return 0, nil
+		})
+		if err != nil {
+			return nil, errors.Wrap(err, "error binary searching row groups")
+		}
+	}
+
+	if rowGroup == -1 {
+		// Not within the bounds of any row group
+		return nil, nil
 	}
 
 	// Now iterate the matching row group
@@ -151,4 +244,31 @@ func findTraceByID(ctx context.Context, traceID common.ID, meta *backend.BlockMe
 
 	// convert to proto trace and return
 	return parquetTraceToTempopbTrace(meta, tr), nil
+}
+
+// binarySearch that finds exact matching entry. Returns non-zero index when found, or -1 when not found
+// Inspired by sort.Search but makes uses of tri-state comparator to eliminate the last comparison when
+// we want to find exact match, not insertion point.
+func binarySearch(n int, compare func(int) (int, error)) (int, error) {
+	i, j := 0, n
+	for i < j {
+		h := int(uint(i+j) >> 1) // avoid overflow when computing h
+		c, err := compare(h)
+		if err != nil {
+			return -1, err
+		}
+		// i ≤ h < j
+		switch c {
+		case 0:
+			// Found exact match
+			return h, nil
+		case -1:
+			j = h
+		case 1:
+			i = h + 1
+		}
+	}
+
+	// No match
+	return -1, nil
 }
