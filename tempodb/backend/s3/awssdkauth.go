5e18d78bae65b5792b8bfa61dfb29dd7e2799294
Joe Elliott <number101010@gmail.com>
2023-08-30 15:01:16 -0400

    Backport https://github.com/cortexproject/cortex/pull/4897 to fix IDMSv1 (#2760) (#2871)
    * Backport https://github.com/cortexproject/cortex/pull/4897 to fix IDMSv1
    
    Signed-off-by: Jakub Coufal <jakub.coufal@jamf.com>
    
    * Update CHANGELOG.md
    
    Signed-off-by: Jakub Coufal <jakub.coufal@jamf.com>
    
    ---------
    
    Signed-off-by: Jakub Coufal <jakub.coufal@jamf.com>
    (cherry picked from commit ba621f626506f986e2c76cc379e6af56660891f4)
    
    Co-authored-by: Jakub Coufal <jakub.coufal@jamf.com>

---
+++ tempodb/backend/s3/awssdkauth.go
@@ -0,0 +1,40 @@
+package s3
+
+import (
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/defaults"
+	mcreds "github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/pkg/errors"
+)
+
+func NewAWSSDKAuth(region string) *AWSSDKAuth {
+	dc := defaults.Config().WithRegion(region)
+	creds := defaults.CredChain(dc, defaults.Handlers())
+	return &AWSSDKAuth{
+		creds: creds,
+	}
+}
+
+// AWSSDKAuth retrieves credentials from the aws-sdk-go.
+type AWSSDKAuth struct {
+	creds *credentials.Credentials
+}
+
+// Retrieve retrieves the keys from the environment.
+func (a *AWSSDKAuth) Retrieve() (mcreds.Value, error) {
+	val, err := a.creds.Get()
+	if err != nil {
+		return mcreds.Value{}, errors.Wrap(err, "retrieve AWS SDK credentials")
+	}
+	return mcreds.Value{
+		AccessKeyID:     val.AccessKeyID,
+		SecretAccessKey: val.SecretAccessKey,
+		SessionToken:    val.SessionToken,
+		SignerType:      mcreds.SignatureV4,
+	}, nil
+}
+
+// IsExpired returns if the credentials have been retrieved.
+func (a *AWSSDKAuth) IsExpired() bool {
+	return a.creds.IsExpired()
+}
