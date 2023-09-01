6fdb1a992f6c42a0f91bd18b942f5bc5a786a794
Joe Elliott <number101010@gmail.com>
2023-09-01 07:44:26 -0400

    added key
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tempodb/encoding/vparquet3/block_traceql.go
+++ tempodb/encoding/vparquet3/block_traceql.go
@@ -1896,7 +1896,8 @@ func (c *attributeCollector) KeepGroup(res *parquetquery.IteratorResult) bool {
 		}
 	}
 
-	if val.Type == traceql.TypeNil {
+	// if this row didn't match up a key/value pair then just drop it
+	if val.Type == traceql.TypeNil || key == "" {
 		return false
 	}
 
