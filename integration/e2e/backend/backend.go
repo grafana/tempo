7dcf9fd7b6e8b9ef39ce18c976436c47491d3073
Zach Leslie <zach.leslie@grafana.com>
2023-09-20 22:39:40 +0000

    Fix azure config reference

--- integration/e2e/backend/backend.go
+++ integration/e2e/backend/backend.go
@@ -12,7 +12,7 @@ import (
 	"github.com/grafana/tempo/cmd/tempo/app"
 	util "github.com/grafana/tempo/integration"
 	"github.com/grafana/tempo/tempodb/backend"
-	"github.com/grafana/tempo/tempodb/backend/azure"
+	azure "github.com/grafana/tempo/tempodb/backend/azure/v1"
 )
 
 const (
