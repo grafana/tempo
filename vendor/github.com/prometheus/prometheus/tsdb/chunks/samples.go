b20c61249150d7318a9f945078bdd99701b9ff51
Koenraad Verheyden <koenraad.verheyden@grafana.com>
2023-10-11 15:15:49 +0200

    Update prometheus to latest release v2.47.1 (c4d1a8b) (#3001)
    * Update prometheus to latest release v2.47.1 (c4d1a8b)
    
    I ran
    ```
    go get github.com/prometheus/prometheus@c4d1a8b
    ```
    And fixed any compilation issues
    
    * Checkin .test files from prometheus
    
    * go mod tidy
    
    * More fixes
    
    * Final set of fixes? 🤞
    
    * Lint
    
    ---------
    
    Co-authored-by: Mario <mariorvinas@gmail.com>

--- vendor/github.com/prometheus/prometheus/tsdb/chunks/samples.go
+++ vendor/github.com/prometheus/prometheus/tsdb/chunks/samples.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Prometheus Authors
+// Copyright 2023 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,11 +11,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package tsdbutil
+package chunks
 
 import (
+	"github.com/prometheus/prometheus/model/histogram"
 	"github.com/prometheus/prometheus/tsdb/chunkenc"
-	"github.com/prometheus/prometheus/tsdb/chunks"
 )
 
 type Samples interface {
@@ -25,7 +25,10 @@ type Samples interface {
 
 type Sample interface {
 	T() int64
-	V() float64
+	F() float64
+	H() *histogram.Histogram
+	FH() *histogram.FloatHistogram
+	Type() chunkenc.ValueType
 }
 
 type SampleSlice []Sample
@@ -33,60 +36,54 @@ type SampleSlice []Sample
 func (s SampleSlice) Get(i int) Sample { return s[i] }
 func (s SampleSlice) Len() int         { return len(s) }
 
-func ChunkFromSamples(s []Sample) chunks.Meta {
-	return ChunkFromSamplesGeneric(SampleSlice(s))
-}
-
-func ChunkFromSamplesGeneric(s Samples) chunks.Meta {
-	mint, maxt := int64(0), int64(0)
-
-	if s.Len() > 0 {
-		mint, maxt = s.Get(0).T(), s.Get(s.Len()-1).T()
-	}
-
-	c := chunkenc.NewXORChunk()
-	ca, _ := c.Appender()
-
-	for i := 0; i < s.Len(); i++ {
-		ca.Append(s.Get(i).T(), s.Get(i).V())
-	}
-	return chunks.Meta{
-		MinTime: mint,
-		MaxTime: maxt,
-		Chunk:   c,
-	}
-}
-
 type sample struct {
-	t int64
-	v float64
+	t  int64
+	f  float64
+	h  *histogram.Histogram
+	fh *histogram.FloatHistogram
 }
 
 func (s sample) T() int64 {
 	return s.t
 }
 
-func (s sample) V() float64 {
-	return s.v
+func (s sample) F() float64 {
+	return s.f
 }
 
-// PopulatedChunk creates a chunk populated with samples every second starting at minTime
-func PopulatedChunk(numSamples int, minTime int64) chunks.Meta {
-	samples := make([]Sample, numSamples)
-	for i := 0; i < numSamples; i++ {
-		samples[i] = sample{minTime + int64(i*1000), 1.0}
+func (s sample) H() *histogram.Histogram {
+	return s.h
+}
+
+func (s sample) FH() *histogram.FloatHistogram {
+	return s.fh
+}
+
+func (s sample) Type() chunkenc.ValueType {
+	switch {
+	case s.h != nil:
+		return chunkenc.ValHistogram
+	case s.fh != nil:
+		return chunkenc.ValFloatHistogram
+	default:
+		return chunkenc.ValFloat
 	}
-	return ChunkFromSamples(samples)
 }
 
 // GenerateSamples starting at start and counting up numSamples.
 func GenerateSamples(start, numSamples int) []Sample {
+	return generateSamples(start, numSamples, func(i int) Sample {
+		return sample{
+			t: int64(i),
+			f: float64(i),
+		}
+	})
+}
+
+func generateSamples(start, numSamples int, gen func(int) Sample) []Sample {
 	samples := make([]Sample, 0, numSamples)
 	for i := start; i < start+numSamples; i++ {
-		samples = append(samples, sample{
-			t: int64(i),
-			v: float64(i),
-		})
+		samples = append(samples, gen(i))
 	}
 	return samples
 }
