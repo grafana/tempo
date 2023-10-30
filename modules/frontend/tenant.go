a0e4b53ca9ba610478571465737dbd152ea0ad33
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2023-10-31 00:27:23 +0530

    Add MultiTenantMiddleware and tests
    ---------
    
    Co-authored-by: Mario <mariorvinas@gmail.com>

---
+++ modules/frontend/tenant.go
@@ -0,0 +1,180 @@
+package frontend
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/grafana/dskit/tenant"
+	"github.com/grafana/dskit/user"
+	"github.com/grafana/tempo/modules/frontend/combiner"
+	"github.com/grafana/tempo/pkg/tempopb"
+	v1 "github.com/grafana/tempo/pkg/tempopb/common/v1"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+const (
+	statusCodeLabel = "status_code"
+	tenantLabel     = "tenant"
+)
+
+var (
+	tenantSuccessTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "tempo_tenant_federation",
+			Name:      "success_total",
+			Help:      "Total number of successful fetches of a trace per tenant.",
+		},
+		[]string{tenantLabel})
+
+	tenantFailureTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "tempo_tenant_federation",
+			Name:      "failures_total",
+			Help:      "Total number of failing fetches of a trace per tenant.",
+		},
+		[]string{tenantLabel, statusCodeLabel})
+)
+
+type tenantRoundTripper struct {
+	cfg    Config
+	next   http.RoundTripper
+	logger log.Logger
+
+	resolver tenant.Resolver
+
+	newCombiner func() combiner.Combiner
+
+	tenantSuccessTotal *prometheus.CounterVec
+	tenantFailureTotal *prometheus.CounterVec
+}
+
+// TODO: add a middleware to return error in case of multiple tenants in unsupported routes
+
+// newMultiTenantMiddleware returns a middleware that takes a request and fans it out to each tenant
+func newMultiTenantMiddleware(cfg Config, combinerFn func() combiner.Combiner, logger log.Logger) Middleware {
+	return MiddlewareFunc(func(next http.RoundTripper) http.RoundTripper {
+		return &tenantRoundTripper{
+			cfg:                cfg,
+			next:               next,
+			logger:             logger,
+			resolver:           tenant.NewMultiResolver(),
+			newCombiner:        combinerFn,
+			tenantSuccessTotal: tenantSuccessTotal,
+			tenantFailureTotal: tenantFailureTotal,
+		}
+	})
+}
+
+func (t *tenantRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	if !t.cfg.MultiTenantQueriesEnabled {
+		// move on to next tripper if multi-tenant queries are not enabled
+		return t.next.RoundTrip(req)
+	}
+
+	_, ctx, err := user.ExtractOrgIDFromHTTPRequest(req)
+	if err == user.ErrNoOrgID {
+		// no org id, move to next tripper
+		return t.next.RoundTrip(req)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to extract org id from request: %w", err)
+	}
+
+	// extract tenant ids
+	tenants, err := t.resolver.TenantIDs(ctx)
+	if err != nil {
+		return nil, err
+	}
+	// for single tenant, fall through to next round tripper
+	if len(tenants) <= 1 {
+		return t.next.RoundTrip(req)
+	}
+
+	_ = level.Debug(t.logger).Log("msg", "got multiple tenant ids...", "tenants", tenants)
+
+	var wg sync.WaitGroup
+	respCombiner := t.newCombiner()
+
+	// call RoundTrip for each tenant and combine results
+	// Send one request per tenant to down-stream tripper
+	// Return early if statusCode is already set by a previous response
+	for _, tenantID := range tenants {
+		wg.Add(1)
+		go func(tenant string) {
+			defer wg.Done()
+			// build a sub request context for each tenant because we want to modify and inject a tenant id into the context.
+			// this is done so that components downstream of frontend doesn't need to know anything about multi-tenant query
+			subCtx, cancel := context.WithCancel(req.Context())
+			defer cancel()
+
+			if respCombiner.ShouldQuit() {
+				return
+			}
+
+			_ = level.Info(t.logger).Log("msg", "sending request for tenant", "tenant", tenant)
+
+			r := requestForTenant(subCtx, req, tenant)
+			resp, err := t.next.RoundTrip(r)
+
+			if respCombiner.ShouldQuit() {
+				return
+			}
+
+			// Check http error
+			if err != nil {
+				_ = level.Error(t.logger).Log("msg", "error querying for tenant", "tenant", tenant, "err", err)
+				t.tenantFailureTotal.With(prometheus.Labels{tenantLabel: tenant, statusCodeLabel: strconv.Itoa(respCombiner.StatusCode())}).Inc()
+				return
+			}
+
+			// If we get here, we have a successful response
+			if err := respCombiner.AddRequest(resp, injectTenantResource(tenant)); err != nil {
+				_ = level.Error(t.logger).Log("msg", "error combining responses", "tenant", tenant, "err", err)
+				t.tenantFailureTotal.With(prometheus.Labels{tenantLabel: tenant, statusCodeLabel: strconv.Itoa(resp.StatusCode)}).Inc()
+				return
+			}
+
+			_ = level.Debug(t.logger).Log("msg", "success probing", "tenant", tenant)
+			t.tenantSuccessTotal.With(prometheus.Labels{tenantLabel: tenant}).Inc()
+		}(tenantID)
+	}
+	// TODO: will this work for search streaming, look into it. might need a search steaming combiner
+	wg.Wait()
+
+	return respCombiner.Complete()
+}
+
+// requestForTenant makes a copy of request and injects the tenant id into context and Header.
+// this allows us to keep all multi-tenant logic in query frontend and keep other components single tenant
+func requestForTenant(ctx context.Context, r *http.Request, tenant string) *http.Request {
+	ctx = user.InjectOrgID(ctx, tenant)
+	rCopy := r.Clone(ctx)
+	rCopy.Header.Set(user.OrgIDHeaderName, tenant)
+	return rCopy
+}
+
+// injectTenantResource will add tenantLabel attribute into response to show which tenant the response came from
+func injectTenantResource(tenant string) func(t *tempopb.Trace) {
+	return func(t *tempopb.Trace) {
+		if t == nil || t.Batches == nil {
+			return
+		}
+
+		for _, b := range t.Batches {
+			b.Resource.Attributes = append(b.Resource.Attributes, &v1.KeyValue{
+				Key: tenantLabel,
+				Value: &v1.AnyValue{
+					Value: &v1.AnyValue_StringValue{
+						StringValue: tenant,
+					},
+				},
+			})
+		}
+	}
+}
---
+++ modules/frontend/tenant_test.go
@@ -0,0 +1,129 @@
+package frontend
+
+import (
+	"bytes"
+	"crypto/rand"
+	"io"
+	"net/http"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/go-kit/log"
+	"github.com/grafana/dskit/user"
+	"github.com/grafana/tempo/modules/frontend/combiner"
+	"github.com/grafana/tempo/pkg/tempopb"
+	"github.com/grafana/tempo/pkg/util/test"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+)
+
+func TestMultiTenant(t *testing.T) {
+	tests := []struct {
+		name    string
+		tenants string
+	}{
+		{
+			name:    "single tenant",
+			tenants: "single-tenant",
+		},
+		{
+			name:    "multiple tenants",
+			tenants: "tenant-1|tenant-2|tenant-3",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := Config{
+				TraceByID: TraceByIDConfig{
+					QueryShards: minQueryShards,
+					SLO:         testSLOcfg,
+				},
+				Search: SearchConfig{
+					Sharder: SearchSharderConfig{
+						ConcurrentRequests:    defaultConcurrentRequests,
+						TargetBytesPerRequest: defaultTargetBytesPerRequest,
+					},
+					SLO: testSLOcfg,
+				},
+				MultiTenantQueriesEnabled: true,
+			}
+			tenantMiddleware := newMultiTenantMiddleware(cfg, combiner.NewTraceByID, log.NewNopLogger())
+
+			var reqCount atomic.Int32
+
+			tenantsMap := make(map[string]struct{}, len(tc.tenants))
+			tenants := strings.Split(tc.tenants, "|")
+			for _, tenant := range tenants {
+				tenantsMap[tenant] = struct{}{}
+			}
+
+			traceID := make([]byte, 16)
+			_, err := rand.Read(traceID)
+			require.NoError(t, err)
+			trace := test.MakeTrace(10, traceID)
+
+			once := sync.Once{}
+			var fastestTenant string
+			next := RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+				reqCount.Inc() // Count the number of requests.
+
+				// Check if the tenant is in the list of tenants.
+				tenantID, _, err := user.ExtractOrgIDFromHTTPRequest(req)
+				require.NoError(t, err)
+				_, ok := tenantsMap[tenantID]
+				require.True(t, ok)
+
+				// we do this in requestForTenant method, which is skipped for single tenant
+				if len(tenants) > 1 {
+					// ensure that tenant id in http header is same as org id in context
+					// some places are using http headers and some are using context to
+					// extract tenant id form the request so need both to be set and be correct.
+					orgID, err := user.ExtractOrgID(req.Context())
+					require.NoError(t, err)
+					require.Equal(t, tenantID, orgID)
+				}
+
+				statusCode := http.StatusNotFound
+				var body []byte
+				once.Do(func() {
+					fastestTenant = tenantID
+					statusCode = http.StatusOK
+					buff, err := trace.Marshal()
+					require.NoError(t, err)
+					body = buff
+				})
+
+				return &http.Response{
+					StatusCode: statusCode,
+					Body:       io.NopCloser(bytes.NewReader(body)),
+				}, nil
+			})
+
+			rt := NewRoundTripper(next, tenantMiddleware)
+
+			req, err := http.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+			require.NoError(t, err)
+			req.Header.Set(user.OrgIDHeaderName, tc.tenants)
+
+			res, err := rt.RoundTrip(req)
+			require.NoError(t, err)
+			require.Equal(t, len(tenants), int(reqCount.Load()))
+			require.NotNil(t, res)
+			require.Equal(t, http.StatusOK, res.StatusCode)
+
+			buff, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+			// Unmarshal response into a trace.
+			responseTrace := &tempopb.Trace{}
+			require.NoError(t, responseTrace.Unmarshal(buff))
+			// Add tenant to the original trace to compare.
+			if len(tenants) > 1 {
+				injectTenantResource(fastestTenant)(trace)
+			}
+			// Check if the trace is the same as the original.
+			require.Equal(t, trace, responseTrace)
+		})
+	}
+}
