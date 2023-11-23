f9100fb7b54f4203532d87fb9a1b1f6c1c5b2812
Mario <mariorvinas@gmail.com>
2023-11-23 11:04:39 +0100

    Prevent `LeftJoinIterator`s with no required conditions from being built (#3159)
    * Prevent LeftJoin iterators with no required conditions from being built
    
    * chlog
    
    * Change panic for err return

--- pkg/parquetquery/iters.go
+++ pkg/parquetquery/iters.go
@@ -1356,7 +1356,14 @@ type LeftJoinIterator struct {
 
 var _ Iterator = (*LeftJoinIterator)(nil)
 
-func NewLeftJoinIterator(definitionLevel int, required, optional []Iterator, pred GroupPredicate) *LeftJoinIterator {
+func NewLeftJoinIterator(definitionLevel int, required, optional []Iterator, pred GroupPredicate) (*LeftJoinIterator, error) {
+	// No query should ever result in a left-join with no required iterators.
+	// If this happens, it's a bug in the iter building code.
+	// LeftJoinIterator is not designed to handle this case and will loop forever.
+	if len(required) == 0 {
+		return nil, fmt.Errorf("left join iterator requires at least one required iterator")
+	}
+
 	j := LeftJoinIterator{
 		definitionLevel: definitionLevel,
 		required:        required,
@@ -1366,7 +1373,7 @@ func NewLeftJoinIterator(definitionLevel int, required, optional []Iterator, pre
 		peeksOptional:   make([]*IteratorResult, len(optional)),
 		pred:            pred,
 	}
-	return &j
+	return &j, nil
 }
 
 func (j *LeftJoinIterator) String() string {
