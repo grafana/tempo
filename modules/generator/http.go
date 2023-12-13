82d4c527c64d88c6c480bbd5c918bfed11b38022
Zach Leslie <zach.leslie@grafana.com>
2023-12-13 14:44:32 +0000

    [TraceQL Metrics] Generator

--- modules/generator/http.go
+++ modules/generator/http.go
@@ -6,9 +6,10 @@ import (
 	"time"
 
 	"github.com/gogo/protobuf/jsonpb"
+	"github.com/opentracing/opentracing-go"
+
 	"github.com/grafana/tempo/pkg/api"
 	"github.com/grafana/tempo/pkg/tempopb"
-	"github.com/opentracing/opentracing-go"
 )
 
 func (g *Generator) SpanMetricsHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +29,37 @@ func (g *Generator) SpanMetricsHandler(w http.ResponseWriter, r *http.Request) {
 
 	var resp *tempopb.SpanMetricsResponse
 	resp, err = g.GetMetrics(ctx, req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	marshaller := &jsonpb.Marshaler{}
+	err = marshaller.Marshal(w, resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set(api.HeaderContentType, api.HeaderAcceptJSON)
+}
+
+func (g *Generator) QueryRangeHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithDeadline(r.Context(), time.Now().Add(g.cfg.QueryTimeout))
+	defer cancel()
+
+	span, ctx := opentracing.StartSpanFromContext(ctx, "Generator.QueryRangeHandler")
+	defer span.Finish()
+
+	span.SetTag("requestURI", r.RequestURI)
+
+	req, err := api.ParseQueryRangeRequest(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var resp *tempopb.QueryRangeResponse
+	resp, err = g.QueryRange(ctx, req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
