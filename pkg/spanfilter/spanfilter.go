e85c194ca3724cd4e241a39ea8a6a6b03685dae0
Zach Leslie <zach.leslie@grafana.com>
2024-01-19 21:05:59 +0000

    Revert "Change spanmetrics filtering with multiple policies"

--- pkg/spanfilter/spanfilter.go
+++ pkg/spanfilter/spanfilter.go
@@ -49,59 +49,24 @@ func NewSpanFilter(filterPolicies []config.FilterPolicy) (*SpanFilter, error) {
 	}, nil
 }
 
-// ApplyFilterPolicy returns true if the span should be included in the
-// metrics.  Each filter policy is evaluated in order.
+// ApplyFilterPolicy returns true if the span should be included in the metrics.
 func (f *SpanFilter) ApplyFilterPolicy(rs *v1.Resource, span *tracev1.Span) bool {
 	// With no filter policies specified, all spans are included.
 	if len(f.filterPolicies) == 0 {
 		return true
 	}
 
-	var (
-		included bool // a policy matched and included the span
-		excluded bool // a policy matched and excluded the span
-		tries    int  // the number of attempts to exclude a span
-	)
-
 	for _, policy := range f.filterPolicies {
-		// if both matched, the span is excluded unless included by a later policy
-		// if neither matched, the span is included
-		// if the include policy matched, but the exclude policy did not, include the span
-		// if the exclude policy matched, but the include policy did not, exclude the span
-		// if only exclude policies are specified, and none match, include the span
-		// if only exclude policies are specified, and any match, exclude the span
-
-		if policy.Include != nil {
-			if policy.Include.Match(rs, span) {
-				included = true
-				if policy.Exclude != nil {
-					// continue to check additional policies for inclusion
-					if policy.Exclude.Match(rs, span) {
-						excluded = true
-						continue
-					}
-				}
-				return true
-			}
+		if policy.Include != nil && !policy.Include.Match(rs, span) {
+			return false
 		}
 
-		if policy.Exclude != nil {
-			tries++
-			if policy.Exclude.Match(rs, span) {
-				excluded = true
-			}
+		if policy.Exclude != nil && policy.Exclude.Match(rs, span) {
+			return false
 		}
 	}
 
-	// attempts were made to exclude the span
-	if tries > 0 {
-		// we didn't include nor exclude the span, include it
-		if !included && !excluded {
-			return true
-		}
-	}
-
-	return false
+	return true
 }
 
 func getSplitPolicy(policy *config.PolicyMatch) (*splitPolicy, error) {
