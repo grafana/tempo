a7c78f969058e3973e62508aa9f2e50f16c52d39
Mario <mariorvinas@gmail.com>
2024-04-02 18:15:49 +0200

    Add in-place version

--- pkg/feistel/cipher.go
+++ pkg/feistel/cipher.go
@@ -1,8 +1,6 @@
 package feistel
 
 import (
-	"encoding/hex"
-
 	"github.com/segmentio/fasthash/fnv1a"
 )
 
@@ -12,13 +10,15 @@ const (
 
 func Encrypt(data []byte) []byte {
 	if len(data)%2 != 0 {
-		// TODO: Add padding (?)
+		// TODO: Add padding
 		panic("data must be an even number of bytes")
 	}
 	half := len(data) / 2
 	left, right := data[:half], data[half:]
+	key := make([]byte, half)
 	for i := 0; i < rounds; i++ {
-		newRight := xor(left, roundKey(right))
+		roundKey(right, key)
+		newRight := xor(left, key)
 		left, right = right, newRight
 	}
 	return append(left, right...)
@@ -30,23 +30,50 @@ func Decrypt(data []byte) []byte {
 	}
 	half := len(data) / 2
 	left, right := data[:half], data[half:]
+	key := make([]byte, half)
 	for i := 0; i < rounds; i++ {
-		newLeft := xor(right, roundKey(left))
+		roundKey(left, key)
+		newLeft := xor(right, key)
 		left, right = newLeft, left
 	}
 	// TODO: Remove padding (?) if any
 	return append(left, right...)
 }
 
-func roundKey(data []byte) []byte {
+func InPlaceEncrypt(data []byte) {
+	if len(data)%2 != 0 {
+		panic("data must be an even number of bytes")
+	}
+	half := len(data) / 2
+	key := make([]byte, half)
+	for i := 0; i < rounds; i++ {
+		roundKey(data[half:], key)
+		for j := 0; j < half; j++ {
+			data[j], data[half+j] = data[half+j], data[j]^key[j]
+		}
+	}
+}
+
+func InPlaceDecrypt(data []byte) {
+	if len(data)%2 != 0 {
+		panic("data must be an even number of bytes")
+	}
+	half := len(data) / 2
+	key := make([]byte, half)
+	for i := 0; i < rounds; i++ {
+		roundKey(data[:half], key)
+		for j := 0; j < half; j++ {
+			data[j], data[half+j] = data[half+j]^key[j], data[j]
+		}
+	}
+}
+
+func roundKey(data []byte, hash []byte) {
 	h := fnv1a.HashBytes64(data)
 	// Convert the hash to a byte slice
-	hash := make([]byte, 8)
 	for i := 0; i < 8; i++ {
 		hash[i] = byte(h >> (8 * i))
 	}
-	hex.EncodeToString(hash)
-	return hash
 }
 
 // xor performs a bitwise XOR operation on two byte slices.
--- pkg/feistel/cipher_test.go
+++ pkg/feistel/cipher_test.go
@@ -2,10 +2,8 @@ package feistel
 
 import (
 	"crypto/rand"
-	"fmt"
 	"testing"
 
-	"github.com/grafana/tempo/pkg/util"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,18 +11,25 @@ func TestEncryptDecrypt(t *testing.T) {
 	traceID := make([]byte, 16)
 	_, err := rand.Read(traceID)
 	require.NoError(t, err)
-	fmt.Println("traceID:", util.TraceIDToHexString(traceID))
+
+	encrypted := Encrypt(traceID)
+	decrypted := Decrypt(encrypted)
+	require.Equal(t, traceID, decrypted)
+}
+
+func TestInPlaceEncryptDecrypt(t *testing.T) {
+	traceID := make([]byte, 16)
+	_, err := rand.Read(traceID)
+	require.NoError(t, err)
 
 	cpyTraceID := make([]byte, 16)
 	copy(cpyTraceID, traceID)
-	encrypted := Encrypt(cpyTraceID)
-	fmt.Println("encrypted:", util.TraceIDToHexString(encrypted))
+	InPlaceEncrypt(cpyTraceID)
 
 	cpyEncrypted := make([]byte, 16)
-	copy(cpyEncrypted, encrypted)
-	decrypted := Decrypt(cpyEncrypted)
-	fmt.Println("decrypted:", util.TraceIDToHexString(decrypted))
-	require.Equal(t, traceID, decrypted)
+	copy(cpyEncrypted, cpyTraceID)
+	InPlaceDecrypt(cpyEncrypted)
+	require.Equal(t, traceID, cpyEncrypted)
 }
 
 func BenchmarkEncryptDecrypt(b *testing.B) {
@@ -35,5 +40,17 @@ func BenchmarkEncryptDecrypt(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = Decrypt(Encrypt(traceID))
+	}
+}
+
+func BenchmarkInPlaceEncryptDecrypt(b *testing.B) {
+	traceID := make([]byte, 16)
+	_, err := rand.Read(traceID)
+	require.NoError(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		InPlaceEncrypt(traceID)
+		InPlaceDecrypt(traceID)
 	}
 }
