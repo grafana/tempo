3053b1fd254d2172abe92c4ca7343c01b871b3b7
Joe Elliott <joe.elliott@grafana.com>
2024-04-11 11:59:53 -0400

    Apply suggestions from code review
    Co-authored-by: Koenraad Verheyden <koenraad.verheyden@posteo.net>

--- modules/frontend/combiner/interface.go
+++ modules/frontend/combiner/interface.go
@@ -7,7 +7,7 @@ import (
 // Combiner is used to merge multiple responses into a single response.
 //
 // Implementations must be thread-safe.
-// TODO: StatusCode() and the tenant parameter on AddRequest are only used in for multi-tenant support. Can we remove them?
+// TODO: StatusCode() is only used for multi-tenant support. Can we remove it?
 type Combiner interface {
 	AddResponse(r *http.Response) error
 	StatusCode() int
