74b76969ac97d22975a9d59960b5cf6e6335eb22
Zach Leslie <zach.leslie@grafana.com>
2024-04-17 15:36:00 +0000

    Avoid /0

--- modules/frontend/search_sharder.go
+++ modules/frontend/search_sharder.go
@@ -237,8 +237,9 @@ func (s *asyncSearchSharder) ingesterRequests(ctx context.Context, tenantID stri
 	interval := duration / uint32(s.cfg.IngesterShards)
 
 	// If the interval is less than a minute, reduce the shards until the
-	// interval is above the 1 minute threshold.
-	for interval > 0 && interval < 60 {
+	// interval is above the 1 minute threshold.  This only applies when the
+	// shard count is greater than 1.
+	for interval > 0 && interval < 60 && shards > 1 {
 		shards--
 		interval = duration / uint32(shards)
 	}
