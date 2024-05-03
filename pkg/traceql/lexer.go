854cacaa422da5a9ff0b0584528cc04119309f22
Jennie Pham <94262131+ie-pham@users.noreply.github.com>
2024-05-02 19:25:22 -0500

    TraceQL - Add support for scoped intrinsics using `:` (#3629)
    * add scoped intrinsics support :
    
    * lint
    
    * test & changelog
    
    * more test & changelog & doc
    
    * rename
    
    * add comment

--- pkg/traceql/lexer.go
+++ pkg/traceql/lexer.go
@@ -65,6 +65,7 @@ var tokens = map[string]int{
 	"kind":               KIND,
 	"rootName":           ROOTNAME,
 	"rootServiceName":    ROOTSERVICENAME,
+	"rootService":        ROOTSERVICE,
 	"traceDuration":      TRACEDURATION,
 	"nestedSetLeft":      NESTEDSETLEFT,
 	"nestedSetRight":     NESTEDSETRIGHT,
@@ -73,6 +74,8 @@ var tokens = map[string]int{
 	"parent.":            PARENT_DOT,
 	"resource.":          RESOURCE_DOT,
 	"span.":              SPAN_DOT,
+	"trace:":             TRACE_COLON,
+	"span:":              SPAN_COLON,
 	"count":              COUNT,
 	"avg":                AVG,
 	"max":                MAX,
@@ -192,7 +195,7 @@ func (l *lexer) Lex(lval *yySymType) int {
 		break
 	}
 
-	if multiTok == PARENT_DOT || multiTok == SPAN_DOT || multiTok == RESOURCE_DOT {
+	if multiTok == PARENT_DOT || multiTok == SPAN_DOT || multiTok == RESOURCE_DOT || multiTok == SPAN_COLON || multiTok == TRACE_COLON {
 		l.currentScope = multiTok
 	}
 
