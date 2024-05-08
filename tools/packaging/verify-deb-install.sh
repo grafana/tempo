d9641c365e168a4181dcfc06f6d279e6377380f5
Martin Disibio <mdisibio@gmail.com>
2024-05-08 09:56:09 -0400

    Create and assign permissions of /var/tempo after creating user

--- tools/packaging/verify-deb-install.sh
+++ tools/packaging/verify-deb-install.sh
@@ -18,22 +18,8 @@ cat <<EOF | docker exec --interactive "${image}" sh
     # Install tempo and check it's running
     dpkg -i ${dir}/dist/tempo*_amd64.deb
     [ "\$(systemctl is-active tempo)" = "active" ] || (echo "tempo is inactive" && exit 1)
-
-    mkdir /var/tempo
-    chown tempo /var/tempo
-
-    systemctl restart tempo
-
-    systemctl is-active tempo
-    tail /var/log/dpkg.log
-    journalctl
     
     # Wait for tempo to be ready. The script is cat-ed because it is passed to docker exec
     apt update && apt install -y curl
-
-    curl -vvv http://localhost:3200/status
-    curl -vvv http://localhost:3200/ready
-
-
     $(cat ${dir}/tools/packaging/wait-for-ready.sh)
 EOF
