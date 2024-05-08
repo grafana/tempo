0862529c44ca635c62984e8649827cc648ca52ce
Martin Disibio <mdisibio@gmail.com>
2024-05-08 09:43:25 -0400

    create /var/tempo and assign perms

--- tools/packaging/verify-deb-install.sh
+++ tools/packaging/verify-deb-install.sh
@@ -19,16 +19,21 @@ cat <<EOF | docker exec --interactive "${image}" sh
     dpkg -i ${dir}/dist/tempo*_amd64.deb
     [ "\$(systemctl is-active tempo)" = "active" ] || (echo "tempo is inactive" && exit 1)
 
+    mkdir /var/tempo
+    chown tempo /var/tempo
+
+    systemctl restart tempo
+
     systemctl is-active tempo
     tail /var/log/dpkg.log
-    journalctl -b
     journalctl
-    journalctl -u tempo.service
+    
+    # Wait for tempo to be ready. The script is cat-ed because it is passed to docker exec
+    apt update && apt install -y curl
 
     curl -vvv http://localhost:3200/status
     curl -vvv http://localhost:3200/ready
 
-    # Wait for tempo to be ready. The script is cat-ed because it is passed to docker exec
-    apt update && apt install -y curl
+
     $(cat ${dir}/tools/packaging/wait-for-ready.sh)
 EOF
