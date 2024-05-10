5f9cfb839422f5f2bac4758a0bf4762c5811703a
Joe Elliott <number101010@gmail.com>
2024-05-10 11:21:05 -0400

    skip nulls
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/parquetquery/iters.go
+++ pkg/parquetquery/iters.go
@@ -912,7 +912,11 @@ func (c *SyncIterator) next() (RowNumber, *pq.Value, error) {
 			c.currBufN++
 			c.currPageN++
 
-			// jpe consider v.IsNull skip
+			// currently no predicates need a null value. this may change in the future
+			// but for now this is a very nice perf improvement
+			if v.IsNull() {
+				continue
+			}
 
 			if c.filter != nil && !c.filter.KeepValue(*v) {
 				continue
