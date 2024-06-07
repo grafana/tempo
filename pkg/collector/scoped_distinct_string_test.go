d5fa045c16d7057bc54c1aff0c7943379db90e5d
Joe Elliott <number101010@gmail.com>
2024-06-07 10:04:39 -0400

    distinct scope diff test
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/collector/scoped_distinct_string_test.go
+++ pkg/collector/scoped_distinct_string_test.go
@@ -68,12 +68,57 @@ func TestScopedDistinct(t *testing.T) {
 		require.Equal(t, tc.exceeded, c.Exceeded())
 
 		actual := c.Strings()
-		require.Equal(t, len(tc.expected), len(actual))
-
-		for k, v := range tc.expected {
-			require.Equal(t, v, actual[k])
-		}
+		assertMaps(t, tc.expected, actual)
 	}
 }
 
-// test diff
+func TestScopedDistinctDiff(t *testing.T) {
+	c := NewScopedDistinctString(0)
+
+	c.Collect("scope1", "val1")
+	expected := map[string][]string{
+		"scope1": {"val1"},
+	}
+	assertMaps(t, expected, c.Diff())
+
+	// no diff
+	c.Collect("scope1", "val1")
+	expected = map[string][]string{}
+	assertMaps(t, expected, c.Diff())
+	assertMaps(t, map[string][]string{}, c.Diff())
+
+	// new value
+	c.Collect("scope1", "val2")
+	expected = map[string][]string{
+		"scope1": {"val2"},
+	}
+	assertMaps(t, expected, c.Diff())
+	assertMaps(t, map[string][]string{}, c.Diff())
+
+	// new scope
+	c.Collect("scope2", "val1")
+	expected = map[string][]string{
+		"scope2": {"val1"},
+	}
+	assertMaps(t, expected, c.Diff())
+	assertMaps(t, map[string][]string{}, c.Diff())
+
+	// all
+	c.Collect("scope2", "val1")
+	c.Collect("scope2", "val2")
+	c.Collect("scope1", "val3")
+	expected = map[string][]string{
+		"scope1": {"val3"},
+		"scope2": {"val2"},
+	}
+	assertMaps(t, expected, c.Diff())
+	assertMaps(t, map[string][]string{}, c.Diff())
+}
+
+func assertMaps(t *testing.T, expected, actual map[string][]string) {
+	require.Equal(t, len(expected), len(actual))
+
+	for k, v := range expected {
+		require.Equal(t, v, actual[k])
+	}
+}
