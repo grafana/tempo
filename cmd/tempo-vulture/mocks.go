ac6aa5fe6b604763a1f17075f55725dc80c765e5
javiermolinar <javiermolinar@live.com>
2024-07-07 17:50:04 -0400

    linting

--- cmd/tempo-vulture/mocks.go
+++ cmd/tempo-vulture/mocks.go
@@ -12,8 +12,8 @@ import (
 )
 
 type MockReporter struct {
-	err            error
-	batches_emited []*thrift.Batch
+	err             error
+	batches_emitted []*thrift.Batch
 }
 
 func (r MockReporter) EmitZipkinBatch(_ context.Context, _ []*zipkincore.Span) error {
@@ -21,106 +21,87 @@ func (r MockReporter) EmitZipkinBatch(_ context.Context, _ []*zipkincore.Span) e
 }
 
 func (r *MockReporter) EmitBatch(_ context.Context, b *thrift.Batch) error {
-	r.batches_emited = append(r.batches_emited, b)
+	r.batches_emitted = append(r.batches_emitted, b)
 	return r.err
 }
 
-type MockHttpClient struct {
+type MockHTTPClient struct {
 	err  error
 	resp http.Response
 }
 
-// DeleteOverrides implements httpclient.IClient.
-func (m MockHttpClient) DeleteOverrides(version string) error {
+func (m MockHTTPClient) DeleteOverrides(version string) error {
 	panic("unimplemented")
 }
 
-// Do implements httpclient.IClient.
-func (m MockHttpClient) Do(req *http.Request) (*http.Response, error) {
+func (m MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	return &m.resp, m.err
 }
 
-// GetOverrides implements httpclient.IClient.
-func (m MockHttpClient) GetOverrides() (*userconfigurableoverrides.Limits, string, error) {
+func (m MockHTTPClient) GetOverrides() (*userconfigurableoverrides.Limits, string, error) {
 	panic("unimplemented")
 }
 
-// MetricsSummary implements httpclient.IClient.
-func (m MockHttpClient) MetricsSummary(query string, groupBy string, start int64, end int64) (*tempopb.SpanMetricsSummaryResponse, error) {
+func (m MockHTTPClient) MetricsSummary(query string, groupBy string, start int64, end int64) (*tempopb.SpanMetricsSummaryResponse, error) {
 	panic("unimplemented")
 }
 
-// PatchOverrides implements httpclient.IClient.
-func (m MockHttpClient) PatchOverrides(limits *userconfigurableoverrides.Limits) (*userconfigurableoverrides.Limits, string, error) {
+func (m MockHTTPClient) PatchOverrides(limits *userconfigurableoverrides.Limits) (*userconfigurableoverrides.Limits, string, error) {
 	panic("unimplemented")
 }
 
-// QueryTrace implements httpclient.IClient.
-func (m MockHttpClient) QueryTrace(id string) (*tempopb.Trace, error) {
+func (m MockHTTPClient) QueryTrace(id string) (*tempopb.Trace, error) {
 	panic("unimplemented")
 }
 
-// Search implements httpclient.IClient.
-func (m MockHttpClient) Search(tags string) (*tempopb.SearchResponse, error) {
+func (m MockHTTPClient) Search(tags string) (*tempopb.SearchResponse, error) {
 	panic("unimplemented")
 }
 
-// SearchTagValues implements httpclient.IClient.
-func (m MockHttpClient) SearchTagValues(key string) (*tempopb.SearchTagValuesResponse, error) {
+func (m MockHTTPClient) SearchTagValues(key string) (*tempopb.SearchTagValuesResponse, error) {
 	panic("unimplemented")
 }
 
-// SearchTagValuesV2 implements httpclient.IClient.
-func (m MockHttpClient) SearchTagValuesV2(key string, query string) (*tempopb.SearchTagValuesV2Response, error) {
+func (m MockHTTPClient) SearchTagValuesV2(key string, query string) (*tempopb.SearchTagValuesV2Response, error) {
 	panic("unimplemented")
 }
 
-// SearchTagValuesV2WithRange implements httpclient.IClient.
-func (m MockHttpClient) SearchTagValuesV2WithRange(tag string, start int64, end int64) (*tempopb.SearchTagValuesV2Response, error) {
+func (m MockHTTPClient) SearchTagValuesV2WithRange(tag string, start int64, end int64) (*tempopb.SearchTagValuesV2Response, error) {
 	panic("unimplemented")
 }
 
-// SearchTags implements httpclient.IClient.
-func (m MockHttpClient) SearchTags() (*tempopb.SearchTagsResponse, error) {
+func (m MockHTTPClient) SearchTags() (*tempopb.SearchTagsResponse, error) {
 	panic("unimplemented")
 }
 
-// SearchTagsV2 implements httpclient.IClient.
-func (m MockHttpClient) SearchTagsV2() (*tempopb.SearchTagsV2Response, error) {
+func (m MockHTTPClient) SearchTagsV2() (*tempopb.SearchTagsV2Response, error) {
 	panic("unimplemented")
 }
 
-// SearchTagsV2WithRange implements httpclient.IClient.
-func (m MockHttpClient) SearchTagsV2WithRange(start int64, end int64) (*tempopb.SearchTagsV2Response, error) {
+func (m MockHTTPClient) SearchTagsV2WithRange(start int64, end int64) (*tempopb.SearchTagsV2Response, error) {
 	panic("unimplemented")
 }
 
-// SearchTagsWithRange implements httpclient.IClient.
-func (m MockHttpClient) SearchTagsWithRange(start int64, end int64) (*tempopb.SearchTagsResponse, error) {
+func (m MockHTTPClient) SearchTagsWithRange(start int64, end int64) (*tempopb.SearchTagsResponse, error) {
 	panic("unimplemented")
 }
 
-// SearchTraceQL implements httpclient.IClient.
-func (m MockHttpClient) SearchTraceQL(query string) (*tempopb.SearchResponse, error) {
+func (m MockHTTPClient) SearchTraceQL(query string) (*tempopb.SearchResponse, error) {
 	panic("unimplemented")
 }
 
-// SearchTraceQLWithRange implements httpclient.IClient.
-func (m MockHttpClient) SearchTraceQLWithRange(query string, start int64, end int64) (*tempopb.SearchResponse, error) {
+func (m MockHTTPClient) SearchTraceQLWithRange(query string, start int64, end int64) (*tempopb.SearchResponse, error) {
 	panic("unimplemented")
 }
 
-// SearchWithRange implements httpclient.IClient.
-func (m MockHttpClient) SearchWithRange(tags string, start int64, end int64) (*tempopb.SearchResponse, error) {
+func (m MockHTTPClient) SearchWithRange(tags string, start int64, end int64) (*tempopb.SearchResponse, error) {
 	panic("unimplemented")
 }
 
-// SetOverrides implements httpclient.IClient.
-func (m MockHttpClient) SetOverrides(limits *userconfigurableoverrides.Limits, version string) (string, error) {
+func (m MockHTTPClient) SetOverrides(limits *userconfigurableoverrides.Limits, version string) (string, error) {
 	panic("unimplemented")
 }
 
-// WithTransport implements httpclient.IClient.
-func (m MockHttpClient) WithTransport(t http.RoundTripper) {
+func (m MockHTTPClient) WithTransport(t http.RoundTripper) {
 	panic("unimplemented")
 }
