78f14700d21f9a034c0234608620176164fe2644
Zach Leslie <zach.leslie@grafana.com>
2024-08-06 17:29:08 +0000

    Copy test structure into benchmarks

--- tempodb/backend/tenantindex_test.go
+++ tempodb/backend/tenantindex_test.go
@@ -87,3 +87,65 @@ func TestIndexUnmarshalErrors(t *testing.T) {
 	err := test.unmarshal([]byte("bad data"))
 	assert.Error(t, err)
 }
+
+func BenchmarkIndexMarshal(b *testing.B) {
+	idx := &TenantIndex{
+		Meta: []*BlockMeta{
+			NewBlockMeta("test", uuid.New(), "v1", EncGZIP, "adsf"),
+			NewBlockMeta("test", uuid.New(), "v2", EncNone, "adsf"),
+			NewBlockMeta("test", uuid.New(), "v3", EncLZ4_4M, "adsf"),
+		},
+		CompactedMeta: []*CompactedBlockMeta{
+			{
+				BlockMeta:     *NewBlockMeta("test", uuid.New(), "v1", EncGZIP, "adsf"),
+				CompactedTime: time.Now(),
+			},
+			{
+				BlockMeta:     *NewBlockMeta("test", uuid.New(), "v1", EncZstd, "adsf"),
+				CompactedTime: time.Now(),
+			},
+			{
+				BlockMeta:     *NewBlockMeta("test", uuid.New(), "v1", EncSnappy, "adsf"),
+				CompactedTime: time.Now(),
+			},
+		},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = idx.marshal()
+	}
+}
+
+func BenchmarkIndexUnmarshal(b *testing.B) {
+	idx := &TenantIndex{
+		Meta: []*BlockMeta{
+			NewBlockMeta("test", uuid.New(), "v1", EncGZIP, "adsf"),
+			NewBlockMeta("test", uuid.New(), "v2", EncNone, "adsf"),
+			NewBlockMeta("test", uuid.New(), "v3", EncLZ4_4M, "adsf"),
+		},
+		CompactedMeta: []*CompactedBlockMeta{
+			{
+				BlockMeta:     *NewBlockMeta("test", uuid.New(), "v1", EncGZIP, "adsf"),
+				CompactedTime: time.Now(),
+			},
+			{
+				BlockMeta:     *NewBlockMeta("test", uuid.New(), "v1", EncZstd, "adsf"),
+				CompactedTime: time.Now(),
+			},
+			{
+				BlockMeta:     *NewBlockMeta("test", uuid.New(), "v1", EncSnappy, "adsf"),
+				CompactedTime: time.Now(),
+			},
+		},
+	}
+
+	buf, err := idx.marshal()
+	require.NoError(b, err)
+
+	unIdx := &TenantIndex{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = unIdx.unmarshal(buf)
+	}
+}
