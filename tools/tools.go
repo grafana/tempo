e23bdd24960dac581fe02154f3e319d84668b22a
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2024-08-20 01:16:53 +0530

    tools: update go and add goyacc and drone-cli

--- tools/tools.go
+++ tools/tools.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb"
 	_ "github.com/psampaz/go-mod-outdated"
 	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/goyacc"
 	_ "gotest.tools/gotestsum"
 	_ "mvdan.cc/gofumpt"
 )
