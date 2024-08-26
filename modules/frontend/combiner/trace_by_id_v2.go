597f90f4d7abde336813028edf375cbfb9020642
javiermolinar <javiermolinar@live.com>
2024-08-26 17:03:38 +0200

    flag when querier returns a partial trace

--- modules/frontend/combiner/trace_by_id_v2.go
+++ modules/frontend/combiner/trace_by_id_v2.go
@@ -9,15 +9,13 @@ import (
 
 func NewTraceByIDV2(maxBytes int, marshalingFormat string) Combiner {
 	combiner := trace.NewCombiner(maxBytes, true)
+	var partialTrace bool
 	gc := &genericCombiner[*tempopb.TraceByIDResponse]{
 		combine: func(partial *tempopb.TraceByIDResponse, _ *tempopb.TraceByIDResponse, _ PipelineResponse) error {
-			var err error
 			if partial.Status == tempopb.TraceByIDResponse_PARTIAL {
-				_, err = combiner.ConsumePartialTrace(partial.Trace)
-			} else {
-				_, err = combiner.Consume(partial.Trace)
+				partialTrace = true
 			}
-
+			_, err := combiner.Consume(partial.Trace)
 			return err
 		},
 		finalize: func(resp *tempopb.TraceByIDResponse) (*tempopb.TraceByIDResponse, error) {
@@ -31,7 +29,7 @@ func NewTraceByIDV2(maxBytes int, marshalingFormat string) Combiner {
 			traceResult = deduper.dedupe(traceResult)
 			resp.Trace = traceResult
 
-			if combiner.IsPartialTrace() {
+			if partialTrace || combiner.IsPartialTrace() {
 				resp.Status = tempopb.TraceByIDResponse_PARTIAL
 				resp.Message = fmt.Sprintf("Trace exceeds maximum size of %d bytes, a partial trace is returned", maxBytes)
 			}
