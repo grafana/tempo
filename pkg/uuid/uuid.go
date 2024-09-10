b2b7ad59b4267fd9672ce66a75a9078675e5bd73
Zach Leslie <zach.leslie@grafana.com>
2024-09-10 21:45:54 +0000

    Implement json for uuid

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -1,6 +1,8 @@
 package uuid
 
 import (
+	"encoding/json"
+
 	google_uuid "github.com/google/uuid"
 )
 
@@ -8,12 +10,12 @@ type UUID struct {
 	google_uuid.UUID
 }
 
-func (u *UUID) Size() int {
-	return 16
+func (u UUID) Marshal() ([]byte, error) {
+	return u.MarshalBinary()
 }
 
 func (u *UUID) MarshalTo(data []byte) (n int, err error) {
-	b, err := u.UUID.MarshalBinary()
+	b, err := u.MarshalBinary()
 	if err != nil {
 		return 0, err
 	}
@@ -22,10 +24,30 @@ func (u *UUID) MarshalTo(data []byte) (n int, err error) {
 }
 
 func (u *UUID) Unmarshal(data []byte) error {
-	err := u.UUID.UnmarshalBinary(data)
+	err := u.UnmarshalBinary(data)
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func (u *UUID) Size() int {
+	return 16
+}
+
+func (u UUID) MarshalJSON() ([]byte, error) {
+	return json.Marshal(u.UUID.String())
+}
+
+func (u *UUID) UnmarshalJSON(data []byte) error {
+	uu := google_uuid.UUID{}
+	err := json.Unmarshal(data, &uu)
+	if err != nil {
+		return err
+	}
+
+	u.UUID = uu
 
 	return nil
 }
