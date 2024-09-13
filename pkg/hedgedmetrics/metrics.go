85950ca136b5afe79e02843738c4f833c390212d
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2024-09-14 00:48:20 +0530

    fix: correctly publish the hedgedmetrics counter with delta (#4078)
    * fix: correctly publish the hedgedmetrics counter with delta
    
    * fix and update CHANGELOG.md
    
    * fix datarace in the test
    
    * replace locking with atomic
    
    * remove locking and unnecessary code
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * i don't care
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    ---------
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    Co-authored-by: Joe Elliott <number101010@gmail.com>

--- pkg/hedgedmetrics/metrics.go
+++ pkg/hedgedmetrics/metrics.go
@@ -11,17 +11,40 @@ const (
 	hedgedMetricsPublishDuration = 10 * time.Second
 )
 
-// PublishHedgedMetrics flushes metrics from hedged requests every 10 seconds
+type diffCounter struct {
+	previous uint64
+	counter  prometheus.Counter
+}
+
+func (d *diffCounter) addAbsoluteToCounter(value uint64) {
+	diff := float64(value - d.previous)
+	if value < d.previous {
+		diff = float64(value)
+	}
+	d.counter.Add(diff)
+	d.previous = value
+}
+
+// statsProvider defines the interface that wraps hedgedhttp.Stats for ease of testing
+type statsProvider interface {
+	Snapshot() hedgedhttp.StatsSnapshot
+}
+
+// Publish flushes metrics from hedged requests every tickerDur
 func Publish(s *hedgedhttp.Stats, counter prometheus.Counter) {
-	ticker := time.NewTicker(hedgedMetricsPublishDuration)
+	publishWithDuration(s, counter, hedgedMetricsPublishDuration)
+}
+
+func publishWithDuration(s statsProvider, counter prometheus.Counter, duration time.Duration) {
+	ticker := time.NewTicker(duration)
+	diff := &diffCounter{previous: 0, counter: counter}
+
 	go func() {
 		for range ticker.C {
 			snap := s.Snapshot()
-			hedgedRequests := int64(snap.ActualRoundTrips) - int64(snap.RequestedRoundTrips)
-			if hedgedRequests < 0 {
-				hedgedRequests = 0
-			}
-			counter.Add(float64(hedgedRequests))
+
+			hedgedRequests := snap.ActualRoundTrips - snap.RequestedRoundTrips
+			diff.addAbsoluteToCounter(hedgedRequests)
 		}
 	}()
 }
