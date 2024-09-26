fad3f1cc61e8738c3612c6447ac050d41907735e
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2024-09-27 01:12:41 +0530

    default to using v2 endpoint and mark it as breaking change

--- cmd/tempo-cli/cmd-query-trace-id.go
+++ cmd/tempo-cli/cmd-query-trace-id.go
@@ -11,7 +11,7 @@ type queryTraceIDCmd struct {
 	APIEndpoint string `arg:"" help:"tempo api endpoint"`
 	TraceID     string `arg:"" help:"trace ID to retrieve"`
 
-	V2    bool   `name:"v2" help:"use v2 API"`
+	V1    bool   `name:"v1" help:"Use v1 API /api/traces endpoint"`
 	OrgID string `help:"optional orgID"`
 }
 
@@ -19,25 +19,26 @@ func (cmd *queryTraceIDCmd) Run(_ *globalOptions) error {
 	client := httpclient.New(cmd.APIEndpoint, cmd.OrgID)
 	// util.QueryTrace will only add orgID header if len(orgID) > 0
 
-	// use v2 API if specified
-	if cmd.V2 {
-		traceResp, err := client.QueryTraceV2(cmd.TraceID)
+	// use v1 API if specified, we default to v2
+	if cmd.V1 {
+		trace, err := client.QueryTrace(cmd.TraceID)
 		if err != nil {
 			return err
 		}
-		// log the Message and trace field
-		if traceResp.Message != "" {
-			// print message and status to stderr if there is one.
-			// allows users to get a clean trace on the stdout, and pipe it to a file or another commands.
-			_, _ = fmt.Fprintf(os.Stderr, "status: %s , message: %s\n", traceResp.Status, traceResp.Message)
-		}
-		// only print the trace field
-		return printAsJSON(traceResp.Trace)
+		return printAsJSON(trace)
 	}
 
-	trace, err := client.QueryTrace(cmd.TraceID)
+	traceResp, err := client.QueryTraceV2(cmd.TraceID)
 	if err != nil {
 		return err
 	}
-	return printAsJSON(trace)
+	// log the Message and trace field
+	if traceResp.Message != "" {
+		// print message and status to stderr if there is one.
+		// allows users to get a clean trace on the stdout, and pipe it to a file or another commands.
+		_, _ = fmt.Fprintf(os.Stderr, "status: %s , message: %s\n", traceResp.Status, traceResp.Message)
+	}
+	// only print the trace field
+	return printAsJSON(traceResp.Trace)
+
 }
