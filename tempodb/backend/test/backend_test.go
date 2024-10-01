cc30cbd8224dc9dba70885df67d9f3041acabbb8
Zach Leslie <zach.leslie@grafana.com>
2024-10-01 15:11:51 +0000

    Import original tenantindex and test contents for backwards compatibility

--- tempodb/backend/test/backend_test.go
+++ tempodb/backend/test/backend_test.go
@@ -49,10 +49,6 @@ func TestFixtures(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, listCompactedMetas, 1)
 
-	for _, v := range listMetas {
-		t.Logf("listMetas: %v", v)
-	}
-
 	blockMetas := make([]*backend.BlockMeta, 0, len(listMetas))
 	for _, u := range listMetas {
 		m, e := r.BlockMeta(ctx, u, tenant)
@@ -86,6 +82,74 @@ func TestFixtures(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, blockMetas, i.Meta)
 	require.Len(t, i.Meta, len(listMetas))
+}
+
+func TestOriginalFixtures(t *testing.T) {
+	var (
+		tenant = "3" // A sample index
+		ctx    = context.Background()
+	)
+
+	rr, rw, _, err := local.New(&local.Config{
+		Path: "./test-data",
+	})
+	require.NoError(t, err)
+
+	var (
+		_ = backend.NewWriter(rw)
+		r = backend.NewReader(rr)
+	)
+
+	expectedDedicatedColumns := backend.DedicatedColumns{
+		backend.DedicatedColumn{Scope: "span", Name: "db.statement", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "component", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "http.user_agent", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "otel.library.name", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "db.connection_string", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "organization", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "peer.address", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "net.peer.name", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "blockID", Type: "string"},
+		backend.DedicatedColumn{Scope: "span", Name: "db.name", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "host.name", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "opencensus.exporterversion", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "client-uuid", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "ip", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "database", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "os.description", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "process.runtime.description", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "container.id", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "slug", Type: "string"},
+		backend.DedicatedColumn{Scope: "resource", Name: "module.path", Type: "string"},
+	}
+
+	i, err := r.TenantIndex(ctx, tenant)
+	assert.NoError(t, err)
+	assert.NotNil(t, i)
+
+	assert.Equal(t, 22435, len(i.Meta))
+	assert.Equal(t, 3264, len(i.CompactedMeta))
+
+	nonZeroMeta(t, i.Meta)
+	for _, v := range i.Meta {
+		assert.Equal(t, tenant, v.TenantID)
+		assert.Equal(t, "vParquet4", v.Version)
+		assert.NotZero(t, v.StartTime)
+		assert.NotZero(t, v.EndTime)
+		assert.Equal(t, 20, len(v.DedicatedColumns))
+		assert.Equal(t, expectedDedicatedColumns, v.DedicatedColumns)
+	}
+
+	nonZeroCompactedMeta(t, i.CompactedMeta)
+	for _, v := range i.CompactedMeta {
+		assert.Equal(t, "vParquet4", v.Version)
+		assert.Equal(t, tenant, v.TenantID)
+		assert.NotZero(t, v.CompactedTime)
+		assert.NotZero(t, v.StartTime)
+		assert.NotZero(t, v.EndTime)
+		assert.Equal(t, 20, len(v.DedicatedColumns))
+		assert.Equal(t, expectedDedicatedColumns, v.DedicatedColumns)
+	}
 }
 
 func nonZeroMeta(t *testing.T, m []*backend.BlockMeta) {
