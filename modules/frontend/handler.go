0ba43c16318f13117737e1a5ac6955814a048a13
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2024-11-22 20:42:15 +0530

    cleanup and fix bad log line

--- modules/frontend/handler.go
+++ modules/frontend/handler.go
@@ -160,14 +160,9 @@ func writeError(w http.ResponseWriter, err error) error {
 		err = errCanceled
 	} else if errors.Is(err, context.DeadlineExceeded) {
 		err = errDeadlineExceeded
-	} else if isRequestBodyTooLarge(err) {
+	} else if util.IsRequestBodyTooLarge(err) {
 		err = errRequestEntityTooLarge
 	}
 	httpgrpc.WriteError(w, err)
 	return err
-}
-
-// isRequestBodyTooLarge returns true if the error is "http: request body too large".
-func isRequestBodyTooLarge(err error) bool {
-	return err != nil && strings.Contains(err.Error(), "http: request body too large")
 }
