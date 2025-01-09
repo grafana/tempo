e3c2848514a35bf86cb85107c078de6ae08d0464
Carles Garcia <carles.garciacabot@grafana.com>
2025-01-09 13:12:36 +0100

    Migrate release pipeline from Drone to GHA (#4503)

--- tools/packaging/verify-rpm-install.sh
+++ tools/packaging/verify-rpm-install.sh
@@ -1,27 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -euxo pipefail
 
-docker ps
-image="$(docker ps --filter ancestor=jrei/systemd-centos:8 --latest --format "{{.ID}}")"
-echo "Running on container: ${image}"
+# Import the Grafana GPG key
+rpm --import https://packages.grafana.com/gpg.key
 
-dir="."
-if [ -n "${CI}" ]; then
-    dir="/drone/src"
-fi
-echo "Running on directory: ${dir}"
+# Install tempo and check it's running
+rpm -i ./tempo_*_linux_amd64.rpm
+[ "$(systemctl is-active tempo)" = "active" ] || (echo "tempo is inactive" && exit 1)
 
-cat <<EOF | docker exec --interactive "${image}" sh
-    set -x
-
-    # Import the Grafana GPG key
-    rpm --import https://packages.grafana.com/gpg.key
-
-    # Install tempo and check it's running
-    rpm -i ${dir}/dist/tempo*_amd64.rpm
-    [ "\$(systemctl is-active tempo)" = "active" ] || (echo "tempo is inactive" && exit 1)
-
-    # Wait for tempo to be ready. The script is cat-ed because it is passed to docker exec
-    $(cat ${dir}/tools/packaging/wait-for-ready.sh)
-EOF
+# Wait for tempo to be ready.
+./wait-for-ready.sh
