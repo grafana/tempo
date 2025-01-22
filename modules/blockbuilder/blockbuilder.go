e680d6ee8d88e2fdb3e49b647eb9759dbb5b1963
Martin Disibio <martin.disibio@grafana.com>
2025-01-22 03:13:49 -0500

    [Rhythm] Move group partition lag metric to ingest package, export from generators too (#4571)
    * Move group partition lag metric to ingest package, export from generators too
    
    * Cleanup
    
    * changelog
    
    * Remove unnecessary go
    
    Co-authored-by: Mario <mariorvinas@gmail.com>
    
    ---------
    
    Co-authored-by: Mario <mariorvinas@gmail.com>

--- modules/blockbuilder/blockbuilder.go
+++ modules/blockbuilder/blockbuilder.go
@@ -20,7 +20,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/twmb/franz-go/pkg/kadm"
-	"github.com/twmb/franz-go/pkg/kerr"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
@@ -50,12 +49,6 @@ var (
 		Subsystem: "block_builder",
 		Name:      "fetch_records_total",
 		Help:      "Total number of records fetched from Kafka",
-	}, []string{"partition"})
-	metricPartitionLag = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "tempo",
-		Subsystem: "block_builder",
-		Name:      "partition_lag",
-		Help:      "Lag of a partition.",
 	}, []string{"partition"})
 	metricPartitionLagSeconds = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "tempo",
@@ -167,7 +160,12 @@ func (b *BlockBuilder) starting(ctx context.Context) (err error) {
 
 	b.kadm = kadm.NewClient(b.kafkaClient)
 
-	go b.metricLag(ctx)
+	ingest.ExportPartitionLagMetrics(
+		ctx,
+		b.kadm,
+		b.logger,
+		b.cfg.IngestStorageConfig,
+		b.getAssignedActivePartitions)
 
 	return nil
 }
@@ -387,33 +385,6 @@ outer:
 	return more, nil
 }
 
-func (b *BlockBuilder) metricLag(ctx context.Context) {
-	var (
-		waitTime = time.Second * 15
-		topic    = b.cfg.IngestStorageConfig.Kafka.Topic
-		group    = b.cfg.IngestStorageConfig.Kafka.ConsumerGroup
-	)
-
-	for {
-		select {
-		case <-time.After(waitTime):
-			lag, err := getGroupLag(ctx, b.kadm, topic, group)
-			if err != nil {
-				level.Error(b.logger).Log("msg", "metric lag failed:", "err", err)
-				continue
-			}
-			for _, p := range b.getAssignedActivePartitions() {
-				l, ok := lag.Lookup(topic, p)
-				if ok {
-					metricPartitionLag.WithLabelValues(strconv.Itoa(int(p))).Set(float64(l.Lag))
-				}
-			}
-		case <-ctx.Done():
-			return
-		}
-	}
-}
-
 func (b *BlockBuilder) stopping(err error) error {
 	if b.kafkaClient != nil {
 		b.kafkaClient.Close()
@@ -441,40 +412,4 @@ func (b *BlockBuilder) getAssignedActivePartitions() []int32 {
 		assignedActivePartitions = append(assignedActivePartitions, partition)
 	}
 	return assignedActivePartitions
-}
-
-// getGroupLag is similar to `kadm.Client.Lag` but works when the group doesn't have live participants.
-// Similar to `kadm.CalculateGroupLagWithStartOffsets`, it takes into account that the group may not have any commits.
-//
-// The lag is the difference between the last produced offset (high watermark) and an offset in the "past".
-// If the block builder committed an offset for a given partition to the consumer group at least once, then
-// the lag is the difference between the last produced offset and the offset committed in the consumer group.
-// Otherwise, if the block builder didn't commit an offset for a given partition yet (e.g. block builder is
-// running for the first time), then the lag is the difference between the last produced offset and fallbackOffsetMillis.
-func getGroupLag(ctx context.Context, admClient *kadm.Client, topic, group string) (kadm.GroupLag, error) {
-	offsets, err := admClient.FetchOffsets(ctx, group)
-	if err != nil {
-		if !errors.Is(err, kerr.GroupIDNotFound) {
-			return nil, fmt.Errorf("fetch offsets: %w", err)
-		}
-	}
-	if err := offsets.Error(); err != nil {
-		return nil, fmt.Errorf("fetch offsets got error in response: %w", err)
-	}
-
-	startOffsets, err := admClient.ListStartOffsets(ctx, topic)
-	if err != nil {
-		return nil, err
-	}
-	endOffsets, err := admClient.ListEndOffsets(ctx, topic)
-	if err != nil {
-		return nil, err
-	}
-
-	descrGroup := kadm.DescribedGroup{
-		// "Empty" is the state that indicates that the group doesn't have active consumer members; this is always the case for block-builder,
-		// because we don't use group consumption.
-		State: "Empty",
-	}
-	return kadm.CalculateGroupLagWithStartOffsets(descrGroup, offsets, startOffsets, endOffsets), nil
 }
