c6ed10e5cdbb4c09a9459821a184c7aa3de122b9
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2025-01-23 16:08:29 +0530

    remove serverless code from the querier

--- modules/overrides/config_legacy.go
+++ modules/overrides/config_legacy.go
@@ -130,7 +130,7 @@ type LegacyOverrides struct {
 	MaxMetricsDuration model.Duration `yaml:"max_metrics_duration" json:"max_metrics_duration"`
 	UnsafeQueryHints   bool           `yaml:"unsafe_query_hints" json:"unsafe_query_hints"`
 
-	// MaxBytesPerTrace is enforced in the Ingester, Compactor, Querier (Search) and Serverless (Search). It
+	// MaxBytesPerTrace is enforced in the Ingester, Compactor, Querier (Search). It
 	//  is not used when doing a trace by id lookup.
 	MaxBytesPerTrace int `yaml:"max_bytes_per_trace" json:"max_bytes_per_trace"`
 
