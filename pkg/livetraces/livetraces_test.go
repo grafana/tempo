572c22d4ece6215a8d9e6034c59bc85ed956d260
Martin Disibio <mdisibio@gmail.com>
2025-02-06 12:53:51 -0500

    Add benchmark for live traces

--- pkg/livetraces/livetraces_test.go
+++ pkg/livetraces/livetraces_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/grafana/tempo/pkg/tempopb"
 	v1 "github.com/grafana/tempo/pkg/tempopb/trace/v1"
 	"github.com/grafana/tempo/pkg/util/test"
 	"github.com/stretchr/testify/require"
@@ -43,5 +44,38 @@ func TestLiveTracesSizesAndLen(t *testing.T) {
 
 		require.Equal(t, expectedSz, lt.Size())
 		require.Equal(t, expectedLen, lt.Len())
+	}
+}
+
+func BenchmarkLiveTracesWrite(b *testing.B) {
+	lt := New[*v1.ResourceSpans](func(rs *v1.ResourceSpans) uint64 { return uint64(rs.Size()) })
+
+	var traces []*tempopb.Trace
+	for i := 0; i < 100_000; i++ {
+		traces = append(traces, test.MakeTrace(1, nil))
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, tr := range traces {
+			lt.Push(tr.ResourceSpans[0].ScopeSpans[0].Spans[0].TraceId, tr.ResourceSpans[0], 0)
+		}
+	}
+}
+
+func BenchmarkLiveTracesRead(b *testing.B) {
+	lt := New[*v1.ResourceSpans](func(rs *v1.ResourceSpans) uint64 { return uint64(rs.Size()) })
+
+	for i := 0; i < 100_000; i++ {
+		tr := test.MakeTrace(1, nil)
+		lt.Push(tr.ResourceSpans[0].ScopeSpans[0].Spans[0].TraceId, tr.ResourceSpans[0], 0)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		// This won't anything, instead of will benchmark the map iteration performance.
+		lt.CutIdle(time.Now().Add(-time.Hour), false)
 	}
 }
