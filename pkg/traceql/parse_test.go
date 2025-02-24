a03c70303ba189fd8ddeed1c2b95d5a9f4cf227e
Suraj Nath <9503187+electron0zero@users.noreply.github.com>
2025-02-25 00:05:14 +0530

    fix failing tests

--- pkg/traceql/parse_test.go
+++ pkg/traceql/parse_test.go
@@ -1468,6 +1468,16 @@ func TestMetricsSecondStage(t *testing.T) {
 			),
 		},
 		{
+			in: `{ } | rate() | topk(10) with(foo="bar")`,
+			expected: newRootExprWithMetricsTwoStage(
+				newPipeline(newSpansetFilter(NewStaticBool(true))),
+				newMetricsAggregate(metricsAggregateRate, nil),
+				newMetricsTopK(10),
+			).withHints(newHints([]*Hint{
+				newHint("foo", NewStaticString("bar")),
+			})),
+		},
+		{
 			in: `{ } | rate() | bottomk(10)`,
 			expected: newRootExprWithMetricsTwoStage(
 				newPipeline(newSpansetFilter(NewStaticBool(true))),
@@ -1487,17 +1497,7 @@ func TestMetricsSecondStage(t *testing.T) {
 			),
 		},
 		{
-			in: `{ } | rate() | topk(10) with(foo="bar")`,
-			expected: newRootExprWithMetricsTwoStage(
-				newPipeline(newSpansetFilter(NewStaticBool(true))),
-				newMetricsAggregate(metricsAggregateRate, nil),
-				newMetricsTopK(10),
-			).withHints(newHints([]*Hint{
-				newHint("foo", NewStaticString("bar")),
-			})),
-		},
-		{
-			in: `{ } | rate() | topk(10) with(foo="bar")`,
+			in: `{ } | rate() | bottomk(10) with(foo="bar")`,
 			expected: newRootExprWithMetricsTwoStage(
 				newPipeline(newSpansetFilter(NewStaticBool(true))),
 				newMetricsAggregate(metricsAggregateRate, nil),
