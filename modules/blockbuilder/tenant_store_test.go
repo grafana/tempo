08b862585785d636129dac6101e76e638b2b5920
Martin Disibio <martin.disibio@grafana.com>
2025-02-28 11:17:28 -0500

    [Rhythm] Block-builder metrics and code cleanup (#4765)
    * Consolidate block builder on common lag_seconds metric, clean out metrics for revoked partitions, cleanup
    
    * Cleanup, lint
    
    * utc
    
    * Reuse existing test data functions

--- modules/blockbuilder/tenant_store_test.go
+++ modules/blockbuilder/tenant_store_test.go
@@ -1,10 +1,13 @@
 package blockbuilder
 
 import (
+	"context"
+	"flag"
 	"testing"
 	"time"
 
 	"github.com/go-kit/log"
+	"github.com/grafana/tempo/pkg/util/test"
 	"github.com/grafana/tempo/tempodb/backend"
 	"github.com/grafana/tempo/tempodb/encoding"
 	"github.com/grafana/tempo/tempodb/wal"
@@ -14,11 +17,13 @@ import (
 func getTenantStore(t *testing.T, startTime time.Time, cycleDuration, slackDuration time.Duration) (*tenantStore, error) {
 	var (
 		logger      = log.NewNopLogger()
-		blockCfg    = BlockConfig{}
 		tmpDir      = t.TempDir()
 		partition   = uint64(1)
 		startOffset = uint64(1)
+		blockCfg    = BlockConfig{}
 	)
+
+	blockCfg.RegisterFlagsAndApplyDefaults("", &flag.FlagSet{})
 
 	w, err := wal.New(&wal.Config{
 		Filepath:       tmpDir,
@@ -30,7 +35,7 @@ func getTenantStore(t *testing.T, startTime time.Time, cycleDuration, slackDurat
 	return newTenantStore("test-tenant", partition, startOffset, startTime, cycleDuration, slackDuration, blockCfg, logger, w, encoding.DefaultEncoding(), &mockOverrides{})
 }
 
-func TestAdjustTimeRangeForSlack(t *testing.T) {
+func TestTenantStoreAdjustTimeRangeForSlack(t *testing.T) {
 	var (
 		startCycleTime = time.Now()
 		cycleDuration  = time.Minute
@@ -91,4 +96,95 @@ func TestAdjustTimeRangeForSlack(t *testing.T) {
 			require.Equal(t, tt.expectedEnd, end)
 		})
 	}
+}
+
+func TestTenantStoreEndToEndHistoricalData(t *testing.T) {
+	var (
+		count     = 3
+		startTime = time.Now().Add(-24 * time.Hour)
+	)
+
+	testCases := []struct {
+		name              string
+		traceStart        time.Time // All spans will start at this time
+		traceEnd          time.Time // All spans will end at this time
+		cycleDuration     time.Duration
+		slackDuration     time.Duration
+		expectedStartTime time.Time
+		expectedEndTime   time.Time
+	}{
+		{
+			name:              "all good",
+			traceStart:        startTime,
+			traceEnd:          startTime.Add(time.Minute),
+			cycleDuration:     5 * time.Minute,
+			slackDuration:     5 * time.Minute,
+			expectedStartTime: startTime,
+			expectedEndTime:   startTime.Add(time.Minute),
+		},
+		{
+			name:              "before start",
+			traceStart:        startTime.Add(-10 * time.Minute),
+			traceEnd:          startTime,
+			cycleDuration:     5 * time.Minute,
+			slackDuration:     5 * time.Minute,
+			expectedStartTime: startTime.Add(-5 * time.Minute), // Only goes as early as -slack
+			expectedEndTime:   startTime,
+		},
+		{
+			name:              "after end",
+			traceStart:        startTime,
+			traceEnd:          startTime.Add(20 * time.Minute),
+			cycleDuration:     5 * time.Minute,
+			slackDuration:     5 * time.Minute,
+			expectedStartTime: startTime,
+			expectedEndTime:   startTime.Add(10 * time.Minute), // Only goes as far as cycle + slack
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			meta := writeHistoricalData(t, count, startTime, tc.cycleDuration, tc.slackDuration, tc.traceStart, tc.traceEnd)
+
+			// NOTE - Roundtripped times from the meta json don't include the monotonic clock value,
+			//        therefore we do the same on the test values by calling Truncate(0).
+			require.Equal(t, tc.expectedStartTime.Truncate(0).UTC(), meta.StartTime.UTC())
+			require.Equal(t, tc.expectedEndTime.Truncate(0).UTC(), meta.EndTime.UTC())
+
+			// Verify other properties of the block
+			require.EqualValues(t, count, meta.TotalObjects)
+			require.EqualValues(t, 1, meta.TotalRecords)
+			require.Greater(t, meta.Size_, uint64(0))
+		})
+	}
+}
+
+func writeHistoricalData(t *testing.T, count int, startTime time.Time, cycleDuration, slackDuration time.Duration, traceStart, traceEnd time.Time) *backend.BlockMeta {
+	var (
+		ctx   = context.Background()
+		log   = log.NewNopLogger()
+		store = newStoreWithLogger(ctx, t, log)
+	)
+
+	ts, err := getTenantStore(t, startTime, cycleDuration, slackDuration)
+	require.NoError(t, err)
+
+	for i := 0; i < count; i++ {
+		req := test.MakePushBytesRequest(t, 3, nil, uint64(traceStart.UnixNano()), uint64(traceEnd.UnixNano()))
+		for j := range req.Traces {
+			err = ts.AppendTrace(req.Ids[j], req.Traces[j].Slice, startTime)
+			require.NoError(t, err)
+		}
+	}
+
+	err = ts.Flush(ctx, store)
+	require.NoError(t, err)
+
+	// This forces another immediate poll
+	store.EnablePolling(ctx, &ownEverythingSharder{})
+
+	metas := store.BlockMetas(ts.tenantID)
+	require.Equal(t, 1, len(metas))
+
+	return metas[0]
 }
