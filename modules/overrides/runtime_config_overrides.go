59562194b7ab25946d66758e82cd1b4481d6fa33
Robbie Lankford <robert.lankford@grafana.com>
2025-03-14 05:27:59 +0000

    wip: add host info to userconfigurable overrides api

--- modules/overrides/runtime_config_overrides.go
+++ modules/overrides/runtime_config_overrides.go
@@ -528,6 +528,14 @@ func (o *runtimeConfigOverridesManager) MetricsGeneratorProcessorSpanMetricsTarg
 	return o.getOverridesForUser(userID).MetricsGenerator.Processor.SpanMetrics.TargetInfoExcludedDimensions
 }
 
+func (o *runtimeConfigOverridesManager) MetricsGeneratorProcessorHostInfoHostIdentifiers(userID string) []string {
+	return o.getOverridesForUser(userID).MetricsGenerator.Processor.HostInfo.HostIdentifiers
+}
+
+func (o *runtimeConfigOverridesManager) MetricsGeneratorProcessorHostInfoMetricName(userID string) string {
+	return o.getOverridesForUser(userID).MetricsGenerator.Processor.HostInfo.MetricName
+}
+
 // BlockRetention is the duration of the block retention for this tenant.
 func (o *runtimeConfigOverridesManager) BlockRetention(userID string) time.Duration {
 	return time.Duration(o.getOverridesForUser(userID).Compaction.BlockRetention)
