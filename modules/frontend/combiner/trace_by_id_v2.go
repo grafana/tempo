be5c28eab8555cbd8692a272e964aa475cfe8c45
Carles Garcia Cabot <carles.garciacabot@grafana.com>
2025-03-27 14:32:34 +0100

    Refactor InspectedBytes collection
    Refactor metrics collection for InspectedBytes to ensure that we do it in the same way in all endpoints.
    This also adds the metrics collection in some places where it was missing, which is probably why sometimes
    we get 0 reported bytes.
    In addition, avoid usage of an atomic where not necessary for performance.

--- modules/frontend/combiner/trace_by_id_v2.go
+++ modules/frontend/combiner/trace_by_id_v2.go
@@ -3,6 +3,7 @@ package combiner
 import (
 	"fmt"
 
+	"github.com/grafana/tempo/pkg/collector"
 	"github.com/grafana/tempo/pkg/model/trace"
 	"github.com/grafana/tempo/pkg/tempopb"
 )
@@ -14,14 +15,15 @@ func NewTypedTraceByIDV2(maxBytes int, marshalingFormat string) GRPCCombiner[*te
 func NewTraceByIDV2(maxBytes int, marshalingFormat string) Combiner {
 	combiner := trace.NewCombiner(maxBytes, true)
 	var partialTrace bool
-	var inspectedBytes uint64
+	metricsCollector := collector.NewSimpleMetricsCollector()
+
 	gc := &genericCombiner[*tempopb.TraceByIDResponse]{
 		combine: func(partial *tempopb.TraceByIDResponse, _ *tempopb.TraceByIDResponse, _ PipelineResponse) error {
 			if partial.Status == tempopb.PartialStatus_PARTIAL {
 				partialTrace = true
 			}
 			if partial.Metrics != nil {
-				inspectedBytes += partial.Metrics.InspectedBytes
+				metricsCollector.Add(partial.Metrics.InspectedBytes)
 			}
 			_, err := combiner.Consume(partial.Trace)
 			return err
@@ -36,7 +38,7 @@ func NewTraceByIDV2(maxBytes int, marshalingFormat string) Combiner {
 			deduper := newDeduper()
 			traceResult = deduper.dedupe(traceResult)
 			resp.Trace = traceResult
-			resp.Metrics = &tempopb.TraceByIDMetrics{InspectedBytes: inspectedBytes}
+			resp.Metrics = &tempopb.TraceByIDMetrics{InspectedBytes: metricsCollector.TotalValue()}
 
 			if partialTrace || combiner.IsPartialTrace() {
 				resp.Status = tempopb.PartialStatus_PARTIAL
