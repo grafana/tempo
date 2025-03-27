911a82226ee6b8d55bd66bbc53b2907e32ae517d
Carles Garcia Cabot <carles.garciacabot@grafana.com>
2025-03-27 14:32:34 +0100

    Refactor InspectedBytes collection
    Refactor metrics collection for InspectedBytes to ensure that we do it in the same way in all endpoints.
    This also adds the metrics collection in some places where it was missing, which is probably why sometimes
    we get 0 reported bytes.
    In addition, avoid usage of an atomic where not necessary for performance.

--- pkg/collector/metrics_collector.go
+++ pkg/collector/metrics_collector.go
@@ -4,8 +4,9 @@ import (
 	"go.uber.org/atomic"
 )
 
-// MetricsCollector is a simple collector that can be used to accumulate a metric
-// we primarily use it to collect the total bytes read from a reader across a request
+// MetricsCollector is a thread-safe collector that uses atomic operations
+// to accumulate metrics. We primarily use it to collect the total bytes read from
+// a reader across a request
 type MetricsCollector struct {
 	totalValue *atomic.Uint64
 }
@@ -25,4 +26,24 @@ func (mc *MetricsCollector) Add(value uint64) {
 // TotalValue returns the sum of total values collected by the collector
 func (mc *MetricsCollector) TotalValue() uint64 {
 	return mc.totalValue.Load()
+}
+
+// SimpleMetricsCollector is a collector that uses a basic uint64 without thread safety
+type SimpleMetricsCollector struct {
+	totalValue uint64
+}
+
+func NewSimpleMetricsCollector() *SimpleMetricsCollector {
+	return &SimpleMetricsCollector{
+		totalValue: 0,
+	}
+}
+
+func (mc *SimpleMetricsCollector) Add(value uint64) {
+	// Note: this is not thread-safe.
+	mc.totalValue += value
+}
+
+func (mc *SimpleMetricsCollector) TotalValue() uint64 {
+	return mc.totalValue
 }
