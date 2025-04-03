ae6102f7fe54c7f4188c57bbc45d5c39138dd0d3
Ruslan Mikhailov <thedevdruid@pm.me>
2025-04-03 11:05:40 +0200

    Vulture: better logs

--- cmd/tempo-vulture/main.go
+++ cmd/tempo-vulture/main.go
@@ -695,6 +695,7 @@ func queryMetrics(client httpclient.TempoHTTPClient, seed time.Time, config vult
 
 	if len(resp.Series) == 0 {
 		tm.notFoundByMetrics++
+		logger.Error("failed to find trace by metrics", zap.Error(err))
 		return tm, fmt.Errorf("expected trace %s not found in metrics", hexID)
 	}
 
@@ -715,6 +716,7 @@ func queryMetrics(client httpclient.TempoHTTPClient, seed time.Time, config vult
 
 	if sum < 1 {
 		tm.notFoundByMetrics++
+		logger.Error("failed to find trace by metrics", zap.Error(err))
 		return tm, fmt.Errorf("expected trace %s not found in metrics", hexID)
 	}
 
