337ff2de3f55e4247208d961b650db3ebf2ecea9
Zach Leslie <zach.leslie@grafana.com>
2025-06-13 15:53:16 +0000

    Pick lint

--- pkg/traceql/engine_metrics.go
+++ pkg/traceql/engine_metrics.go
@@ -1639,10 +1639,7 @@ func (h *HistogramAggregator) Results() SeriesSet {
 	results := make(SeriesSet, len(h.ss)*len(h.qs))
 
 	// Collect all exemplars and calculate representative quantile values
-	var (
-		allExemplars   []Exemplar
-		quantileValues []float64
-	)
+	var allExemplars []Exemplar
 
 	// Aggregate buckets across all series and time intervals for better quantile calculation
 	aggregatedBuckets := make(map[float64]int) // bucketMax -> totalCount
@@ -1674,7 +1671,7 @@ func (h *HistogramAggregator) Results() SeriesSet {
 		return buckets[i].Max < buckets[j].Max
 	})
 
-	quantileValues = make([]float64, len(h.qs))
+	quantileValues := make([]float64, len(h.qs))
 	for i, q := range h.qs {
 		quantileValues[i] = Log2Quantile(q, buckets)
 	}
