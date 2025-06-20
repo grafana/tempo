2ce2e9fe179639217a698219806482993a6d86f0
Mario <mariorvinas@gmail.com>
2025-06-20 12:40:15 +0200

    Refactor

--- modules/distributor/receiver/middleware.go
+++ modules/distributor/receiver/middleware.go
@@ -9,52 +9,12 @@ import (
 	"go.opentelemetry.io/collector/client"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/ptrace"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/trace"
 )
 
 type ConsumeTracesFunc func(context.Context, ptrace.Traces) error
 
 func (f ConsumeTracesFunc) Capabilities() consumer.Capabilities {
 	return consumer.Capabilities{MutatesData: false}
-}
-
-var _ propagation.TextMapCarrier = (*clientMetadataCarrier)(nil)
-
-// clientMetadataCarrier is a propagation.TextMapCarrier for client.Metadata
-type clientMetadataCarrier struct {
-	metadata client.Metadata
-}
-
-func (c *clientMetadataCarrier) Get(key string) string {
-	values := c.metadata.Get(key)
-	if len(values) == 0 {
-		return ""
-	}
-	return values[0]
-}
-
-func (c *clientMetadataCarrier) Set(string, string) {} // Not implemented as we only need extraction
-
-func (c *clientMetadataCarrier) Keys() []string {
-	var keys []string
-	for key := range c.metadata.Keys() {
-		keys = append(keys, key)
-	}
-	return keys
-}
-
-type onlySampledTraces struct {
-	propagation.TextMapPropagator
-}
-
-func (o onlySampledTraces) Inject(ctx context.Context, carrier propagation.TextMapCarrier) {
-	sc := trace.SpanContextFromContext(ctx)
-	if !sc.IsSampled() {
-		return
-	}
-	o.TextMapPropagator.Inject(ctx, carrier)
 }
 
 func (f ConsumeTracesFunc) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
@@ -100,9 +60,7 @@ func (m *multiTenancyMiddleware) Wrap(next consumer.Traces) consumer.Traces {
 			info := client.FromContext(ctx)
 
 			// Extract trace context from HTTP headers
-			carrier := &clientMetadataCarrier{metadata: info.Metadata}
-			propagator := &onlySampledTraces{otel.GetTextMapPropagator()}
-			ctx = propagator.Extract(ctx, carrier)
+			ctx = extractTracingContextFromMetadata(ctx, info.Metadata)
 
 			orgIDs := info.Metadata.Get(user.OrgIDHeaderName)
 			clientAddr := "unknown"
