678b7a28b38846813bac05193efe007be4b7f422
Zach Leslie <zach.leslie@grafana.com>
2025-07-01 14:21:53 +0000

    Address PR feedback

--- pkg/traceql/engine_metrics.go
+++ pkg/traceql/engine_metrics.go
@@ -1473,8 +1473,8 @@ func (h *HistogramAggregator) Combine(in []*tempopb.TimeSeries) {
 				hist:   make([]Histogram, h.len),
 				exemplarBuckets: newBucketSet(
 					h.exemplarLimit,
-					h.start,
-					h.end,
+					alignStart(h.start, h.end, h.step),
+					alignEnd(h.start, h.end, h.step),
 				),
 			}
 		}
