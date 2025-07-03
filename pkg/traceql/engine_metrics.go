77480a13ba9947665d0f919f4015a9f839a2d7bb
Zach Leslie <zach.leslie@grafana.com>
2025-07-03 21:52:34 +0000

    Skip lint on Put-ing the slice back, since I see we do this all over

--- pkg/traceql/engine_metrics.go
+++ pkg/traceql/engine_metrics.go
@@ -1735,7 +1735,7 @@ func (h *HistogramAggregator) Results() SeriesSet {
 			for i := range histSlice {
 				histSlice[i] = Histogram{}
 			}
-			histogramSlicePool.Put(histSlice[:0])
+			histogramSlicePool.Put(histSlice[:0]) //nolint:all //SA6002
 		}
 	}
 
