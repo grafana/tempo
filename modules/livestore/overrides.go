94178e306a10ef1196332cbf96318802fa7f3108
mattdurham <mattdurham@ppog.org>
2025-08-14 11:32:36 -0400

    simplify

--- modules/livestore/overrides.go
+++ modules/livestore/overrides.go
@@ -1,48 +1,19 @@
 package livestore
 
 import (
-	"time"
-
 	"github.com/grafana/tempo/modules/generator/processor/localblocks"
 	"github.com/grafana/tempo/modules/overrides"
-	"github.com/grafana/tempo/tempodb/backend"
 )
 
 var _ localblocks.ProcessorOverrides = (*Overrides)(nil)
 
 type Overrides struct {
-	cfg     Config
-	service overrides.Interface
+	cfg Config
+	overrides.Interface
 }
 
-// NewMergedOverrides creates a new MergedOverrides that wraps the main overrides service
 func NewOverrides(service overrides.Interface) *Overrides {
 	return &Overrides{
-		service: service,
+		Interface: service,
 	}
-}
-
-// Livestore Overrides interface implementation
-func (m *Overrides) MaxLocalTracesPerUser(userID string) int {
-	return m.service.MaxLocalTracesPerUser(userID)
-}
-
-func (m *Overrides) MaxBytesPerTrace(userID string) int {
-	return m.service.MaxBytesPerTrace(userID)
-}
-
-func (m *Overrides) DedicatedColumns(userID string) backend.DedicatedColumns {
-	return m.service.DedicatedColumns(userID)
-}
-
-func (m *Overrides) UnsafeQueryHints(userID string) bool {
-	return m.service.UnsafeQueryHints(userID)
-}
-
-func (m *Overrides) LiveStoreCompleteBlockTimeout(userID string) time.Duration {
-	return m.cfg.CompleteBlockTimeout
-}
-
-func (m *Overrides) LiveStoreMetricsConcurrentBlocks(_ string) uint {
-	return 10
 }
