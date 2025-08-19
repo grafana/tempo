e125c43d920885d1bbbc7c119bd07bd52b195b09
mattdurham <mattdurham@ppog.org>
2025-08-19 13:49:54 -0400

    Align with how livestore cuts blocks

--- modules/livestore/instance.go
+++ modules/livestore/instance.go
@@ -303,7 +303,7 @@ func (i *instance) deleteOldBlocks() error {
 	defer i.blocksMtx.Unlock()
 
 	// TODO: Configurable
-	cutoff := time.Now().Add(-1 * time.Hour) // Delete blocks older than 1 hour
+	cutoff := time.Now().Add(i.Cfg.CompleteBlockTimeout) // Delete blocks older than Complete Block Timeout
 
 	for id, walBlock := range i.walBlocks {
 		if walBlock.BlockMeta().EndTime.Before(cutoff) {
