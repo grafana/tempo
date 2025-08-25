8a28716592354ed05d23b029e836f9418e750729
Carles Garcia Cabot <carles.garciacabot@grafana.com>
2025-08-25 16:28:52 +0200

    tempo: add tests for vparquet pools
    To ensure data is cleaned and not leaked.

---
+++ tempodb/encoding/vparquet2/pool_test.go
@@ -0,0 +1,81 @@
+package vparquet2
+
+import (
+	"testing"
+
+	"github.com/parquet-go/parquet-go"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/grafana/tempo/pkg/traceql"
+)
+
+func TestSpanPoolRelease(t *testing.T) {
+	// Get a span from the pool and populate it
+	span1 := getSpan()
+	span1.id = []byte("test-id")
+	span1.startTimeUnixNanos = 12345
+	span1.durationNanos = 67890
+	span1.nestedSetParent = 1
+	span1.nestedSetLeft = 2
+	span1.nestedSetRight = 3
+	span1.attributes[traceql.NewIntrinsic(traceql.IntrinsicName)] = traceql.NewStaticString("test")
+	span1.attributes[traceql.NewAttribute("custom")] = traceql.NewStaticInt(42)
+
+	putSpan(span1)
+
+	span2 := getSpan()
+
+	// Verify the reused span is properly cleared
+	assert.Nil(t, span2.id, "id should be cleared")
+	assert.Zero(t, span2.startTimeUnixNanos, "startTimeUnixNanos should be cleared")
+	assert.Zero(t, span2.durationNanos, "durationNanos should be cleared")
+	assert.Zero(t, span2.nestedSetParent, "nestedSetParent should be cleared")
+	assert.Zero(t, span2.nestedSetLeft, "nestedSetLeft should be cleared")
+	assert.Zero(t, span2.nestedSetRight, "nestedSetRight should be cleared")
+	assert.Len(t, span2.attributes, 0, "attributes should be cleared")
+}
+
+func TestSpansetPoolRelease(t *testing.T) {
+	// Get a spanset from the pool and populate it
+	spanset1 := getSpanset()
+	spanset1.TraceID = []byte("trace-id")
+	spanset1.RootSpanName = "root"
+	spanset1.RootServiceName = "service"
+	spanset1.StartTimeUnixNanos = 12345
+	spanset1.DurationNanos = 67890
+	spanset1.Scalar = traceql.NewStaticString("scalar")
+	spanset1.Attributes = append(spanset1.Attributes, &traceql.SpansetAttribute{Name: "test", Val: traceql.NewStaticString("value")})
+	spanset1.Spans = append(spanset1.Spans, getSpan())
+
+	putSpanset(spanset1)
+
+	spanset2 := getSpanset()
+
+	// Verify the reused spanset is properly cleared
+	assert.Nil(t, spanset2.TraceID, "TraceID should be cleared")
+	assert.Empty(t, spanset2.RootSpanName, "RootSpanName should be cleared")
+	assert.Empty(t, spanset2.RootServiceName, "RootServiceName should be cleared")
+	assert.Zero(t, spanset2.StartTimeUnixNanos, "StartTimeUnixNanos should be cleared")
+	assert.Zero(t, spanset2.DurationNanos, "DurationNanos should be cleared")
+	assert.Equal(t, traceql.TypeNil, spanset2.Scalar.Type, "Scalar should be cleared")
+	assert.Len(t, spanset2.Attributes, 0, "Attributes should be cleared")
+	assert.Len(t, spanset2.Spans, 0, "Spans should be cleared")
+}
+
+func TestRowPoolRelease(t *testing.T) {
+	pool := newRowPool(5)
+
+	// Get a row from the pool and populate it
+	row1 := pool.Get()
+	row1 = append(row1, parquet.ValueOf("value1"))
+	row1 = append(row1, parquet.ValueOf(42))
+	row1 = append(row1, parquet.ValueOf(true))
+
+	pool.Put(row1)
+
+	row2 := pool.Get()
+
+	// Verify the reused row is properly cleared
+	assert.Len(t, row2, 0, "row should be empty after release")
+	assert.Equal(t, 5, cap(row2), "capacity should be preserved")
+}
