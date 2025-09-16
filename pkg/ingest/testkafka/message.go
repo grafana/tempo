77407e1b8686b5675dd52ee2ead2822f39ce560b
Mario <mariorvinas@gmail.com>
2025-09-16 11:17:12 +0200

    Undo thing

--- pkg/ingest/testkafka/message.go
+++ pkg/ingest/testkafka/message.go
@@ -7,7 +7,7 @@ import (
 	"github.com/twmb/franz-go/pkg/kmsg"
 )
 
-// CreateProduceResponseError returns a kmsg.ProduceResponse containing err for the input topic and Partition.
+// CreateProduceResponseError returns a kmsg.ProduceResponse containing err for the input topic and partition.
 func CreateProduceResponseError(version int16, topic string, partition int32, err *kerr.Error) *kmsg.ProduceResponse {
 	return &kmsg.ProduceResponse{
 		Version: version,
