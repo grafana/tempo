0af01d65421145b1d9763d6e2f91b9d0cd8ead9f
Carles Garcia Cabot <carles.garciacabot@grafana.com>
2025-09-30 13:16:45 +0200

    wip

--- modules/generator/registry/registry.go
+++ modules/generator/registry/registry.go
@@ -232,9 +232,10 @@ func (r *ManagedRegistry) CollectMetrics(ctx context.Context) {
 		demandSeries += c
 		demandSeriesEstimate += m.countTotalSeriesEstimate()
 	}
-	r.metricDemandSeries.Set(float64(demandSeries))
-	r.metricDemandSeriesEstimate.Set(float64(max(demandSeriesEstimate, int(r.activeSeries.Load()))))
+
 	if r.overrides.MetricsGeneratorDisableCollection(r.tenant) {
+		r.metricDemandSeries.Set(float64(demandSeries))
+		r.metricDemandSeriesEstimate.Set(float64(max(demandSeriesEstimate, int(r.activeSeries.Load()))))
 		return
 	}
 
@@ -266,6 +267,14 @@ func (r *ManagedRegistry) CollectMetrics(ctx context.Context) {
 
 	maxActiveSeries := r.overrides.MetricsGeneratorMaxActiveSeries(r.tenant)
 	r.metricMaxActiveSeries.Set(float64(maxActiveSeries))
+
+	if demandSeries > int(maxActiveSeries) || demandSeriesEstimate > int(maxActiveSeries) {
+		r.metricDemandSeries.Set(float64(demandSeries))
+		r.metricDemandSeriesEstimate.Set(float64(demandSeriesEstimate))
+	} else {
+		r.metricDemandSeries.Set(float64(activeSeries))
+		r.metricDemandSeriesEstimate.Set(float64(activeSeries))
+	}
 
 	// Try to avoid committing after we have started the shutdown process.
 	if ctx.Err() != nil { // shutdown
