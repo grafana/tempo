fb2c6505b43f24fb7e92f8823681c67ff2971e7f
A. Stoewer <adrian@stoewer.me>
2025-10-02 13:21:07 +1000

    Convert vp5 test-data and re-enable find by ID test

--- tempodb/encoding/vparquet5/block_findtracebyid_test.go
+++ tempodb/encoding/vparquet5/block_findtracebyid_test.go
@@ -105,7 +105,6 @@ func TestBackendBlockFindTraceByID(t *testing.T) {
 }
 
 func TestBackendBlockFindTraceByID_TestData(t *testing.T) {
-	t.Skip("disable until block in ./test-data is converted")
 	rawR, _, _, err := local.New(&local.Config{
 		Path: "./test-data",
 	})
