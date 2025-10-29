57a8fe315675bdcc649d8b7479cb0f603bd13f20
javiermolinar <javimolina@fastmail.com>
2025-10-29 10:51:24 +0100

    handle nil case

--- modules/generator/registry/util.go
+++ modules/generator/registry/util.go
@@ -5,8 +5,10 @@ import "github.com/prometheus/prometheus/model/labels"
 // newSeriesLabelsBuilder creates a labels builder with user labels and external labels pre-populated.
 func newSeriesLabelsBuilder(labelValueCombo *LabelValueCombo, externalLabels map[string]string) *labels.Builder {
 	builder := labels.NewBuilder(labels.Labels{})
-	for i := range labelValueCombo.labels.names {
-		builder.Set(labelValueCombo.labels.names[i], labelValueCombo.labels.values[i])
+	if labelValueCombo != nil {
+		for i := range labelValueCombo.labels.names {
+			builder.Set(labelValueCombo.labels.names[i], labelValueCombo.labels.values[i])
+		}
 	}
 	for name, value := range externalLabels {
 		builder.Set(name, value)
