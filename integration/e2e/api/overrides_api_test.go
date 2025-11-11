31a98b789d66609db2308e503c07a25c02e3e108
Suraj Nath <suraj.sidh@grafana.com>
2025-11-12 01:25:17 +0530

    move existing TestOverrides test into e2e/api test package

--- integration/e2e/api/overrides_api_test.go
+++ integration/e2e/api/overrides_api_test.go
@@ -14,61 +14,185 @@ import (
 	"github.com/grafana/tempo/modules/overrides/histograms"
 	"github.com/grafana/tempo/modules/overrides/userconfigurable/client"
 	"github.com/grafana/tempo/pkg/httpclient"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
 )
 
 const (
+	configAllInOneS3          = "./../deployments/config-all-in-one-s3.yaml"
+	configAllInOneAzurite     = "./../deployments/config-all-in-one-azurite.yaml"
+	configAllInOneGCS         = "./../deployments/config-all-in-one-gcs.yaml"
 	configAllInOneS3Overrides = "./config-all-in-one-s3-overrides.yaml"
 )
 
-// helper functions for overrides API tests
-func boolPtr(b bool) *bool {
-	return &b
-}
-
-func histogramModePtr(h histograms.HistogramMethod) *histograms.HistogramMethod {
-	return &h
-}
-
-func uint32Ptr(u uint32) *uint32 {
-	return &u
-}
-
-func keys(m map[string]struct{}) []string {
-	var keys []string
-	for k := range m {
-		keys = append(keys, k)
+func TestOverrides(t *testing.T) {
+	testBackends := []struct {
+		name           string
+		skipVersioning bool
+		configFile     string
+	}{
+		{
+			name: "local",
+			// local backend does not enforce versioning
+			skipVersioning: true,
+			configFile:     configAllInOneLocal,
+		},
+		{
+			name:       "s3",
+			configFile: configAllInOneS3,
+		},
+		{
+			name:       "azure",
+			configFile: configAllInOneAzurite,
+		},
+		{
+			name:       "gcs",
+			configFile: configAllInOneGCS,
+		},
 	}
-	return keys
-}
+	for _, tc := range testBackends {
+		t.Run(tc.name, func(t *testing.T) {
+			s, tempo := setupOverridesAPITest(t, tc.configFile)
+			defer s.Close()
 
-func setupOverridesAPITest(t *testing.T) (*e2e.Scenario, *e2e.HTTPService) {
-	s, err := e2e.NewScenario("tempo_e2e")
-	require.NoError(t, err)
+			orgID := ""
+			apiClient := httpclient.New("http://"+tempo.Endpoint(3200), orgID)
 
-	// copy config template to shared directory and expand template variables
-	tmplConfig := map[string]any{"Prefix": ""}
-	configFile, err := util.CopyTemplateToSharedDir(s, configAllInOneS3Overrides, "config.yaml", tmplConfig)
-	require.NoError(t, err)
+			// Create overrides
+			initialLimits := &client.Limits{
+				MetricsGenerator: client.LimitsMetricsGenerator{
+					DisableCollection: boolPtr(true),
+				},
+			}
 
-	// set up the S3 backend
-	cfg := app.Config{}
-	buff, err := os.ReadFile(configFile)
-	require.NoError(t, err)
-	err = yaml.UnmarshalStrict(buff, &cfg)
-	require.NoError(t, err)
-	_, err = backend.New(s, cfg)
-	require.NoError(t, err)
+			if !tc.skipVersioning {
+				fmt.Println("* Creating overrides with non-0 version")
+				_, err := apiClient.SetOverrides(initialLimits, "123")
+				assert.ErrorContains(t, err, "412") // precondition failed
+			}
 
-	tempo := util.NewTempoAllInOne()
-	require.NoError(t, s.StartAndWaitReady(tempo))
+			fmt.Println("* Creating overrides")
+			_, err := apiClient.SetOverrides(initialLimits, "0")
+			assert.NoError(t, err)
 
-	return s, tempo
+			limits, version, err := apiClient.GetOverrides()
+			assert.NoError(t, err)
+			EnableInstanceLabel, EnableInstanceLabelIsSet := limits.GetMetricsGenerator().GetProcessor().GetSpanMetrics().GetEnableInstanceLabel()
+			assert.True(t, EnableInstanceLabel)
+			assert.False(t, EnableInstanceLabelIsSet)
+			printLimits(limits, version)
+
+			disableCollection, ok := limits.GetMetricsGenerator().GetDisableCollection()
+			assert.True(t, ok)
+			assert.True(t, disableCollection)
+
+			// Update overrides - POST
+			updatedLimits := &client.Limits{
+				MetricsGenerator: client.LimitsMetricsGenerator{
+					DisableCollection: nil,
+					Processors:        map[string]struct{}{"span-metrics": {}},
+				},
+			}
+
+			if !tc.skipVersioning {
+				fmt.Println("* Update overrides with bogus version number")
+				_, err = apiClient.SetOverrides(updatedLimits, "abc")
+				assert.ErrorContains(t, err, "412") // precondition failed
+
+				fmt.Println("* Update overrides with backend.VersionNew")
+				_, err = apiClient.SetOverrides(updatedLimits, "0")
+				assert.ErrorContains(t, err, "412") // precondition failed
+
+				fmt.Println("* Update overrides with wrong version number")
+				_, err = apiClient.SetOverrides(updatedLimits, "123")
+				assert.ErrorContains(t, err, "412") // precondition failed
+			}
+
+			fmt.Println("* Update overrides")
+			_, err = apiClient.SetOverrides(updatedLimits, version)
+			assert.NoError(t, err)
+
+			limits, version, err = apiClient.GetOverrides()
+
+			assert.NoError(t, err)
+			printLimits(limits, version)
+
+			_, ok = limits.GetMetricsGenerator().GetDisableCollection()
+			assert.False(t, ok) // is not set anymore
+			processors, ok := limits.GetMetricsGenerator().GetProcessors()
+			assert.True(t, ok)
+			assert.ElementsMatch(t, keys(processors.GetMap()), []string{"span-metrics"})
+
+			// Modify overrides - PATCH
+			patch := &client.Limits{
+				MetricsGenerator: client.LimitsMetricsGenerator{
+					GenerateNativeHistograms:       histogramModePtr(histograms.HistogramMethodNative),
+					NativeHistogramMaxBucketNumber: uint32Ptr(200),
+					DisableCollection:              boolPtr(true),
+					Processor: client.LimitsMetricsGeneratorProcessor{
+						SpanMetrics: client.LimitsMetricsGeneratorProcessorSpanMetrics{
+							EnableInstanceLabel: boolPtr(false),
+						},
+					},
+				},
+			}
+
+			fmt.Println("* Patch overrides")
+			limits, version, err = apiClient.PatchOverrides(patch)
+			assert.NoError(t, err)
+
+			disableCollection, ok = limits.GetMetricsGenerator().GetDisableCollection()
+			assert.True(t, ok)
+			assert.True(t, disableCollection)
+			processors, ok = limits.GetMetricsGenerator().GetProcessors()
+			assert.True(t, ok)
+			assert.ElementsMatch(t, keys(processors.GetMap()), []string{"span-metrics"})
+			EnableInstanceLabel, EnableInstanceLabelIsSet = limits.GetMetricsGenerator().GetProcessor().GetSpanMetrics().GetEnableInstanceLabel()
+			assert.False(t, EnableInstanceLabel)
+			assert.True(t, EnableInstanceLabelIsSet)
+
+			generateNativeHistograms, ok := limits.GetMetricsGenerator().GetGenerateNativeHistograms()
+			assert.True(t, ok)
+			assert.Equal(t, histograms.HistogramMethodNative, generateNativeHistograms)
+			nativeHistogramMaxBucketNumber, ok := limits.GetMetricsGenerator().GetNativeHistogramMaxBucketNumber()
+			assert.True(t, ok)
+			assert.Equal(t, uint32(200), nativeHistogramMaxBucketNumber)
+
+			// Delete overrides
+			if !tc.skipVersioning && tc.name != "gcs" {
+				// Delete with preconditions is not supported by fake-gcs-server https://github.com/fsouza/fake-gcs-server/issues/1282
+				fmt.Println("* Deleting overrides - don't respect version")
+				err = apiClient.DeleteOverrides("123")
+				assert.ErrorContains(t, err, "412") // precondition failed
+			}
+
+			fmt.Println("* Deleting overrides")
+			err = apiClient.DeleteOverrides(version)
+			assert.NoError(t, err)
+
+			// Get overrides - 404
+			fmt.Println("* Get overrides - 404")
+			_, _, err = apiClient.GetOverrides()
+			assert.ErrorIs(t, err, httpclient.ErrNotFound)
+
+			// Recreate overrides - PATCH
+			patch = &client.Limits{
+				MetricsGenerator: client.LimitsMetricsGenerator{
+					DisableCollection: boolPtr(true),
+				},
+			}
+
+			fmt.Println("* Patch overrides - overrides don't exist yet")
+			_, _, err = apiClient.PatchOverrides(patch)
+			assert.NoError(t, err)
+		})
+	}
 }
 
 func TestOverridesAPI_GET(t *testing.T) {
-	s, tempo := setupOverridesAPITest(t)
+	s, tempo := setupOverridesAPITest(t, configAllInOneS3Overrides)
 	defer s.Close()
 
 	t.Run("returns 404 when config not found", func(t *testing.T) {
@@ -105,7 +229,7 @@ func TestOverridesAPI_GET(t *testing.T) {
 }
 
 func TestOverridesAPI_POST(t *testing.T) {
-	s, tempo := setupOverridesAPITest(t)
+	s, tempo := setupOverridesAPITest(t, configAllInOneS3Overrides)
 	defer s.Close()
 
 	t.Run("API returns 428 without if-match header", func(t *testing.T) {
@@ -239,7 +363,7 @@ func TestOverridesAPI_POST(t *testing.T) {
 }
 
 func TestOverridesAPI_PATCH(t *testing.T) {
-	s, tempo := setupOverridesAPITest(t)
+	s, tempo := setupOverridesAPITest(t, configAllInOneS3Overrides)
 	defer s.Close()
 
 	t.Run("with no existing config creates new config", func(t *testing.T) {
@@ -502,7 +626,7 @@ func TestOverridesAPI_PATCH(t *testing.T) {
 }
 
 func TestOverridesAPI_DELETE(t *testing.T) {
-	s, tempo := setupOverridesAPITest(t)
+	s, tempo := setupOverridesAPITest(t, configAllInOneS3Overrides)
 	defer s.Close()
 
 	t.Run("config is deleted with correct etag", func(t *testing.T) {
@@ -576,4 +700,62 @@ func TestOverridesAPI_DELETE(t *testing.T) {
 		require.Equal(t, setEtag, gotEtag)
 		require.Equal(t, limits, returnedLimits)
 	})
+}
+
+// Helper functions for overrides API tests
+
+// setupOverridesAPITest stands up a new NewTempoAllInOne for e2e test
+func setupOverridesAPITest(t *testing.T, configFilePath string) (*e2e.Scenario, *e2e.HTTPService) {
+	s, err := e2e.NewScenario("tempo_e2e")
+	require.NoError(t, err)
+
+	// copy config template to shared directory and expand template variables
+	tmplConfig := map[string]any{"Prefix": ""}
+	configFile, err := util.CopyTemplateToSharedDir(s, configFilePath, "config.yaml", tmplConfig)
+	require.NoError(t, err)
+
+	// set up the backend using the config file
+	cfg := app.Config{}
+	buff, err := os.ReadFile(configFile)
+	require.NoError(t, err)
+	err = yaml.UnmarshalStrict(buff, &cfg)
+	require.NoError(t, err)
+	_, err = backend.New(s, cfg)
+	require.NoError(t, err)
+
+	tempo := util.NewTempoAllInOne()
+	require.NoError(t, s.StartAndWaitReady(tempo))
+
+	return s, tempo
+}
+
+func printLimits(limits *client.Limits, version string) {
+	var str string
+	if limits != nil {
+		bytes, err := jsoniter.Marshal(limits)
+		if err == nil {
+			str = string(bytes)
+		}
+	}
+	fmt.Printf("* Overrides (version = %s): %+v\n", version, str)
+}
+
+func boolPtr(b bool) *bool {
+	return &b
+}
+
+func histogramModePtr(h histograms.HistogramMethod) *histograms.HistogramMethod {
+	return &h
+}
+
+func uint32Ptr(u uint32) *uint32 {
+	return &u
+}
+
+func keys(m map[string]struct{}) []string {
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
 }
