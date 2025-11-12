2b933921b081298bff011dddf4b183d4d3dae6ac
A. Stoewer <adrian@stoewer.me>
2025-11-12 14:16:45 +1000

    Use virtualRowNumberIterator instead of StatusCode colum iter

--- tempodb/encoding/vparquet5/block_traceql.go
+++ tempodb/encoding/vparquet5/block_traceql.go
@@ -2227,7 +2227,8 @@ func createSpanIterator(makeIter, makeNilIter makeIterFn, innerIterators []parqu
 		if sampler != nil {
 			pred = newSamplingPredicate(sampler, nil)
 		}
-		required = []parquetquery.Iterator{makeIter(columnPathSpanStatusCode, pred, "")}
+		required = []parquetquery.Iterator{newVirtualRowNumberIterator(makeIter(columnPathScopeSpanSpanCount, pred, "spanCount"), DefinitionLevelResourceSpansILSSpan)}
+		// required = []parquetquery.Iterator{makeIter(columnPathSpanStatusCode, pred, "")}
 	}
 
 	// Left join here means the span id/start/end iterators + 1 are required,
