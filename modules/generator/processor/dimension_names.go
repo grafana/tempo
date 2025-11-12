614123a7ae528ed590d213f64c8e59cf198b7f86
Carles Garcia <carles.garciacabot@grafana.com>
2025-11-12 13:43:50 +0100

    generator: extract validation to reusable package (#5854)
    * generator: extract validation to reusable package
    
    We will also use these functions for the config Controller
    
    * wip
    
    * update deprecated label validation
    
    * fix intrinsic dimensions
    
    * add span metrics subprocessor comments

---
+++ modules/generator/processor/dimension_names.go
@@ -0,0 +1,11 @@
+package processor
+
+const (
+	DimService       = "service"
+	DimSpanName      = "span_name"
+	DimSpanKind      = "span_kind"
+	DimStatusCode    = "status_code"
+	DimStatusMessage = "status_message"
+	DimJob           = "job"
+	DimInstance      = "instance"
+)
---
+++ modules/generator/validation/sanitize.go
@@ -0,0 +1,29 @@
+package validation
+
+import (
+	"fmt"
+	"slices"
+	"unicode/utf8"
+)
+
+type SanitizeFn func(string) string
+
+func SanitizeLabelNameWithCollisions(name string, dimensions []string, sanitizeFn SanitizeFn) string {
+	sanitized := sanitizeFn(name)
+
+	// check if same label as intrinsics
+	if slices.Contains(dimensions, sanitized) {
+		return "__" + sanitized
+	}
+
+	return sanitized
+}
+
+func ValidateUTF8LabelValues(v []string) error {
+	for _, value := range v {
+		if !utf8.ValidString(value) {
+			return fmt.Errorf("invalid utf8 string: %s", value)
+		}
+	}
+	return nil
+}
