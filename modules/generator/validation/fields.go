8c9f93af2a994bd3e5ec8a0d7205fdff6d122725
Suraj Nath <suraj.sidh@grafana.com>
2025-11-20 00:00:19 +0530

    Add validation logic for cost attribution config

--- modules/generator/validation/fields.go
+++ modules/generator/validation/fields.go
@@ -8,7 +8,9 @@ import (
 	"github.com/grafana/tempo/modules/generator/processor"
 	"github.com/grafana/tempo/modules/generator/registry"
 	"github.com/grafana/tempo/pkg/sharedconfig"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/util/strutil"
 )
 
 var SupportedProcessors = []string{
@@ -118,5 +120,37 @@ func ValidateNativeHistogramBucketFactor(factor float64) error {
 	if factor <= 1 {
 		return fmt.Errorf("metrics_generator.native_histogram_bucket_factor must be greater than 1")
 	}
+	return nil
+}
+
+func ValidateCostAttributionDimensions(dimensions map[string]string) error {
+	// track sanitized label names to detect duplicates
+	seenLabels := make(map[string]string)
+
+	// map is with key=tempo attribute, value=prometheus labelName
+	for k, v := range dimensions {
+		// build labelName the same way as usage.GetBuffersForDimensions in the usage tracker
+		labelName := v
+		if labelName == "" {
+			labelName = k // The dimension is using default mapping, we map it to attribute
+		}
+		labelName = strutil.SanitizeLabelName(labelName) // sanitize final label name the label name
+
+		// Check for duplicate prometheus label names, we silently pick one so validate and don't allow it.
+		if originalKey, exists := seenLabels[labelName]; exists {
+			return fmt.Errorf("cost_attribution.dimensions has duplicate label name: '%s', both '%s' and '%s' map to it", labelName, originalKey, k)
+		}
+		seenLabels[labelName] = k
+
+		// creating a desc just so can do the validation
+		desc := prometheus.NewDesc("test_desc", "test desc created for validation", []string{labelName}, nil)
+		// try to create a metric and see if there are any error, we use same method in usage.Collect
+		_, err := prometheus.NewConstMetric(desc, prometheus.CounterValue, float64(1), labelName)
+		if err != nil {
+			return fmt.Errorf("cost_attribution.dimensions config has invalid label name: '%s'", labelName)
+		}
+	}
+
+	// no errors, we are good.
 	return nil
 }
