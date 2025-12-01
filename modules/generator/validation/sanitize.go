3d3d138d1da9168c70f5b96976f4269dab376293
Carles Garcia <carles.garciacabot@grafana.com>
2025-12-01 09:26:36 +0100

    config: validate more fields and duplicate labels in the user API (#6008)
    * config: validate more fields and duplicate labels in the user API
    
    - detect duplicate labels after sanitization in span-metrics and service-graphs dimensions
    - add validation for dimension mappings
    - add validation for host-info metric name
    
    * add changelog entry

--- modules/generator/validation/sanitize.go
+++ modules/generator/validation/sanitize.go
@@ -1,9 +1,6 @@
 package validation
 
 import (
-	"fmt"
-	"unicode/utf8"
-
 	"github.com/prometheus/prometheus/util/strutil"
 )
 
@@ -18,15 +15,6 @@ func SanitizeLabelNameWithCollisions(name string, dimensions map[string]struct{}
 	}
 
 	return sanitized
-}
-
-func ValidateUTF8LabelValues(v []string) error {
-	for _, value := range v {
-		if !utf8.ValidString(value) {
-			return fmt.Errorf("invalid utf8 string: %s", value)
-		}
-	}
-	return nil
 }
 
 func SanitizeLabelName(name string) string {
