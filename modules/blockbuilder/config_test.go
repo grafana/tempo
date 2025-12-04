83a47d7b9c3d9bb0a008b68e30b0892c6ba3268c
Mario <mariorvinas@gmail.com>
2025-12-04 16:47:19 +0100

    add fix

--- modules/blockbuilder/config_test.go
+++ modules/blockbuilder/config_test.go
@@ -62,3 +62,68 @@ func TestConfig_validate(t *testing.T) {
 		})
 	}
 }
+
+func TestConfig_partitionAssignment(t *testing.T) {
+	instanceID := "block-builder-42"
+	for _, tc := range []struct {
+		name               string
+		cfg                Config
+		expectedPartitions []int32
+	}{
+		{
+			name: "assigned_partitions",
+			cfg: Config{
+				InstanceID: instanceID,
+				AssignedPartitionsMap: map[string][]int32{
+					instanceID: {1, 2, 56},
+				},
+			},
+			expectedPartitions: []int32{1, 2, 56},
+		},
+		{
+			name: "partitions_per_instance",
+			cfg: Config{
+				InstanceID:            instanceID,
+				PartitionsPerInstance: 2,
+			},
+			expectedPartitions: []int32{84, 85},
+		},
+		{
+			name: "partitions_per_instance takes precedence",
+			cfg: Config{
+				InstanceID:            instanceID,
+				PartitionsPerInstance: 2,
+				AssignedPartitionsMap: map[string][]int32{
+					instanceID: {1, 2, 56},
+				},
+			},
+			expectedPartitions: []int32{84, 85},
+		},
+		{
+			name: "falls back to assigned_partitions if ID doesn't have an index",
+			cfg: Config{
+				InstanceID: "block-builder",
+				AssignedPartitionsMap: map[string][]int32{
+					"block-builder": {1, 2, 56},
+				},
+				PartitionsPerInstance: 2,
+			},
+			expectedPartitions: []int32{1, 2, 56},
+		},
+		{
+			name: "returns nil if no instance ID",
+			cfg: Config{
+				InstanceID: "",
+				AssignedPartitionsMap: map[string][]int32{
+					instanceID: {1, 2, 56},
+				},
+				PartitionsPerInstance: 2,
+			},
+			expectedPartitions: nil,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expectedPartitions, tc.cfg.AssignedPartitions())
+		})
+	}
+}
