f68a697077ffa12def6ed7354111d6964081c01d
Kaustubh Kurve <kaustubh6024@gmail.com>
2025-12-05 22:53:13 +0530

    Fix delete implementation for s3/gcs/azure backends to account for prefix (#6011)
    * Fix delete impl for s3/gcs/azure backends to account for prefix
    
    * Update changelog
    
    * Omit prefix from backend.readerWriter.Find result set
    
    The backend delete function (which prepends the prefix) is being used
    in two places: first to delete indexes directly using the file path,
    and secondly, the results from the Find function are iterated over to
    delete the files for a tenant. If the result set from the Find
    function returns the full path (including the prefix), the delete
    function would prepend the prefix again and hence would not target the
    correct path. Hence all Find implementations are updated to return
    unprefixed key in the result set (we trim the prefix from key before passing
    over to the FindFunc arg). This is consistent with the List implementation.
    
    * Add prefix for keypath for versioned deletes
    
    * Remove bucket prefix in backend.Find instead of entire prefix

--- tempodb/backend/azure/azure.go
+++ tempodb/backend/azure/azure.go
@@ -153,6 +153,8 @@ func (rw *Azure) CloseAppend(context.Context, backend.AppendTracker) error {
 }
 
 func (rw *Azure) Delete(ctx context.Context, name string, keypath backend.KeyPath, _ *backend.CacheInfo) error {
+	keypath = backend.KeyPathWithPrefix(keypath, rw.cfg.Prefix)
+
 	blobClient, err := getBlobClient(ctx, rw.cfg, backend.ObjectFileName(keypath, name))
 	if err != nil {
 		return fmt.Errorf("cannot get Azure blob client, name: %s: %w", backend.ObjectFileName(keypath, name), err)
@@ -286,7 +288,7 @@ func (rw *Azure) Find(ctx context.Context, keypath backend.KeyPath, f backend.Fi
 
 			o = strings.TrimSuffix(*b.Name, dir)
 			opts := backend.FindMatch{
-				Key:      o,
+				Key:      strings.TrimPrefix(o, rw.cfg.Prefix),
 				Modified: *b.Properties.LastModified,
 			}
 			f(opts)
@@ -363,6 +365,8 @@ func (rw *Azure) WriteVersioned(ctx context.Context, name string, keypath backen
 }
 
 func (rw *Azure) DeleteVersioned(ctx context.Context, name string, keypath backend.KeyPath, version backend.Version) error {
+	keypath = backend.KeyPathWithPrefix(keypath, rw.cfg.Prefix)
+
 	// TODO use conditional if-match API
 	_, currentVersion, err := rw.ReadVersioned(ctx, name, keypath)
 	if err != nil && !errors.Is(err, backend.ErrDoesNotExist) {
