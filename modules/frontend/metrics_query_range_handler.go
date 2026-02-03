9fd7299dc0c276475ca5ef3bb8bc1045abc29ddb
Ruslan Mikhailov <thedevdruid@pm.me>
2026-02-03 14:02:46 +0100

    Align with ClampDateRangeReq behaviour

--- modules/frontend/metrics_query_range_handler.go
+++ modules/frontend/metrics_query_range_handler.go
@@ -54,11 +54,12 @@ func newQueryRangeStreamingGRPCHandler(cfg Config, next pipeline.AsyncRoundTripp
 		traceql.AlignRequest(req)
 
 		// the end time cutoff is applied here because it has to be done before combiner creation
+		// TODO: this is a copy of ClampDateRangeReq and needs to be removed after a proper fix
 		if cfg.QueryEndCutoff > 0 {
 			now := time.Now()
 			maxEnd := now.Add(-cfg.QueryEndCutoff)
 			reqEnd := time.Unix(0, int64(req.End))
-			if reqEnd.After(maxEnd) {
+			if maxEnd.Before(reqEnd) {
 				req.End = uint64(maxEnd.UnixNano())
 				traceql.AlignEndToLeft(req) // realign, but always to the left
 			}
@@ -135,11 +136,12 @@ func newMetricsQueryRangeHTTPHandler(cfg Config, next pipeline.AsyncRoundTripper
 		traceql.AlignRequest(queryRangeReq)
 
 		// the end time cutoff is applied here because it has to be done before combiner creation
+		// TODO: this is a copy of ClampDateRangeReq and needs to be removed after a proper fix
 		if cfg.QueryEndCutoff > 0 {
 			now := time.Now()
 			maxEnd := now.Add(-cfg.QueryEndCutoff)
 			reqEnd := time.Unix(0, int64(queryRangeReq.End))
-			if reqEnd.After(maxEnd) {
+			if maxEnd.Before(reqEnd) {
 				queryRangeReq.End = uint64(maxEnd.UnixNano())
 				traceql.AlignEndToLeft(queryRangeReq) // realign, but always to the left
 			}
