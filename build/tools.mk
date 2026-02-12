99638458b84463edb12d6b5c8e59b4a2d6f94135
A. Stoewer <adrian@stoewer.me>
2026-02-12 18:27:33 +0100

    Update TOOLS_IMAGE_TAG

--- build/tools.mk
+++ build/tools.mk
@@ -12,7 +12,7 @@ TOOL_DIR     ?= tools
 TOOL_CONFIG  ?= $(TOOL_DIR)/tools.go
 
 TOOLS_IMAGE ?= grafana/tempo-ci-tools
-TOOLS_IMAGE_TAG ?= main-c4f476b
+TOOLS_IMAGE_TAG ?= main-36bd1c7
 
 GOTOOLS ?= $(shell cd $(TOOL_DIR) && go list -e -f '{{ .Imports }}' -tags tools |tr -d '[]')
 
