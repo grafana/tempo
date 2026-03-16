bfc24d7de2fe7cd22c4ca971e3f87e4e3e2b8eba
Javi <javiermolinar@live.com>
2026-03-16 17:14:35 +0100

    chore: use synctest to speedup (#6556)
    * chore: use synctest to speedup
    
    * more tests

--- pkg/usagestats/stats_test.go
+++ pkg/usagestats/stats_test.go
@@ -4,6 +4,7 @@ import (
 	"runtime"
 	"sync"
 	"testing"
+	"testing/synctest"
 	"time"
 
 	"github.com/grafana/tempo/cmd/tempo/build"
@@ -117,18 +118,20 @@ func Test_BuildStats(t *testing.T) {
 }
 
 func TestCounter(t *testing.T) {
-	c := NewCounter("test_counter")
-	c.Inc(100)
-	c.Inc(200)
-	c.Inc(300)
-	time.Sleep(1 * time.Second)
-	c.updateRate()
-	v := c.Value()
-	require.Equal(t, int64(600), v["total"])
-	require.GreaterOrEqual(t, v["rate"], float64(590))
-	c.reset()
-	require.Equal(t, int64(0), c.Value()["total"])
-	require.Equal(t, float64(0), c.Value()["rate"])
+	synctest.Test(t, func(t *testing.T) {
+		c := NewCounter("test_counter")
+		c.Inc(100)
+		c.Inc(200)
+		c.Inc(300)
+		time.Sleep(1 * time.Second)
+		c.updateRate()
+		v := c.Value()
+		require.Equal(t, int64(600), v["total"])
+		require.GreaterOrEqual(t, v["rate"], float64(590))
+		c.reset()
+		require.Equal(t, int64(0), c.Value()["total"])
+		require.Equal(t, float64(0), c.Value()["rate"])
+	})
 }
 
 func TestStatistic(t *testing.T) {
