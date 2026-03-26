6d4906dcbd5ddddaa1ad315b782450f599e1e972
J Pham <94262131+ie-pham@users.noreply.github.com>
2026-03-26 09:33:24 -0500

    update go version for CVE-2026-25679 (#6779) (#6794)
    * update go version for CVE-2026-25679 (#6779)
    
    * update tools go version for CVE-2026-25679
    
    * update go version globally
    
    * update ci go version

--- build/tools.mk
+++ build/tools.mk
@@ -12,7 +12,7 @@ TOOL_DIR     ?= tools
 TOOL_CONFIG  ?= $(TOOL_DIR)/tools.go
 
 TOOLS_IMAGE ?= grafana/tempo-ci-tools
-TOOLS_IMAGE_TAG ?= main-36bd1c7
+TOOLS_IMAGE_TAG ?= main-8340a1d49
 
 GOTOOLS ?= $(shell cd $(TOOL_DIR) && go list -e -f '{{ .Imports }}' -tags tools |tr -d '[]')
 
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/grafana/tempo
 
-go 1.25.7
+go 1.26.1
 
 require (
 	cloud.google.com/go/storage v1.58.0
