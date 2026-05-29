59d14298d59e177df632200c67573daea6c04cc7
Xiaoguang Zhang <shawn.zhang@grafana.com>
2026-05-29 12:43:07 -0700

    chore!: drop OpenCensus receiver support in tempo 2.10 (#7322)

--- vendor/github.com/open-telemetry/opentelemetry-collector-contrib/receiver/opencensusreceiver/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-//go:generate mdatagen metadata.yaml
-
-// Package opencensusreceiver receives OpenCensus traces and metrics.
-// Deprecated: this receiver is no longer maintained and has reached end-of-life. See https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/36791
-package opencensusreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/opencensusreceiver"
