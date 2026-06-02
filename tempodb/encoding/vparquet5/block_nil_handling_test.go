8a8548115dfbcfc7289c7acff63f4d5a5bcde283
Martin Disibio <mdisibio@gmail.com>
2026-06-02 08:55:31 -0400

    cleanup

--- tempodb/encoding/vparquet5/block_nil_handling_test.go
+++ tempodb/encoding/vparquet5/block_nil_handling_test.go
@@ -8,31 +8,29 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/tempo/pkg/traceql"
-	"github.com/grafana/tempo/pkg/util"
 	"github.com/grafana/tempo/pkg/util/test"
 	"github.com/grafana/tempo/tempodb/backend"
 	"github.com/grafana/tempo/tempodb/encoding/common"
 )
 
 func TestNilHandlingBlockSearchTraceQL(t *testing.T) {
-	dc := []backend.DedicatedColumn{
-		{Scope: "resource", Name: "dc_present", Type: "string"},
-		{Scope: "resource", Name: "dc_nil", Type: "string"},
-		{Scope: "span", Name: "dc_present", Type: "string"},
-		{Scope: "span", Name: "dc_nil", Type: "string"},
-		{Scope: "event", Name: "dc_present", Type: "string"},
-		{Scope: "event", Name: "dc_nil", Type: "string"},
-	}
-	// beforeID := test.ValidTraceID(nil)
-	wantTraceID := test.ValidTraceID(nil)
-	// afterID := test.ValidTraceID(nil)
+	var (
+		ctx          = context.Background()
+		wantTraceID1 = test.ValidTraceID(nil)
+		wantTraceID2 = test.ValidTraceID(nil)
+		dc           = []backend.DedicatedColumn{
+			{Scope: "resource", Name: "dc_present", Type: "string"},
+			{Scope: "resource", Name: "dc_nil", Type: "string"},
+			{Scope: "span", Name: "dc_present", Type: "string"},
+			{Scope: "span", Name: "dc_nil", Type: "string"},
+			{Scope: "event", Name: "dc_present", Type: "string"},
+			{Scope: "event", Name: "dc_nil", Type: "string"},
+		}
+	)
 
-	// Minimal trace: every scope has foo=bar; baz is omitted and used in "= nil" queries.
 	traces := []*Trace{
-		// fullyPopulatedTestTrace(beforeID),
 		{
-			TraceID:     wantTraceID,
-			TraceIDText: util.TraceIDToHexString(wantTraceID),
+			TraceID: wantTraceID1,
 			ResourceSpans: []ResourceSpans{
 				{
 					Resource: Resource{
@@ -84,109 +82,122 @@ func TestNilHandlingBlockSearchTraceQL(t *testing.T) {
 			},
 		},
 		{
-			RootSpanName: "no-events",
+			TraceID:      wantTraceID2,
+			RootSpanName: "empty",
 			ResourceSpans: []ResourceSpans{
 				{
 					Resource: Resource{},
 					ScopeSpans: []ScopeSpans{
 						{
 							Spans: []Span{
-								{},
+								{
+									Name: "empty",
+								},
 							},
 						},
 					},
 				},
 			},
 		},
-		// fullyPopulatedTestTrace(afterID),
+	}
+
+	searchesThatMatch := []struct {
+		wantTraceID []byte
+		query       string
+	}{
+		{wantTraceID1, `{resource.dc_nil = nil}`},
+		{wantTraceID1, `{resource.bar = nil}`},
+		{wantTraceID1, `{span.dc_nil = nil}`},
+		{wantTraceID1, `{span.bar = nil}`},
+		{wantTraceID1, `{instrumentation.bar = nil}`},
+		{wantTraceID1, `{event.dc_nil = nil}`},
+		{wantTraceID1, `{event.bar = nil}`},
+		{wantTraceID1, `{link.bar = nil}`},
+		{wantTraceID1, `{resource.foo != nil}`},
+		{wantTraceID1, `{resource.dc_present != nil}`},
+		{wantTraceID1, `{span.foo != nil}`},
+		{wantTraceID1, `{span.dc_present != nil}`},
+		{wantTraceID1, `{instrumentation.foo != nil}`},
+		{wantTraceID1, `{event.foo != nil}`},
+		{wantTraceID1, `{event.dc_present != nil}`},
+		{wantTraceID1, `{link.foo != nil}`},
+
+		{wantTraceID2, `{trace:rootName = "empty" && span.bar = nil}`},
+		{wantTraceID2, `{trace:rootName = "empty" && span.dc_nil= nil}`},
+		{wantTraceID2, `{span:name = "empty" && span.bar = nil}`},
+		{wantTraceID2, `{span:name = "empty" && span.dc_nil = nil}`},
+	}
+
+	searchesThatDontMatch := []struct {
+		dontWantTraceID []byte
+		query           string
+	}{
+		// = nil on attributes that exist
+		{wantTraceID1, `{resource.foo = nil}`},
+		{wantTraceID1, `{resource.dc_present = nil}`},
+		{wantTraceID1, `{span.foo = nil}`},
+		{wantTraceID1, `{span.dc_present = nil}`},
+		{wantTraceID1, `{instrumentation.foo = nil}`},
+		{wantTraceID1, `{event.dc_present = nil}`},
+		{wantTraceID1, `{link.foo = nil}`},
+		// != nil on attributes that do not exist
+		{wantTraceID1, `{resource.baz != nil}`},
+		{wantTraceID1, `{span.baz != nil}`},
+		{wantTraceID1, `{instrumentation.baz != nil}`},
+		{wantTraceID1, `{event.baz != nil}`},
+		{wantTraceID1, `{link.baz != nil}`},
+
+		{wantTraceID2, `{event.foo = nil}`}, // Doesn't match because this span has no events.
+		{wantTraceID2, `{link.foo = nil}`},  // Doesn't match because this span has no links.
 	}
 
 	b := makeBackendBlockWithTracesWithDedicatedColumns(t, traces, dc)
-	// b := makeBackendBlockWithTraces(t, traces)
-	ctx := context.Background()
 
-	searchesThatMatch := []string{
-		`{resource.dc_nil = nil}`,
-		`{resource.bar = nil}`,
-		`{span.dc_nil = nil}`,
-		`{span.baz = nil}`,
-		`{instrumentation.baz = nil}`,
-		`{event.dc_nil = nil}`,
-		`{event.bar = nil}`,
-		`{link.baz = nil}`,
-		`{resource.foo != nil}`,
-		`{resource.dc_present != nil}`,
-		`{span.foo != nil}`,
-		`{span.dc_present != nil}`,
-		`{instrumentation.foo != nil}`,
-		`{event.foo != nil}`,
-		`{event.dc_present != nil}`,
-		`{link.foo != nil}`,
-	}
-
-	searchesThatDontMatch := []string{
-		// = nil on attributes that exist
-		`{resource.foo = nil}`,
-		`{resource.dc_present = nil}`,
-		`{span.foo = nil}`,
-		`{span.dc_present = nil}`,
-		`{instrumentation.foo = nil}`,
-		`{trace:rootName = "no-events" && event.foo = nil}`,
-		`{event.dc_present = nil}`,
-		`{link.foo = nil}`,
-		// != nil on attributes that do not exist
-		`{resource.baz != nil}`,
-		`{span.baz != nil}`,
-		`{instrumentation.baz != nil}`,
-		`{event.baz != nil}`,
-		`{link.baz != nil}`,
-	}
-
-	for _, q := range searchesThatMatch {
-		t.Run("match/"+q, func(t *testing.T) {
-			req := traceql.MustExtractFetchSpansRequestWithMetadata(q)
+	for _, tc := range searchesThatMatch {
+		t.Run("match/"+tc.query, func(t *testing.T) {
+			req := traceql.MustExtractFetchSpansRequestWithMetadata(tc.query)
 			if req.SecondPass == nil {
 				req.SecondPass = func(s *traceql.Spanset) ([]*traceql.Spanset, error) { return []*traceql.Spanset{s}, nil }
 				req.SecondPassConditions = traceql.SearchMetaConditions()
 			}
 
 			resp, err := b.Fetch(ctx, req, common.DefaultSearchOptions())
-			require.NoError(t, err, "query: %s", q)
+			require.NoError(t, err, "query: %s", tc)
 
 			found := false
 			for {
 				spanSet, err := resp.Results.Next(ctx)
-				require.NoError(t, err, "query: %s", q)
+				require.NoError(t, err, "query: %s", tc)
 				if spanSet == nil {
 					break
 				}
-				if bytes.Equal(spanSet.TraceID, wantTraceID) {
+				if bytes.Equal(spanSet.TraceID, tc.wantTraceID) {
 					found = true
 					break
 				}
 			}
-			require.True(t, found, "query: %s", q)
+			require.True(t, found, "query: %s", tc)
 		})
 	}
 
-	for _, q := range searchesThatDontMatch {
-		t.Run("no_match/"+q, func(t *testing.T) {
-			req := traceql.MustExtractFetchSpansRequestWithMetadata(q)
+	for _, tc := range searchesThatDontMatch {
+		t.Run("no_match/"+tc.query, func(t *testing.T) {
+			req := traceql.MustExtractFetchSpansRequestWithMetadata(tc.query)
 			if req.SecondPass == nil {
 				req.SecondPass = func(s *traceql.Spanset) ([]*traceql.Spanset, error) { return []*traceql.Spanset{s}, nil }
 				req.SecondPassConditions = traceql.SearchMetaConditions()
 			}
 
 			resp, err := b.Fetch(ctx, req, common.DefaultSearchOptions())
-			require.NoError(t, err, "query: %s", q)
+			require.NoError(t, err, "query: %s", tc.query)
 
 			for {
 				spanSet, err := resp.Results.Next(ctx)
-				require.NoError(t, err, "query: %s", q)
+				require.NoError(t, err, "query: %s", tc.query)
 				if spanSet == nil {
 					break
 				}
-				require.NotEqual(t, wantTraceID, spanSet.TraceID, "query: %s", q)
+				require.NotEqual(t, tc.dontWantTraceID, spanSet.TraceID, "query: %s", tc.query)
 			}
 		})
 	}
